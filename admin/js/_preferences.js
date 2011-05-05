d08d9d5913c1c2565ed20a8bb99c55a7d8ee56ae
Franck <carnet.franck.paul@gmail.com>
2011-05-05 11:10:56 +0200

    - Ajout d'une option d'accessibilité pour désactiver le drag and drop javascript pour ordonner les éléments - Application de cette option à l'ordre des favoris

--- admin/js/_preferences.js
+++ admin/js/_preferences.js
@@ -19,20 +19,4 @@ $(function() {
 		}
 		return true;
 	});
-	$("#my-favs ul").sortable({'cursor':'move'});
-	$("#my-favs ul").hover(function () {
-		$(this).css({'cursor':'move'});
-	}, function () {
-		$(this).css({'cursor':'auto'});
-	});
-	$('#favs-form').submit(function() {
-		var order=[];
-		$("#my-favs ul li input.position").each(function() {
-			order.push(this.name.replace(/^order\[([^\]]+)\]$/,'$1'));
-		});
-		$("input[name=favs_order]")[0].value = order.join(',');
-		return true;
-	});
-	$("#my-favs ul li input.position").hide();
-
 });
