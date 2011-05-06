1d31c1be219ebbc2c2064be1941045208f5e6731
Franck <carnet.franck.paul@gmail.com>
2011-05-06 15:31:30 +0200

    Des lignes moins longues comme des jours sans bière on a dit ! Mets les doigts comme ça Franck !

--- admin/user.php
+++ admin/user.php
@@ -194,12 +194,14 @@ form::field('user_id',20,255,html::escapeHTML($user_id),'',2).
 '</label></p>'.
 '<p class="form-note">'.__('At least 2 characters using letters, numbers or symbols.').'</p>'.
 
-'<p><label for="new_pwd" '.($user_id != '' ? '' : 'class="required"').'>'.($user_id != '' ? '' : '<abbr title="'.__('Required field').'">*</abbr> ').
+'<p><label for="new_pwd" '.($user_id != '' ? '' : 'class="required"').'>'.
+($user_id != '' ? '' : '<abbr title="'.__('Required field').'">*</abbr> ').
 ($user_id != '' ? __('New password:') : __('Password:')).' '.
 form::password('new_pwd',20,255,'','',3).
 '</label></p>'.
 
-'<p><label for="new_pwd_c" '.($user_id != '' ? '' : 'class="required"').'>'.($user_id != '' ? '' : '<abbr title="'.__('Required field').'">*</abbr> ').__('Confirm password:').' '.
+'<p><label for="new_pwd_c" '.($user_id != '' ? '' : 'class="required"').'>'.
+($user_id != '' ? '' : '<abbr title="'.__('Required field').'">*</abbr> ').__('Confirm password:').' '.
 form::password('new_pwd_c',20,255,'','',4).
 '</label></p>'.
 
@@ -263,7 +265,8 @@ $core->callBehavior('adminUserForm',isset($rs) ? $rs : null);
 
 echo
 '<fieldset>'.
-'<p><label for="your_pwd" '.($user_id != '' ? '' : 'class="required"').'>'.($user_id != '' ? '' : '<abbr title="'.__('Required field').'">*</abbr> ').__('Your password:').
+'<p><label for="your_pwd" '.($user_id != '' ? '' : 'class="required"').'>'.
+($user_id != '' ? '' : '<abbr title="'.__('Required field').'">*</abbr> ').__('Your password:').
 form::password('your_pwd',20,255,'','',17).'</label></p>'.
 '</fieldset>'.
 '<p class="clear"><input type="submit" accesskey="s" value="'.__('Save').'" tabindex="16" />'.
