eab393b728c1b878e7ba59162e613c07da440d3a
kozlika <none@none>
2011-05-22 17:12:27 +0200

    Ductile. Ready to work on le chantier.
    --HG--
    branch : themes

--- themes/ductile/_define.php
+++ themes/ductile/_define.php
@@ -1,9 +1,7 @@
 <?php
 # -- BEGIN LICENSE BLOCK ---------------------------------------
 #
-# This file is part of Dotclear 2.
-#
-# Copyright (c) 2003-2011 Olivier Meunier & Association Dotclear
+# Copyright (c) Association Dotclear
 # Licensed under the GPL version 2.0 license.
 # See LICENSE file or
 # http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
@@ -15,6 +13,6 @@ $this->registerModule(
 	/* Name */			"Ductile",
 	/* Description*/		"Chantier pour un nouveau theme",
 	/* Author */			"Dotclear Team",
-	/* Version */			'1.0'
+	/* Version */			'0.1'
 );
 ?>
