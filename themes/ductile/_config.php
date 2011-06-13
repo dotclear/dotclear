f75c51b3f35687bdd634753cf2052c902f69b476
Franck <carnet.franck.paul@gmail.com>
2011-06-13 15:31:04 +0200

    Mise en place des premiers réglages : couleur des liens

--- themes/ductile/_config.php
+++ themes/ductile/_config.php
@@ -11,6 +11,77 @@
 # -- END LICENSE BLOCK -----------------------------------------
 if (!defined('DC_CONTEXT_ADMIN')) { return; }
 
-echo
-'<p class="area">Ici, bientôt, la configuration du thème Ductile.</p>';
+l10n::set(dirname(__FILE__).'/locales/'.$_lang.'/main');
+
+function adjustColor($c)
+{
+	if ($c === '') {
+		return '';
+	}
+
+	$c = strtoupper($c);
+
+	if (preg_match('/^[A-F0-9]{3,6}$/',$c)) {
+		$c = '#'.$c;
+	}
+
+	if (preg_match('/^#[A-F0-9]{6}$/',$c)) {
+		return $c;
+	}
+
+	if (preg_match('/^#[A-F0-9]{3,}$/',$c)) {
+		return '#'.substr($c,1,1).substr($c,1,1).substr($c,2,1).substr($c,2,1).substr($c,3,1).substr($c,3,1);
+	}
+
+	return '';
+}
+
+$ductile_base = array(
+	'body_link_c' => null,
+	'body_link_v_c' => null,
+	'body_link_f_c' => null
+);
+
+$ductile_user = $core->blog->settings->themes->ductile_style;
+$ductile_user = @unserialize($ductile_user);
+if (!is_array($ductile_user)) {
+	$ductile_user = array();
+}
+
+$ductile_user = array_merge($ductile_base,$ductile_user);
+
+if (!empty($_POST))
+{
+	try
+	{
+		$ductile_user['body_link_c'] = adjustColor($_POST['body_link_c']);
+		$ductile_user['body_link_f_c'] = adjustColor($_POST['body_link_f_c']);
+		$ductile_user['body_link_v_c'] = adjustColor($_POST['body_link_v_c']);
+		
+		$core->blog->settings->addNamespace('themes');
+		$core->blog->settings->themes->put('ductile_style',serialize($ductile_user));
+		$core->blog->triggerBlog();
+
+		echo
+		'<div class="message"><p>'.
+		__('Style sheet upgraded.').
+		'</p></div>';
+	}
+	catch (Exception $e)
+	{
+		$core->error->add($e->getMessage());
+	}
+}
+
+echo '<fieldset><legend>'.__('Links').'</legend>'.
+'<p class="field"><label for="body_link_c">'.__('Links color:').' '.
+form::field('body_link_c',7,7,$ductile_user['body_link_c'],'colorpicker').'</label></p>'.
+
+'<p class="field"><label for="body_link_v_c">'.__('Visited links color:').' '.
+form::field('body_link_v_c',7,7,$ductile_user['body_link_v_c'],'colorpicker').'</label></p>'.
+
+'<p class="field"><label for="body_link_f_c">'.__('Focus links color:').' '.
+form::field('body_link_f_c',7,7,$ductile_user['body_link_f_c'],'colorpicker').'</label></p>'.
+'</fieldset>';
+
 ?>
--- themes/ductile/_public.php
+++ themes/ductile/_public.php
@@ -18,7 +18,51 @@ class tplDuctileTheme
 {
 	public static function publicHeadContent($core)
 	{
-		echo '<style type="text/css">'."\n".'/* Additionnal style directives */'."\n</style>\n";
+		echo 
+			'<style type="text/css">'."\n".
+			'/* Additionnal style directives */'."\n".
+			self::ductileStyleHelper().
+			"</style>\n";
+	}
+	
+	public static function ductileStyleHelper()
+	{
+		$s = $GLOBALS['core']->blog->settings->themes->ductile_style;
+
+		if ($s === null) {
+			return;
+		}
+
+		$s = @unserialize($s);
+		if (!is_array($s)) {
+			return;
+		}
+
+		$css = array();
+
+		# Properties
+
+		self::prop($css,'a','color',$s['body_link_c']);
+		self::prop($css,'a:visited','color',$s['body_link_v_c']);
+		self::prop($css,'a:hover, a:focus, a:active','color',$s['body_link_f_c']);
+
+		$res = '';
+		foreach ($css as $selector => $values) {
+			$res .= $selector." {\n";
+			foreach ($values as $k => $v) {
+				$res .= $k.':'.$v.";\n";
+			}
+			$res .= "}\n";
+		}
+
+		return $res;
+	}
+
+	protected static function prop(&$css,$selector,$prop,$value)
+	{
+		if ($value) {
+			$css[$selector][$prop] = $value;
+		}
 	}
 }
 ?>
