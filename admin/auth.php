ae29c85b0e99db434824db04772a757395842556
Dsls <dsls@morefnu.org>
2011-06-14 08:59:10 +0200

    Reinforced language detection in auth.

--- admin/auth.php
+++ admin/auth.php
@@ -22,7 +22,7 @@ if (isset($_SESSION['sess_user_id']))
 # That's a tricky hack but it works ;)
 $dlang = http::getAcceptLanguage();
 $dlang = ($dlang == '' ? 'en' : $dlang);
-if ($dlang != 'en')
+if ($dlang != 'en' && preg_match('/^[a-z]{2}(-[a-z]{2})?$/',$dlang))
 {
 	l10n::set(dirname(__FILE__).'/../locales/'.$dlang.'/main');
 }
