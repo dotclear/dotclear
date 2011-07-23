5f8b4731abef8ea626cdcd1143524f586ef9bdbf
kozlika <devnull@localhost>
2011-07-23 15:27:42 +0200

    Ductile - Compat IE ≤ 8

--- themes/ductile/_config.php
+++ themes/ductile/_config.php
@@ -1,9 +1,8 @@
 <?php
 # -- BEGIN LICENSE BLOCK ---------------------------------------
+# This file is part of Ductile, a theme for Dotclear
 #
-# This file is part of Dotclear 2.
-#
-# Copyright (c) 2003-2011 Olivier Meunier & Association Dotclear
+# Copyright (c) 2011 - Association Dotclear
 # Licensed under the GPL version 2.0 license.
 # See LICENSE file or
 # http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
