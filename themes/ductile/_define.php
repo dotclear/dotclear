9eba31f47a3ffbac289443a7790e536f8022eab4
Franck <carnet.franck.paul@gmail.com>
2011-07-28 19:49:36 +0200

    Version mineure++
    --HG--
    branch : themes

--- themes/ductile/_define.php
+++ themes/ductile/_define.php
@@ -14,7 +14,7 @@ $this->registerModule(
 	/* Name */				"Ductile",
 	/* Description*/		"Chantier pour un nouveau theme",
 	/* Author */			"Dotclear Team",
-	/* Version */			'0.9.1'
+	/* Version */			'0.9.2'
 	/* Properties (uncomment for 2.4 compliance only : add a / at the end of this line -> ) *
 							,array(
 								'standalone_config' => true
