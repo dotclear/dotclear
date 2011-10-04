896a048a75b9fe1885ae8074ecd6eb2d7af00cec
franck <carnet.franck.paul@gmail.com>
2011-10-04 15:09:48 +0200

    Fixed PHP 5.3 warning when renaming a media, closes  #1225, #1226 (reprise de la modif faite par dols sur la branche 2.3, le merge pose problème à cause de Clearbricks)

--- inc/core/class.dc.media.php
+++ inc/core/class.dc.media.php
@@ -925,7 +925,7 @@ class dcMedia extends filemanager
 		}
 	}
 	
-	protected function imageThumbUpdate(&$file,&$newFile)
+	protected function imageThumbUpdate($file,$newFile)
 	{
 		if ($file->relname != $newFile->relname)
 		{
