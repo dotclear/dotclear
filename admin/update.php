28b6ed40b5ee404629dedf63578677bb2d1f0a67
alex <none@none>
2011-11-17 00:01:59 +0100

    Update error codes in HTML source

--- admin/update.php
+++ admin/update.php
@@ -119,8 +119,12 @@ if ($new_v && $step)
 	catch (Exception $e)
 	{
 		$msg = $e->getMessage();
+		
+		$error_code = 'update_err';
+		
 		if ($e->getCode() == dcUpdate::ERR_FILES_CHANGED)
 		{
+			$error_code = "update_err_files_changed";
 			$msg =
 			__('The following files of your Dotclear installation '.
 			'have been modified so we won\'t try to update your installation. '.
@@ -128,6 +132,7 @@ if ($new_v && $step)
 		}
 		elseif ($e->getCode() == dcUpdate::ERR_FILES_UNREADABLE)
 		{
+			$error_code = "update_err_files_unreadable";
 			$msg =
 			sprintf(__('The following files of your Dotclear installation are not readable. '.
 			'Please fix this or try to make a backup file named %s manually.'),
@@ -135,6 +140,7 @@ if ($new_v && $step)
 		}
 		elseif ($e->getCode() == dcUpdate::ERR_FILES_UNWRITALBE)
 		{
+			$error_code = "update_err_files_unwritalbe";
 			$msg =
 			__('The following files of your Dotclear installation cannot be written. '.
 			'Please fix this or try to <a href="http://dotclear.org/download">update manually</a>.');
@@ -142,7 +148,7 @@ if ($new_v && $step)
 		
 		if (isset($e->bad_files)) {
 			$msg .=
-			'<ul><li><strong>'.
+			'<ul id="'.$error_code.'"><li><strong>'.
 			implode('</strong></li><li><strong>',$e->bad_files).
 			'</strong></li></ul>';
 		}
