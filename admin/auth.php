aa8efc6990244e71f55daf651e8e6cce41617e5c
Dsls <dsls@morefnu.org>
2012-02-08 20:53:04 +0100

    Advisory ID: HTB23074 - 1.1: Sanitize admin/auth.php login_data parameter

--- admin/auth.php
+++ admin/auth.php
@@ -30,7 +30,7 @@ if ($dlang != 'en' && preg_match('/^[a-z]{2}(-[a-z]{2})?$/',$dlang))
 $page_url = http::getHost().$_SERVER['REQUEST_URI'];
 
 $change_pwd = $core->auth->allowPassChange() && isset($_POST['new_pwd']) && isset($_POST['new_pwd_c']) && isset($_POST['login_data']);
-$login_data = !empty($_POST['login_data']) ? $_POST['login_data'] : null;
+$login_data = !empty($_POST['login_data']) ? html::escapeHTML($_POST['login_data']) : null;
 $recover = $core->auth->allowPassChange() && !empty($_REQUEST['recover']);
 $safe_mode = !empty($_REQUEST['safe_mode']);
 $akey = $core->auth->allowPassChange() && !empty($_GET['akey']) ? $_GET['akey'] : null;
