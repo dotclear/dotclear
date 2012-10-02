a40ab85ae60b0407662698fd085fa950dca6fc15
franck <carnet.franck.paul@gmail.com>
2012-10-02 16:59:57 +0200

    minor bug

--- themes/ductile/_define.php
+++ themes/ductile/_define.php
@@ -14,7 +14,7 @@ $this->registerModule(
 	/* Name */				"Ductile",
 	/* Description*/		"Mediaqueries compliant elegant theme",
 	/* Author */			"Dotclear Team",
-	/* Version */			'1.2',
+	/* Version */			'1.2.1',
 	/* Properties */		array(
 								'standalone_config' => true
 							)
