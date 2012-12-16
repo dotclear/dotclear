bf088027c3f7fb7ef04af994b5bb1c3167582556
JcDenis <devnull@localhost>
2012-12-17 00:15:04 +0100

    * First draft for the use of Twig on plugins * aboutConfig is now "Twig compliant"

--- plugins/aboutConfig/_admin.php
+++ plugins/aboutConfig/_admin.php
@@ -14,4 +14,6 @@ if (!defined('DC_CONTEXT_ADMIN')) { return; }
 $_menu['Plugins']->addItem('about:config','plugin.php?p=aboutConfig','index.php?pf=aboutConfig/icon.png',
 		preg_match('/plugin.php\?p=aboutConfig(&.*)?$/',$_SERVER['REQUEST_URI']),
 		$core->auth->isSuperAdmin());
+
+$core->tpl->getLoader()->addPath(dirname(__FILE__).'/admtpl/','aboutConfig');
 ?>
