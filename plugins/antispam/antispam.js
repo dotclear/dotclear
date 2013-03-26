3c3a92bd6ae56c2ab5c7df40cda9d66bdb3afbb9
franck <carnet.franck.paul@gmail.com>
2013-03-26 14:37:00 +0100

    Antispam plugin now uses jQuery UI sortable for filters ordering via drag'n'drop

--- plugins/antispam/antispam.js
+++ plugins/antispam/antispam.js
@@ -1,34 +1,18 @@
-var dragsort = ToolMan.dragsort();
 $(function() {
-	$("#filters-list").each(function() {
-		dragsort.makeTableSortable(this,dotclear.sortable.setHandle, dotclear.sortable.saveOrder);
+	$("#filters-list").sortable({'cursor':'move'});
+	$("#filters-list tr").hover(function () {
+		$(this).css({'cursor':'move'});
+	}, function () {
+		$(this).css({'cursor':'auto'});
 	});
-	$('form input[type=submit][name=delete_all]').click(function(){
-		return window.confirm(dotclear.msg.confirm_spam_delete);
-	});
-});
-
-dotclear.sortable = {
-	setHandle: function(item) {
-		var handle = $(item).find('td.handle').get(0);
-		while (handle.firstChild) {
-			handle.removeChild(handle.firstChild);
-		}
-		
-		item.toolManDragGroup.setHandle(handle);
-		$(handle).addClass('handler');
-	},
-	
-	saveOrder: function(item) {
-		var group = item.toolManDragGroup;
-		var order = $('#filters_order').get(0);
-		group.register('dragend', function() {
-			order.value = '';
-			items = item.parentNode.getElementsByTagName('tr');
-			
-			for (var i=0; i<items.length; i++) {
-				order.value += items[i].id.substr(2)+',';
-			}
+	$('#filters-form').submit(function() {
+		var order=[];
+		$("#filters-list tr td input.position").each(function() {
+			order.push(this.name.replace(/^f_order\[([^\]]+)\]$/,'$1'));
 		});
-	}
-};
+		$("input[name=filters_order]")[0].value = order.join(',');
+		return true;
+	});
+	$("#filters-list tr td input.position").hide();
+	$("#filters-list tr td.handle").addClass('handler');
+});
