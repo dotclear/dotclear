920648fa9a6113ce0e7d9ae095cd562d957bf27b
franck <carnet.franck.paul@gmail.com>
2013-03-29 11:32:20 +0100

    Add p access key for post and page preview.

--- admin/post.php
+++ admin/post.php
@@ -418,7 +418,7 @@ if ($can_edit_post)
 		$core->blog->url.$core->url->getURLFor('preview',$core->auth->userID().'/'.
 		http::browserUID(DC_MASTER_KEY.$core->auth->userID().$core->auth->getInfo('user_pwd')).
 		'/'.$post->post_url);
-		echo '<a id="post-preview" href="'.$preview_url.'" class="button">'.__('Preview').'</a> ';
+		echo '<a id="post-preview" href="'.$preview_url.'" class="button" accesskey="p">'.__('Preview').' (p)'.'</a> ';
 	}
 	echo
 	($can_delete ? '<input type="submit" class="delete" value="'.__('Delete').'" name="delete" />' : '').
