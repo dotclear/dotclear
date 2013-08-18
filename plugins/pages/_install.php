720d1a1567b891327d6b7dce3d8309189f077250
franck <carnet.franck.paul@gmail.com>
2013-08-18 12:11:57 +0200

    Simplest title and content for the example page, closes #1448
    --HG--
    branch : 2.5

--- plugins/pages/_install.php
+++ plugins/pages/_install.php
@@ -38,10 +38,10 @@ If( $counter->f(0) == 0 && $core->blog->settings->pages->firstpage == null ) {
 	$cur->post_type = 'page';
 	$cur->post_format = 'xhtml';
 	$cur->post_lang = $core->blog->settings->system->lang;
-	$cur->post_title = __('Page de démonstration');
-	$cur->post_content = __('<h3>Créer une nouvelle page</h3><p>Cliquez sur le lien <strong>Pages</strong> situé sur le <strong>Tableau de bord</strong> ou dans la section <strong>Blog</strong> du menu latéral. La liste des pages s\'affiche. Cliquez sur le bouton <strong>Nouvelle page</strong> en haut de la page pour créer une page. vous retrouvez alors la même interface que pour éditer les <a href="http://fr.dotclear.org/documentation/2.0/usage/entries" class="wikilink1" title="2.0:usage:entries">billets</a> mais sans catégorie ni tags.</p>');
+	$cur->post_title = __('My first page');
+	$cur->post_content = __('<p>'.__('This is your first page. When you\'re ready to blog, log in to edit or delete it.').'</p>');
 	$cur->post_content_xhtml = $cur->post_content;
-	$cur->post_excerpt = __('<p>Les pages sont des contenus indépendants des billets. Elles servent par exemple à créer des pages <em>À propos</em> ou <em>Mentions légales</em> qui ont vocation à être toujours accessibles, contrairement aux billets progressivement remplacés par les nouveaux billets.</p>');
+	$cur->post_excerpt = '';
 	$cur->post_excerpt_xhtml = $cur->post_excerpt;
 	$cur->post_status = 1;
 	$cur->post_open_comment = 0;
@@ -52,3 +52,4 @@ If( $counter->f(0) == 0 && $core->blog->settings->pages->firstpage == null ) {
 
 $core->setVersion('pages',$version);
 return true;
+?>
