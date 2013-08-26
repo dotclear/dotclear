9f43ed630ae1f48580d75b6ce3d4cc655c6436f4
Lepeltier kévin <devnull@localhost>
2013-08-26 21:39:59 +0200

    Ticket #1604 progression

--- plugins/widgets/dragdrop.js
+++ plugins/widgets/dragdrop.js
@@ -6,19 +6,47 @@ $(function() {
 	$('.widgetSettings').hide();
 	
 	// move
-	$( ".connected" ).sortable({
-		axis: "y",
-		connectWith: ".connected",
+	$( ".connected, .sortable-delete" ).sortable({
+		tolerance: "move",
 		cursor: "move",
-		stop: function( event, ui ) {
-			$('.connected').each(function(){
-				if( $(this).find('li').length == 0 ) {
-					$(this).parents('.widgets').find('.empty-widgets').show();
-				} else {
-					$(this).parents('.widgets').find('.empty-widgets').hide();
-				}
-			});
-		}//,
+		axis: "y",
+		dropOnEmpty: true,
+		handle: ".widget-name",
+		placeholder: "ui-sortable-placeholder",
+		connectWith: ".connected, .sortable-delete",
+		start: function( event, ui ) {
+			ui.item.css('left', ui.item.position().left + 30);
+		},
+		update: function(event, ui) {
+			
+			// mes a zéro le décalage
+			ui.item.css('left', 'auto');
+			
+			if( $(this).find('li').length == 0 ) {
+				$(this).parents('.widgets').find('.empty-widgets').show();
+			} else {
+				$(this).parents('.widgets').find('.empty-widgets').hide();
+			}
+			
+			// réordonne
+			if( $(this).attr('id') ) {
+				$(this).find('input[title=ordre]').each(function(i){
+					tab = $(this).val(i);
+				});
+			}
+			
+			// switch
+			if( $(this).attr('id') != ui.item.parents('ul').attr('id') ) {
+				//oldname = $(this).attr('id').split('dnd').join('');
+				var name = ui.item.parents('ul').attr('id').split('dnd').join('');
+				ui.item.find('*[name^=w]').each(function(){
+					tab = $(this).attr('name').split('][');
+					tab[0] = "w["+name;
+					$(this).attr('name', tab.join(']['));
+				});
+			}
+			
+		} //,
 		//change: function( event, ui ) {
 			//ui.helper.css('height', $('#dndnav .widget-name').css('height'));
 		//}
@@ -26,21 +54,19 @@ $(function() {
 	
 	// add
 	$( "#widgets > li" ).draggable({
+		tolerance: "move",
+		cursor: "move",
 		connectToSortable: ".connected",
 		helper: "clone",
 		revert: "invalid",
-		cursor: "move",
 		start: function( event, ui ) {
 			ui.helper.css({'width': $('#widgets > li').css('width')});
 			//ui.helper.css({'min-height': $('#dndnav .widget-name').css('height')});
 			//ui.helper.find('.form-note').hide();
 			//ui.helper.find('.widget-name').css({'min-height': $('#dndnav .widget-name').css('height')});
-		}/*,
-		stop: function( event, ui ) {
-			$('.empty-widgets').css('display', 'none');
-		}*/
+		}
 	});
 	
-	$( "#widgets, .connected" ).disableSelection();
+	//$( "#widgets, .connected, .sortable-delete" ).disableSelection();
 	
 });
