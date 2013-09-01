2b336bc5398edeb82cbed607b291577db53e9dc7
Florent Cotton <florent.cotton@neurokaine.fr>
2013-09-01 03:16:29 +0200

    Support des pingbacks : après le support de la détection et de l'envoi de pingbacks, au tour du support en réception. Dans le détail (ou presque) : * Ajout d'un endpoint "pingback.ping" XML-RPC, mais le tout le gros du traitement est dans le fichier class.dc.trackback.php * Ajout d'une méthode "receive_pb" dans la classe dcTrackback pour la prise en charge quasi-complète de la réception et enregistrement d'un pingback. * Ajout d'une balise template {{tpl:BlogXMLRPCURL}} pour retourner l'URL du serveur XML-RPC du blog courant * Ajout d'un bloc au niveau des en-têtes dans les templates par défaut post.html et page.html pour la mise en oeuvre si besoin d'un <link rel="pingback" ../> * Ajout de l'envoi d'un en-tête HTTP supplémentaire "X-Pingback" dans les gestionnaires d'URLs pour les types "post" et "pages"
    Reste plus qu'à tester en conditions réelles et à polir au besoin.

--- inc/core/class.dc.trackback.php
+++ inc/core/class.dc.trackback.php
@@ -308,6 +308,149 @@ class dcTrackback
 		echo	$resp."</response>";
 	}
 	//@}
+
+	/// @name Receive pingbacks
+	//@{
+	/**
+	Receives a pingback and insert it as a comment of given post.
+	
+	@param	from_url		<b>string</b>		Source URL
+	@param	to_url			<b>string</b>		Target URL
+	*/
+	public function receive_pb($from_url, $to_url)
+	{
+		$reg = '!^'.preg_quote($this->core->blog->url).'(.*)!';
+		$type = $args = $next = '';
+		
+		# Are you dumb?
+		if (!preg_match($reg, $to_url, $m)) {
+			throw new Exception(__('Any chance you ping one of my contents? No? Really?'), 0);
+		}
+		
+		# Does the targeted URL look like a registered post type? 
+		$url_part = $m[1];
+		$p_type = '';
+		$post_types = $this->core->getPostTypes();
+		foreach ($post_types as $k => $v) {
+			$reg = '!^'.preg_quote(str_replace('%s', '', $v['public_url'])).'(.*)!';
+			if (preg_match($reg, $url_part, $n)) {
+				$p_type = $k;
+				$post_url = $n[1];
+				break;
+			}
+		}
+		
+		if (empty($p_type)) {
+			throw new Exception(__('Sorry but you can not ping this type of content.'), 33);
+		}
+
+		# Time to see if we've got a winner...
+		$params = array(
+			'post_type' => $p_type,
+			'post_url' => $post_url,
+		);
+		$posts = $this->core->blog->getPosts($params);
+		
+		# Missed! 
+		if ($posts->isEmpty()) {
+			throw new Exception(__('Oops. Kinda "not found" stuff. Please check the target URL twice.'), 33);
+		}
+		
+		# Nice try. But, sorry, no.
+		if (!$posts->trackbacksActive()) {
+			throw new Exception(__('Sorry, dude. This entry does not accept pingback at the moment.'), 33);
+		}
+
+		# OK. We've found our champion. Time to check the remote part.
+		try {
+			$http = self::initHttp($from_url, $from_path);
+			
+			# First round : just to be sure the ping comes from an acceptable resource type.
+			$http->setHeadersOnly(true);
+			$http->get($from_path);
+			$c_type = explode(';', $http->getHeader('content-type'));
+
+			# Bad luck. Bye, bye...
+			if (!in_array($c_type[0],array('text/html', 'application/xhtml+xml'))) {
+				throw new Exception(__('Your source URL does not look like a supported content type. Sorry. Bye, bye!'), 0);
+			}
+			
+			# Second round : let's go fetch and parse the remote content
+			$http->setHeadersOnly(false);
+			$http->get($from_path);
+			$remote_content = $http->getContent();
+
+			$charset = mb_detect_encoding($remote_content,
+				'UTF-8,ISO-8859-1,ISO-8859-2,ISO-8859-3,'.
+				'ISO-8859-4,ISO-8859-5,ISO-8859-6,ISO-8859-7,ISO-8859-8,'.
+				'ISO-8859-9,ISO-8859-10,ISO-8859-13,ISO-8859-14,ISO-8859-15');
+
+			if (strtolower($charset) != 'utf-8') {
+				$remote_content = iconv($charset,'UTF-8',$remote_content);
+			}
+			
+			# We want a title...
+			if (!preg_match('!<title>([^<].*?)</title>!mis', $remote_content, $m)) {
+				throw new Exception(__('Where\'s your title?'), 0);
+			}
+			$title = trim(html::clean($m[1]));
+			$title = html::decodeEntities($title);
+			$title = html::escapeHTML($title);
+			$title = text::cutString($title,60);
+			
+			preg_match('!<body[^>]*?>(.*)?</body>!msi', $remote_content, $m);
+			$source = $m[1];
+			$source = preg_replace('![\r\n\s]+!ms',' ',$source);
+			$source = preg_replace( "/<\/*(h\d|p|th|td|li|dt|dd|pre|caption|input|textarea|button)[^>]*>/", "\n\n", $source );
+			$source = strip_tags($source, '<a>');
+			$source = explode("\n\n",$source);
+			
+			$excerpt = '';
+			foreach ($source as $line) {
+				if (strpos($line, $to_url) !== false) {
+					if (preg_match("!<a[^>]+?".$to_url."[^>]*>([^>]+?)</a>!", $line, $m)) {
+						$excerpt = strip_tags($line);
+						break;
+					}
+				}
+			}
+			if ($excerpt) {
+				$excerpt = '(&#8230;) '.text::cutString(html::escapeHTML($excerpt),255).' (&#8230;)';
+			}
+			else {
+				$excerpt = '(??)';
+			}
+
+			$comment =
+			"<!-- TB -->\n".
+			'<p><strong>'.$title."</strong></p>\n".
+			'<p>'.$excerpt.'</p>';
+			
+			$cur = $this->core->con->openCursor($this->core->prefix.'comment');
+			$cur->comment_author = 'Anonymous blog';
+			$cur->comment_site = (string) $from_url;
+			$cur->comment_content = (string) $comment;
+			$cur->post_id = $posts->post_id;
+			$cur->comment_trackback = 1;
+			$cur->comment_status = $this->core->blog->settings->system->trackbacks_pub ? 1 : -1;
+			$cur->comment_ip = http::realIP();
+			
+			# --BEHAVIOR-- publicBeforeTrackbackCreate
+			$this->core->callBehavior('publicBeforeTrackbackCreate',$cur);
+			if ($cur->post_id) {
+				$comment_id = $this->core->blog->addComment($cur);
+				
+				# --BEHAVIOR-- publicAfterTrackbackCreate
+				$this->core->callBehavior('publicAfterTrackbackCreate',$cur,$comment_id);
+			}
+		}
+		catch (Exception $e) {
+			throw new Exception(__('Sorry, an internal problem has occured.'), 0);
+		}
+		
+		return __('Thanks, mate. It was a pleasure.');
+	}
+	//@}
 	
 	private static function initHttp($url,&$path)
 	{
