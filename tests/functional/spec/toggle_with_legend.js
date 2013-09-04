cb00988f234619c12de1c7626a51e5a1ab3eefb3
Nicolas <nikrou77@gmail.com>
2013-09-04 21:55:21 +0200

    [Tests] Add tests for media uploader Refactoring of _media.js : put all code in a jquery plugin to make it easy to test and to avoid polluting global namespace. Use same jquery for tests and for admin
    Need to add more tests for media uploader
    Fix indentation for all js

--- tests/functional/spec/toggle_with_legend.js
+++ tests/functional/spec/toggle_with_legend.js
@@ -1,47 +1,47 @@
 describe("toggleWithLegend method (admin/js/common.js)", function() {
-    it("Click arrow must make target visible", function() {
-	loadFixtures('menu.html');
-	$('#post_status').parent().toggleWithLegend($('#post_status'),{	});
+	it("Click arrow must make target visible", function() {
+		loadFixtures('menu.html');
+		$('#post_status').parent().toggleWithLegend($('#post_status'),{	});
 
-	var $arrow = $('#post_status').parent().find('img');
-	$arrow.click();
-	expect($('#post_status')).toBeVisible();
-    });
-    it("Click arrow twice,must make target visible and after second click hidden", function() {
-	loadFixtures('menu.html');
-	$('#post_status').parent().toggleWithLegend($('#post_status'),{	});
+		var $arrow = $('#post_status').parent().find('img');
+		$arrow.click();
+		expect($('#post_status')).toBeVisible();
+	});
+	it("Click arrow twice,must make target visible and after second click hidden", function() {
+		loadFixtures('menu.html');
+		$('#post_status').parent().toggleWithLegend($('#post_status'),{	});
 
-	var $arrow = $('#post_status').parent().find('img');
-	$arrow.click();
-	expect($('#post_status')).toBeVisible();
+		var $arrow = $('#post_status').parent().find('img');
+		$arrow.click();
+		expect($('#post_status')).toBeVisible();
 
-	$arrow.click();
-	expect($('#post_status')).toBeHidden();
-    });
-    it("Chick target must not hide target", function() {
-	loadFixtures('menu.html');
-	$('#post_status').parent().toggleWithLegend($('#post_status'),{	});
+		$arrow.click();
+		expect($('#post_status')).toBeHidden();
+	});
+	it("Chick target must not hide target", function() {
+		loadFixtures('menu.html');
+		$('#post_status').parent().toggleWithLegend($('#post_status'),{	});
 
-	var $arrow = $('#post_status').parent().find('img');
-	$arrow.click();
-	expect($('#post_status')).toBeVisible();
+		var $arrow = $('#post_status').parent().find('img');
+		$arrow.click();
+		expect($('#post_status')).toBeVisible();
 
-	$('#post_status option[value="-2"]').attr('selected', 'selected');
-	expect($('#post_status')).toBeVisible();
-    });
-    it("Chick target must not hide target, when legend_click is true", function() {
-	loadFixtures('menu.html');
-	var $label = $('#post_status').parent().children('label');
-	$label.toggleWithLegend($('#post_status'),{'legend_click':true, a_container:false});
+		$('#post_status option[value="-2"]').attr('selected', 'selected');
+		expect($('#post_status')).toBeVisible();
+	});
+	it("Chick target must not hide target, when legend_click is true", function() {
+		loadFixtures('menu.html');
+		var $label = $('#post_status').parent().children('label');
+		$label.toggleWithLegend($('#post_status'),{'legend_click':true, a_container:false});
 
-	$label.click();
-	expect($('#post_status')).toBeVisible();
+		$label.click();
+		expect($('#post_status')).toBeVisible();
 
-	var $arrow = $('#post_status').parent().find('img');
-	$arrow.click();
-	expect($('#post_status')).toBeVisible();
+		var $arrow = $('#post_status').parent().find('img');
+		$arrow.click();
+		expect($('#post_status')).toBeVisible();
 
-	$('#post_status').val(-2).trigger('change');
-	expect($('#post_status')).toBeVisible();
-    });
+		$('#post_status').val(-2).trigger('change');
+		expect($('#post_status')).toBeVisible();
+	});
 });
--- tests/functional/spec/users_actions.js
+++ tests/functional/spec/users_actions.js
@@ -1,25 +1,25 @@
 describe("updatePermissionsForm method (admin/js/_users_actions.js)", function() {
-    it("Click admin persmission must checked all associated permissions", function() {
-	loadFixtures('normal.html');
-	$('#permissions-form').updatePermissionsForm();
-	var permissions = ['usage','publish','delete','contentadmin','categories'];
+	it("Click admin persmission must checked all associated permissions", function() {
+		loadFixtures('normal.html');
+		$('#permissions-form').updatePermissionsForm();
+		var permissions = ['usage','publish','delete','contentadmin','categories'];
 
-	$('input[name="perm[default][admin]"]').click();
-	for (var _i=0,_len=permissions.length;_i<_len;_i++) {
-	    expect($('input[name="perm[default]['+permissions[_i]+']"]')).toBeChecked();
-	    expect($('input[name="perm[default]['+permissions[_i]+']"]')).toBeDisabled();
-	}
-    });
+		$('input[name="perm[default][admin]"]').click();
+		for (var _i=0,_len=permissions.length;_i<_len;_i++) {
+			expect($('input[name="perm\\[default\\]\\['+permissions[_i]+'\\]"]')).toBeChecked();
+			expect($('input[name="perm\\[default\\]\\['+permissions[_i]+'\\]"]')).toBeDisabled();
+		}
+	});
 
-    it("Click contentadmin persmission must checked all associated permissions", function() {
-	loadFixtures('normal.html');
-	$('#permissions-form').updatePermissionsForm();
-	var permissions = ['usage','publish','delete'];
+	it("Click contentadmin persmission must checked all associated permissions", function() {
+		loadFixtures('normal.html');
+		$('#permissions-form').updatePermissionsForm();
+		var permissions = ['usage','publish','delete'];
 
-	$('input[name="perm[default][contentadmin]"]').click();
-	for (var _i=0,_len=permissions.length;_i<_len;_i++) {
-	    expect($('input[name="perm[default]['+permissions[_i]+']"]')).toBeChecked();
-	    expect($('input[name="perm[default]['+permissions[_i]+']"]')).toBeDisabled();
-	}
-    });
+		$('input[name="perm[default][contentadmin]"]').click();
+		for (var _i=0,_len=permissions.length;_i<_len;_i++) {
+			expect($('input[name="perm[default]['+permissions[_i]+']"]')).toBeChecked();
+			expect($('input[name="perm[default]['+permissions[_i]+']"]')).toBeDisabled();
+		}
+	});
 });
