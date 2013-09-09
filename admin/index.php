bfc542a414356fc066cde8fa6d9213f9bbde9e9d
Anne Kozlika <kozlika@gmail.com>
2013-09-09 21:05:35 +0200

    Je crois que j'ai dompté ce *$^£")& de bug sur les boutons.

--- admin/index.php
+++ admin/index.php
@@ -402,7 +402,7 @@ foreach ($__dashboard_contents as $i)
 	}
 }
 
-$class = ' '.(($dashboardItems != '') && ($dashboardContents != '') ? 'two-boxes' : 'one-box txt-center');
+$class = ' '.(($dashboardItems != '') && ($dashboardContents != '') ? 'two-boxes' : 'one-box');
 
 if ($dashboardContents != '' || $dashboardItems != '') {
 	echo 
