2f7c5761e1361d29b2b2b26aec8af36c067f1af6
franck <carnet.franck.paul@gmail.com>
2013-09-14 06:21:47 +0200

    Minor enhancement

--- plugins/tags/_admin.php
+++ plugins/tags/_admin.php
@@ -320,7 +320,7 @@ class tagsBehaviors
 		$value = array_key_exists('tag_list_format',$opts) ? $opts['tag_list_format'] : 'more';
 		
 		echo
-		'<p><label for="user_tag_list_format">'.__('Tags list format:').'</label> '.
+		'<p><label for="user_tag_list_format" class="classic">'.__('Tags list format:').'</label> '.
 		form::combo('user_tag_list_format',$combo,$value).
 		'</p>';
 	}
