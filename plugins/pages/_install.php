47e4dca5ff263a91555de60d4ff3aebecd096954
franck <carnet.franck.paul@gmail.com>
2013-09-18 08:15:54 +0200

    A sample page will be created only on the first installation of the plugin pages - fixes #1673

--- plugins/pages/_install.php
+++ plugins/pages/_install.php
@@ -17,35 +17,36 @@ if (version_compare($core->getVersion('pages'),$version,'>=')) {
 	return;
 }
 
-/* Create first page
--------------------------------------------------------- */
 $core->blog->settings->addNamespace('pages');
 
-$params = array(
-	'post_type' => 'page',
-	'no_content' => true
-);
-$counter = $core->blog->getPosts($params,true);
+if ($core->getVersion('pages') == null) {
 
-If( $counter->f(0) == 0 && $core->blog->settings->pages->firstpage == null ) {
-	
-	$core->blog->settings->pages->put('firstpage',true, 'boolean');
+	// Create a first pending page, only on a new installation of this plugin
+	$params = array(
+		'post_type' => 'page',
+		'no_content' => true
+	);
+	$counter = $core->blog->getPosts($params,true);
 
-	$cur = $core->con->openCursor($core->prefix.'post');
-	$cur->user_id = $core->auth->userID();
-	$cur->post_type = 'page';
-	$cur->post_format = 'xhtml';
-	$cur->post_lang = $core->blog->settings->system->lang;
-	$cur->post_title = __('My first page');
-	$cur->post_content = __('<p>'.__('This is your first page. When you\'re ready to blog, log in to edit or delete it.').'</p>');
-	$cur->post_content_xhtml = $cur->post_content;
-	$cur->post_excerpt = '';
-	$cur->post_excerpt_xhtml = $cur->post_excerpt;
-	$cur->post_status = 1;
-	$cur->post_open_comment = 0;
-	$cur->post_open_tb = 0;
-	$post_id = $core->blog->addPost($cur);
-	
+	If( $counter->f(0) == 0 && $core->blog->settings->pages->firstpage == null ) {
+
+		$core->blog->settings->pages->put('firstpage',true, 'boolean');
+
+		$cur = $core->con->openCursor($core->prefix.'post');
+		$cur->user_id = $core->auth->userID();
+		$cur->post_type = 'page';
+		$cur->post_format = 'xhtml';
+		$cur->post_lang = $core->blog->settings->system->lang;
+		$cur->post_title = __('My first page');
+		$cur->post_content = __('<p>'.__('This is your first page. When you\'re ready to blog, log in to edit or delete it.').'</p>');
+		$cur->post_content_xhtml = $cur->post_content;
+		$cur->post_excerpt = '';
+		$cur->post_excerpt_xhtml = $cur->post_excerpt;
+		$cur->post_status = -2;	// Pending status
+		$cur->post_open_comment = 0;
+		$cur->post_open_tb = 0;
+		$post_id = $core->blog->addPost($cur);
+	}
 }
 
 $core->setVersion('pages',$version);
