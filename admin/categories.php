71fa7f1611963798373d58b83dc3528d6c67aa9f
Anne Kozlika <kozlika@gmail.com>
2013-09-20 00:30:02 +0200

    Categories. (désolée pour le flood), correction xhtml, il en reste à faire mais ça n'est pas à  ma portée, je passe la main à Nikrou.

--- admin/categories.php
+++ admin/categories.php
@@ -190,17 +190,19 @@ else
 	echo
 	'</div>';
 
-	echo '<div class="clear"><p>';
+	echo '<div class="clear">';
 
 	if ($core->auth->check('categories',$core->blog->id) && $rs->count()>1) {
 		if (!$core->auth->user_prefs->accessibility->nodragdrop) {
 			echo '<p class="form-note hidden-if-no-js">'.__('To rearrange categories order, move items by drag and drop, then click on “Save categories order” button.').'</p>';
 		}
 		echo
-		'<span class="hidden-if-no-js">'.
+		'<p><span class="hidden-if-no-js">'.
 		'<input type="hidden" id="categories_order" name="categories_order" value=""/>'.
 		'<input type="submit" name="save_order" id="save-set-order" value="'.__('Save categories order').'" />'.
 		'</span> ';
+	} else {
+		echo '<p>';
 	}
 
 	echo
