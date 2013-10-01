8970414d066e8f6f83d4acc66d8198fbaadce787
Denis Jean-Chirstian <contact@jcdenis.fr>
2013-10-01 22:43:07 +0200

    And a double toogle for Mrs Kozlika
    --HG--
    branch : dcRepo

--- admin/js/_blog_theme.js
+++ admin/js/_blog_theme.js
@@ -1,6 +1,9 @@
 $(function() {
 	$('.module-name').not('.current-theme .module-name').each(function(){
-		$(this).toggleWithLegend($(this).parent().children('.module-infos'),{legend_click: true});
+		$(this)
+			.toggleWithLegend($(this).parent().children('.module-infos'),{legend_click: true})
+			.toggleWithLegend($(this).parent().children('.module-actions'),{legend_click: true,
+				img_on_src: '', img_on_alt: '', img_off_src: '', img_off_alt: ''});
 	});
 /* Exemple for toggle on screenshot
 	$('.module-sshot').not('.current-theme .module-name').each(function(){
