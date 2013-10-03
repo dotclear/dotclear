d3991b9c1f4ea7d8201a6b8ca8598c4519e9273a
Nicolas <nikrou77@gmail.com>
2013-10-03 10:56:45 +0200

    Open outgoing links in new window and append "new window" to title attribute only if javascript is enabled Closes #1586

--- plugins/pages/page.php
+++ plugins/pages/page.php
@@ -355,7 +355,7 @@ if (!empty($_GET['xconv']))
 }
 
 if ($post_id && $post->post_status == 1) {
-	echo '<p><a class="onblog_link" href="'.$post->getURL().'" onclick="window.open(this.href);return false;" title="'.$post_title.' ('.__('new window').')'.'">'.__('Go to this page on the site').' <img src="images/outgoing-blue.png" alt="" /></a></p>';
+	echo '<p><a class="onblog_link outgoing" href="'.$post->getURL().'" title="'.$post_title.'">'.__('Go to this page on the site').' <img src="images/outgoing-blue.png" alt="" /></a></p>';
 }
 
 echo '';
