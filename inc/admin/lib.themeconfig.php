0380de98f578b7771b09292af1b08731f6d23380
franck <carnet.franck.paul@gmail.com>
2014-02-13 16:48:49 +0100

    Some doc and minor refinement

--- inc/admin/lib.themeconfig.php
+++ inc/admin/lib.themeconfig.php
@@ -20,8 +20,15 @@ if (!defined('DC_ADMIN_CONTEXT')) { return; }
  */
 class dcThemeConfig
 {
-	// Utilities
 
+/**
+ * Compute contrast ratio between two colors
+ *
+ * @param  string $color      text color
+ * @param  string $background background color
+ *
+ * @return float             computed ratio
+ */
 	public static function computeContrastRatio($color,$background)
 	{
 		// Compute contrast ratio between two colors
@@ -47,6 +54,15 @@ class dcThemeConfig
 		return $ratio;
 	}
 
+/**
+ * Compute WCAG contrast ration level
+ *
+ * @param  float  $ratio computed ratio between foreground and backround color
+ * @param  string  $size  font size as defined in CSS
+ * @param  boolean $bold  true if bold font
+ *
+ * @return string         WCAG contrast ratio level (AAA, AA or <nothing>)
+ */
 	public static function contrastRatioLevel($ratio,$size,$bold=false)
 	{
 		if ($size == '') {
@@ -96,6 +112,16 @@ class dcThemeConfig
 		return '';
 	}
 
+/**
+ * Return full information about constrat ratio
+ *
+ * @param  string  $color      text color
+ * @param  string  $background background color
+ * @param  string  $size       font size
+ * @param  boolean $bold       bold font
+ *
+ * @return string              contrast ratio including WCAG level
+ */
 	public static function contrastRatio($color,$background,$size='',$bold=false)
 	{
 		if (($color != '') && ($background != '')) {
@@ -108,6 +134,13 @@ class dcThemeConfig
 		return '';
 	}
 
+/**
+ * Check font size
+ *
+ * @param  string $s font size
+ *
+ * @return string    checked font size
+ */
 	public static function adjustFontSize($s)
 	{
 		if (preg_match('/^([0-9.]+)\s*(%|pt|px|em|ex)?$/',$s,$m)) {
@@ -119,6 +152,13 @@ class dcThemeConfig
 		return null;
 	}
 
+/**
+ * Check object position, should be x:y
+ *
+ * @param  string $p position
+ *
+ * @return string    checked position
+ */
 	public static function adjustPosition($p)
 	{
 		if (!preg_match('/^[0-9]+(:[0-9]+)?$/',$p)) {
@@ -129,6 +169,13 @@ class dcThemeConfig
 		return $p[0].(count($p) == 1 ? ':0' : ':'.$p[1]);
 	}
 
+/**
+ * Check a CSS color
+ *
+ * @param  string $c CSS color
+ *
+ * @return string    checked CSS color
+ */
 	public static function adjustColor($c)
 	{
 		if ($c === '') {
@@ -150,25 +197,53 @@ class dcThemeConfig
 		return '';
 	}
 
-	// CSS file management
+/**
+ * Check and clean CSS
+ *
+ * @param  string $css CSS to be checked
+ *
+ * @return string      checked CSS
+ */
 	public static function cleanCSS($css)
 	{
 		// TODO ?
 		return $css;
 	}
 
+/**
+ * Return real path of a user defined CSS
+ *
+ * @param  string $folder CSS folder
+ *
+ * @return string         real path of CSS
+ */
 	public static function cssPath($folder)
 	{
 		global $core;
 		return path::real($core->blog->public_path).'/'.$folder;
 	}
 
+/**
+ * Retirn URL of a user defined CSS
+ *
+ * @param  string $folder CSS folder
+ *
+ * @return string         CSS URL
+ */
 	public static function cssURL($folder)
 	{
 		global $core;
 		return $core->blog->settings->system->public_url.'/'.$folder;
 	}
 
+/**
+ * Check if user defined CSS may be written
+ *
+ * @param  string  $folder CSS folder
+ * @param  boolean $create create CSS folder if necessary
+ *
+ * @return boolean          true if CSS folder exists and may be written, else false
+ */
 	public static function canWriteCss($folder,$create=false)
 	{
 		global $core;
@@ -200,6 +275,14 @@ class dcThemeConfig
 		return true;
 	}
 
+/**
+ * Store CSS property value in associated array
+ *
+ * @param  array $css       CSS associated array
+ * @param  string $selector selector
+ * @param  string $prop     property
+ * @param  string $value    value
+ */
 	public static function prop(&$css,$selector,$prop,$value)
 	{
 		if ($value) {
@@ -207,6 +290,15 @@ class dcThemeConfig
 		}
 	}
 
+/**
+ * Store background image property in CSS associated array
+ *
+ * @param  string $folder   image folder
+ * @param  array $css       CSS associated array
+ * @param  string $selector selector
+ * @param  boolean $value   false for default, true if image should be set
+ * @param  string $image    image filename
+ */
 	public static function backgroundImg($folder,&$css,$selector,$value,$image)
 	{
 		$file = self::imagesPath($folder).'/'.$image;
@@ -215,11 +307,24 @@ class dcThemeConfig
 		}
 	}
 
+/**
+ * Write CSS file
+ *
+ * @param  string $folder CSS folder
+ * @param  string $theme  CSS filename
+ * @param  string $css    CSS file content
+ */
 	public static function writeCss($folder,$theme,$css)
 	{
 		file_put_contents(self::cssPath($folder).'/'.$theme.'.css', $css);
 	}
 
+/**
+ * Delete CSS file
+ *
+ * @param  string $folder CSS folder
+ * @param  string $theme  CSS filename to be removed
+ */
 	public static function dropCss($folder,$theme)
 	{
 		$file = path::real(self::cssPath($folder).'/'.$theme.'.css');
@@ -228,6 +333,13 @@ class dcThemeConfig
 		}
 	}
 
+/**
+ * Return public URL of user defined CSS
+ *
+ * @param  string $folder CSS folder
+ *
+ * @return string         CSS file URL
+ */
 	public static function publicCssUrlHelper($folder)
 	{
 		$theme = $GLOBALS['core']->blog->settings->system->theme;
@@ -241,18 +353,40 @@ class dcThemeConfig
 		return null;
 	}
 
+/**
+ * Return real path of folder images
+ *
+ * @param  string $folder images folder
+ *
+ * @return string         real path of folder
+ */
 	public static function imagesPath($folder)
 	{
 		global $core;
 		return path::real($core->blog->public_path).'/'.$folder;
 	}
 
+/**
+ * Return URL of images folder
+ *
+ * @param  string $folder images folder
+ *
+ * @return string         URL of images folder
+ */
 	public static function imagesURL($folder)
 	{
 		global $core;
 		return $core->blog->settings->system->public_url.'/'.$folder;
 	}
 
+/**
+ * Check if images folder exists and may be written
+ *
+ * @param  string  $folder images folder
+ * @param  boolean $create create the folder if not exists
+ *
+ * @return boolean          true if folder exists and may be written
+ */
 	public static function canWriteImages($folder,$create=false)
 	{
 		global $core;
@@ -290,7 +424,16 @@ class dcThemeConfig
 		return true;
 	}
 
-	public static function uploadImage($folder,$f)
+/**
+ * Upload an image in images folder
+ *
+ * @param  string $folder images folder
+ * @param  string $f      image filename
+ * @param  int    $width  check accurate width of uploaded image if <> 0
+ *
+ * @return string         full pathname of uploaded image
+ */
+	public static function uploadImage($folder,$f,$width=0)
 	{
 		if (!self::canWriteImages($folder,true)) {
 			throw new Exception(__('Unable to create images.'));
@@ -309,31 +452,37 @@ class dcThemeConfig
 			throw new Exception(__('An error occurred while writing the file.'));
 		}
 
-		$s = getimagesize($dest);
-		if ($s[0] != 800) {
-			throw new Exception(__('Uploaded image is not 800 pixels wide.'));
+		if ($width) {
+			$s = getimagesize($dest);
+			if ($s[0] != $width) {
+				throw new Exception(sprintf(__('Uploaded image is not %s pixels wide.'),$width));
+			}
 		}
 
 		return $dest;
 	}
 
+/**
+ * Delete an image from images folder (with its thumbnails if any)
+ *
+ * @param  string $folder images folder
+ * @param  string $img    image filename
+ */
 	public static function dropImage($folder,$img)
 	{
+		global $core;
+
 		$img = path::real(self::imagesPath($folder).'/'.$img);
 		if (is_writable(dirname($img))) {
+			// Delete thumbnails if any
+			try {
+				$media = new dcMedia($core);
+				$media->imageThumbRemove($img);
+			} catch (Exception $e) {
+				$core->error->add($e->getMessage());
+			}
+			// Delete image
 			@unlink($img);
-			// Following lines should be replaced by a media thumbnails removal tool
-			@unlink(dirname($img).'/.'.basename($img,'.png').'_sq.jpg');
-			@unlink(dirname($img).'/.'.basename($img,'.png').'_m.jpg');
-			@unlink(dirname($img).'/.'.basename($img,'.png').'_s.jpg');
-			@unlink(dirname($img).'/.'.basename($img,'.png').'_sq.jpg');
-			@unlink(dirname($img).'/.'.basename($img,'.png').'_t.jpg');
-			@unlink(dirname($img).'/.'.basename($img,'.png').'_sq.png');
-			@unlink(dirname($img).'/.'.basename($img,'.png').'_m.png');
-			@unlink(dirname($img).'/.'.basename($img,'.png').'_s.png');
-			@unlink(dirname($img).'/.'.basename($img,'.png').'_sq.png');
-			@unlink(dirname($img).'/.'.basename($img,'.png').'_t.png');
 		}
 	}
-
 }
