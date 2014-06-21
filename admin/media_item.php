3ad067703aeb1b5d032a2f820767c5b221622550
Dsls <none@none>
2014-06-21 12:27:04 +0200

    Fixed buggy lines in media_item.php, closes #1973

--- admin/media_item.php
+++ admin/media_item.php
@@ -468,12 +468,12 @@ if ($file->media_image)
 
 if ($file->type == 'audio/mpeg3')
 {
-	echo dcMedia::mp3player($file->file_url,$core->adminurl->get("admin.home",array('pf' => 'player_mp3.swf'));
+	echo dcMedia::mp3player($file->file_url,$core->adminurl->get("admin.home",array('pf' => 'player_mp3.swf')));
 }
 
 if ($file->type == 'video/x-flv' || $file->type == 'video/mp4' || $file->type == 'video/x-m4v')
 {
-	echo dcMedia::flvplayer($file->file_url,$core->adminurl->get("admin.home",array('pf' => 'player_flv.swf'));
+	echo dcMedia::flvplayer($file->file_url,$core->adminurl->get("admin.home",array('pf' => 'player_flv.swf')));
 }
 
 echo
