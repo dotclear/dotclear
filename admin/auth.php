be4ad6a7e0980c0d72209355f5fc18f8bc4b3ee0
franck <carnet.franck.paul@gmail.com>
2014-10-17 15:11:05 +0200

    Switch from XHTML to HTML5 for backend

--- admin/auth.php
+++ admin/auth.php
@@ -264,11 +264,10 @@ if (isset($_GET['user'])) {
 
 header('Content-Type: text/html; charset=UTF-8');
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml"
-xml:lang="<?php echo $dlang; ?>" lang="<?php echo $dlang; ?>">
+<!DOCTYPE html>
+<html lang="<?php echo $dlang; ?>">
 <head>
-  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+  <meta charset="UTF-8" />
   <meta http-equiv="Content-Script-Type" content="text/javascript" />
   <meta http-equiv="Content-Style-Type" content="text/css" />
   <meta http-equiv="Content-Language" content="<?php echo $dlang; ?>" />
--- admin/install/index.php
+++ admin/install/index.php
@@ -239,11 +239,10 @@ if (!isset($step)) {
 }
 header('Content-Type: text/html; charset=UTF-8');
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml"
-xml:lang="en" lang="en">
+<!DOCTYPE html>
+<html lang="en">
 <head>
-  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+  <meta charset="UTF-8" />
   <meta http-equiv="Content-Script-Type" content="text/javascript" />
   <meta http-equiv="Content-Style-Type" content="text/css" />
   <meta http-equiv="Content-Language" content="en" />
