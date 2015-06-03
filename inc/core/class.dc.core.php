461f9894dfed4c1f1342098416024c9a8a520b7b
franck <carnet.franck.paul@gmail.com>
2015-06-03 16:56:00 +0200

    Cope with different form of php file extension (.php, .php5, .php7, …-), fixes #2098

--- inc/core/class.dc.core.php
+++ inc/core/class.dc.core.php
@@ -1381,7 +1381,7 @@ class dcCore
 				'Enable XML/RPC interface'),
 				array('lang','string','en',
 				'Default blog language'),
-				array('media_exclusion','string','/\.php$/i',
+				array('media_exclusion','string','/\.php[0-9]*$/i',
 				'File name exclusion pattern in media manager. (PCRE value)'),
 				array('media_img_m_size','integer',448,
 				'Image medium size in media manager'),
