f8a0ebef86eda77f59a8fa6329a7f4442497d8df
Nicolas <nikrou77@gmail.com>
2015-07-30 20:31:21 +0200

    dcTemplate::getSortByStr must allow new keys from foreign tables through templateCustomSortByAlias behavior. Remove test for existing DC_RC_PATH constant.

--- inc/public/class.dc.template.php
+++ inc/public/class.dc.template.php
@@ -3,13 +3,12 @@
 #
 # This file is part of Dotclear 2.
 #
-# Copyright (c) 2003-2013 Olivier Meunier & Association Dotclear
+# Copyright (c) 2003-2015 Olivier Meunier & Association Dotclear
 # Licensed under the GPL version 2.0 license.
 # See LICENSE file or
 # http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
 #
 # -- END LICENSE BLOCK -----------------------------------------
-if (!defined('DC_RC_PATH')) { return; }
 
 class dcTemplate extends template
 {
@@ -344,7 +343,7 @@ class dcTemplate extends template
 				if (!is_array($v)) {
 					$alias[$k] = array();
 				}
-				if (!is_array($v)) {
+				if (!isset($default_alias[$k]) || !is_array($default_alias[$k])) {
 					$default_alias[$k] = array();
 				}
 				$default_alias[$k] = array_merge($default_alias[$k],$alias[$k]);
