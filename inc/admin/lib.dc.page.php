b6718b1fef796505c64b47484f7273bcde7e1e28
franck <carnet.franck.paul@gmail.com>
2015-08-18 21:25:27 +0200

    Cope with "unknown" scheme in url (ie nor http: neither https: is defined in origin url)
    --HG--
    branch : 2.8

--- inc/admin/lib.dc.page.php
+++ inc/admin/lib.dc.page.php
@@ -946,21 +946,24 @@ class dcPage
 		return self::jsLoad('js/meta-editor.js');
 	}
 
-	public static function getPF($file) {
+	public static function getPF($file)
+	{
 		return $GLOBALS['core']->adminurl->get('load.plugin.file',array('pf' => $file));
 	}
 
-	public static function setXFrameOptions($origin=null) {
+	public static function setXFrameOptions($origin = null)
+	{
 		if (self::$xframe_loaded) {
 			return;
 		}
 		if ($origin !== null) {
 			$url = parse_url($origin);
-			header(sprintf('X-Frame-Options: %s', is_array($url)?("ALLOW-FROM ".$url['scheme'].'://'.$url['host']):'SAMEORIGIN'));
+			header(sprintf('X-Frame-Options: %s',is_array($url) ?
+				("ALLOW-FROM ".(isset($url['scheme']) ? $url['scheme'].':' : '' ).'//'.$url['host']) :
+				'SAMEORIGIN'));
 		} else {
 			header('X-Frame-Options: SAMEORIGIN'); // FF 3.6.9+ Chrome 4.1+ IE 8+ Safari 4+ Opera 10.5+
 		}
 		self::$xframe_loaded = true;
-
 	}
 }
