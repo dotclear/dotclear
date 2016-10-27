14c4a9cf75d3a0d0774f08257ccd8f326bc80567
franck <carnet.franck.paul@gmail.com>
2016-10-27 11:19:34 +0200

    Adjust inner height of the CKEDitor (using rows attributes of the textarea), fixes #2136

--- plugins/dcCKEditor/_post_config.php
+++ plugins/dcCKEditor/_post_config.php
@@ -11,16 +11,21 @@
 # -- END LICENSE BLOCK -----------------------------------------
 
 if (!defined('DC_CONTEXT_ADMIN')) { return; }
+
 header('Content-type: text/javascript');
+
 if (!empty($_GET['context'])) {
 	$context = $_GET['context'];
 } else {
 	$context = '';
 }
+
 $__extraPlugins = new ArrayObject();
 $core->callBehavior('ckeditorExtraPlugins',$__extraPlugins,$context);
 $extraPlugins = $__extraPlugins->getArrayCopy();
+
 ?>
+
 (function($) {
 	$.toolbarPopup = function toolbarPopup(url) {
 		if (dotclear.admin_base_url != '') {
@@ -58,13 +63,13 @@ $extraPlugins = $__extraPlugins->getArrayCopy();
 	$.getEditorName = function getEditorName() {
 		return $.active_editor;
 	}
-	chainHandler(window, 'onbeforeunload', function(e) {
+	chainHandler(window,'onbeforeunload',function(e) {
 		if (e == undefined && window.event) {
 			e = window.event;
 		}
 
 		var editor = CKEDITOR.instances[$.getEditorName()];
-		if (editor!==undefined && !confirmClosePage.formSubmit && editor.checkDirty()) {
+		if (editor !== undefined && !confirmClosePage.formSubmit && editor.checkDirty()) {
 			e.returnValue = confirmClosePage.prompt;
 			return confirmClosePage.prompt;
 		}
@@ -78,62 +83,69 @@ $(function() {
 	 */
 
 	CKEDITOR.timestamp = '';
-<?php if (!isset($dcckeditor_disable_native_spellchecker) || $dcckeditor_disable_native_spellchecker):?>
+
+<?php if (!isset($dcckeditor_disable_native_spellchecker) || $dcckeditor_disable_native_spellchecker): ?>
 	CKEDITOR.config.disableNativeSpellChecker = true;
-<?php else:?>
+<?php else: ?>
 	CKEDITOR.config.disableNativeSpellChecker = false;
-<?php endif;?>
+<?php endif; ?>
+
 	CKEDITOR.config.skin = 'dotclear,'+dotclear.dcckeditor_plugin_url+'/js/ckeditor-skins/dotclear/';
 	CKEDITOR.config.baseHref = dotclear.base_url;
-	CKEDITOR.config.height = '<?php echo $core->auth->getOption('edit_size') * 14;?>px';
+	CKEDITOR.config.height = '<?php echo $core->auth->getOption('edit_size') * 14; ?>px';
 
-<?php if (!empty($dcckeditor_cancollapse_button)):?>
+<?php if (!empty($dcckeditor_cancollapse_button)): ?>
 	CKEDITOR.config.toolbarCanCollapse = true;
-<?php endif;?>
+<?php endif; ?>
 
 	CKEDITOR.plugins.addExternal('entrylink',dotclear.dcckeditor_plugin_url+'/js/ckeditor-plugins/entrylink/');
 	CKEDITOR.plugins.addExternal('dclink',dotclear.dcckeditor_plugin_url+'/js/ckeditor-plugins/dclink/');
 	CKEDITOR.plugins.addExternal('media',dotclear.dcckeditor_plugin_url+'/js/ckeditor-plugins/media/');
 	CKEDITOR.plugins.addExternal('img',dotclear.dcckeditor_plugin_url+'/js/ckeditor-plugins/img/');
-	<?php if (!empty($dcckeditor_textcolor_button) || !empty($dcckeditor_background_textcolor_button)):?>
+
+<?php if (!empty($dcckeditor_textcolor_button) || !empty($dcckeditor_background_textcolor_button)): ?>
 	// button add "More Colors..." can be added if colordialog plugin is enabled
 	CKEDITOR.config.colorButton_enableMore = true;
-	<?php endif;?>
+<?php endif; ?>
 
-<?php if (!empty($extraPlugins) && count($extraPlugins)>0) {
-	foreach ($extraPlugins as $plugin) {
-		printf("\tCKEDITOR.plugins.addExternal('%s','%s');\n", $plugin['name'], $plugin['url']);
+<?php
+	if (!empty($extraPlugins) && count($extraPlugins) > 0) {
+		foreach ($extraPlugins as $plugin) {
+			printf("\tCKEDITOR.plugins.addExternal('%s','%s');\n",$plugin['name'],$plugin['url']);
+		}
 	}
-}
 ?>
-	if (dotclear.ckeditor_context===undefined || dotclear.ckeditor_tags_context[dotclear.ckeditor_context]===undefined) {
+	if (dotclear.ckeditor_context === undefined || dotclear.ckeditor_tags_context[dotclear.ckeditor_context] === undefined) {
 		return;
 	}
 	$(dotclear.ckeditor_tags_context[dotclear.ckeditor_context].join(',')).ckeditor({
+
 <?php
-$defautExtraPlugins = 'entrylink,dclink,media,justify,colorbutton,format,img';
-if (!empty($extraPlugins) && count($extraPlugins)>0) {
-	foreach ($extraPlugins as $plugin) {
-		$defautExtraPlugins .= ','. $plugin['name'];
+	$defautExtraPlugins = 'entrylink,dclink,media,justify,colorbutton,format,img';
+	if (!empty($extraPlugins) && count($extraPlugins)>0) {
+		foreach ($extraPlugins as $plugin) {
+			$defautExtraPlugins .= ','. $plugin['name'];
+		}
 	}
-}
 ?>
-		extraPlugins: '<?php echo $defautExtraPlugins;?>',
+		extraPlugins: '<?php echo $defautExtraPlugins; ?>',
 
 		keystrokes: [
 			[ CKEDITOR.CTRL + (CKEDITOR.env.mac ? CKEDITOR.ALT : CKEDITOR.SHIFT) +
-				dotclear.msg.link_accesskey.toUpperCase().charCodeAt(0), 'dcLinkCommand' ],    // Ctrl+Alt+l
+				dotclear.msg.link_accesskey.toUpperCase().charCodeAt(0),'dcLinkCommand' ],    // Ctrl+Alt+l
 			[ CKEDITOR.CTRL + (CKEDITOR.env.mac ? CKEDITOR.ALT : CKEDITOR.SHIFT) +
-				dotclear.msg.img_select_accesskey.toUpperCase().charCodeAt(0), 'mediaCommand' ],    // Ctrl+Alt+m
+				dotclear.msg.img_select_accesskey.toUpperCase().charCodeAt(0),'mediaCommand' ],    // Ctrl+Alt+m
 		],
 
-		<?php if (!empty($dcckeditor_format_select)):?>
+<?php if (!empty($dcckeditor_format_select)): ?>
 		// format tags
-<?php if (!empty($dcckeditor_format_tags)):?>
-		format_tags: '<?php echo $dcckeditor_format_tags;?>',
-<?php else:?>
+
+	<?php if (!empty($dcckeditor_format_tags)): ?>
+		format_tags: '<?php echo $dcckeditor_format_tags; ?>',
+	<?php else: ?>
 		format_tags: 'p;h1;h2;h3;h4;h5;h6;pre;address',
-<?php endif;?>
+	<?php endif; ?>
+
 		// following definition are needed to be specialized
 		format_p: { element: 'p' },
 		format_h1: { element: 'h1' },
@@ -144,7 +156,7 @@ if (!empty($extraPlugins) && count($extraPlugins)>0) {
 		format_h6: { element: 'h6' },
 		format_pre: { element: 'pre' },
 		format_address: { element: 'address' },
-		<?php endif;?>
+<?php endif; ?>
 
 		entities: false,
 		removeButtons: '',
@@ -153,46 +165,55 @@ if (!empty($extraPlugins) && count($extraPlugins)>0) {
 			{
 				name: 'basicstyles',
 				items: [
-<?php if (!empty($dcckeditor_format_select)):?>
+
+<?php if (!empty($dcckeditor_format_select)): ?>
 					'Format',
 <?php endif;?>
+
 					'Bold','Italic','Underline','Strike','Subscript','Superscript','Code','Blockquote',
 
-<?php if (!empty($dcckeditor_list_buttons)):?>
-					'NumberedList', 'BulletedList',
-<?php endif;?>
+<?php if (!empty($dcckeditor_list_buttons)): ?>
+					'NumberedList','BulletedList',
+<?php endif; ?>
+
 					'RemoveFormat'
 				]
 			},
-<?php if (!empty($dcckeditor_clipboard_buttons)):?>
+
+<?php if (!empty($dcckeditor_clipboard_buttons)): ?>
 			{
 				name: 'clipoard',
 				items: ['Cut','Copy','Paste','PasteText','PasteFromWord']
 			},
-<?php endif;?>
-<?php if (!empty($dcckeditor_alignment_buttons)):?>
+<?php endif; ?>
+
+<?php if (!empty($dcckeditor_alignment_buttons)): ?>
 			{
 				name: 'paragraph',
-				items: ['JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock']
+				items: ['JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock']
 			},
-<?php endif;?>
-<?php if (!empty($dcckeditor_table_button)):?>
+<?php endif; ?>
+
+<?php if (!empty($dcckeditor_table_button)): ?>
 			{
 				name: 'table',
 				items: ['Table']
 			},
-<?php endif;?>
+<?php endif; ?>
+
 			{
 				name: 'custom',
 				items: [
 					'EntryLink','dcLink','Media','img','-',
 					'Source'
-<?php if (!empty($dcckeditor_textcolor_button)):?>
+
+<?php if (!empty($dcckeditor_textcolor_button)): ?>
 					,'TextColor'
-<?php endif;?>
-<?php if (!empty($dcckeditor_background_textcolor_button)):?>
+<?php endif; ?>
+
+<?php if (!empty($dcckeditor_background_textcolor_button)): ?>
 					,'BGColor'
-<?php endif;?>
+<?php endif; ?>
 				]
 			},
 			{
@@ -201,21 +222,39 @@ if (!empty($extraPlugins) && count($extraPlugins)>0) {
 					'Maximize'
 				]
 			},
-			<?php // add extra buttons comming from dotclear plugins
-			if (!empty($extraPlugins) && count($extraPlugins)>0) {
-				$extraPlugins_str = "{name: 'extra', items: [%s]},\n";
-				$extra_icons = '';
-				foreach ($extraPlugins as $plugin) {
-					$extra_icons .= sprintf("'%s',", $plugin['button']);
-				}
-				printf($extraPlugins_str, $extra_icons);
-			}
-			?>
+
+<?php // add extra buttons comming from dotclear plugins
+	if (!empty($extraPlugins) && count($extraPlugins)>0) {
+		$extraPlugins_str = "{name: 'extra', items: [%s]},\n";
+		$extra_icons = '';
+		foreach ($extraPlugins as $plugin) {
+			$extra_icons .= sprintf("'%s',",$plugin['button']);
+		}
+		printf($extraPlugins_str,$extra_icons);
+	}
+?>
 		]
 	});
 
-	CKEDITOR.on('instanceReady', function(e) {
-		if ($('label[for="post_excerpt"] a img').attr('src')==dotclear.img_minus_src) {
+	CKEDITOR.on('instanceLoaded',function(e) {
+
+		// Retrieve textarea element of the instance, then its line-height (in px) and rows values,
+		// then apply line-height * rows (min = 6) to the inner height of the instance.
+
+		var ta = document.getElementById(e.editor.name);
+		if (ta !== undefined) {
+			var ta_rows = ta.rows;
+			var ta_line_height = parseFloat(window.getComputedStyle(ta,null).getPropertyValue('line-height'));
+			if (ta_rows > 0 && ta_line_height > 0) {
+				var ta_height = String(Math.max(ta_rows,6) * ta_line_height);
+				e.editor.resize('100%',ta_height,true);
+			}
+		}
+	});
+
+	CKEDITOR.on('instanceReady',function(e) {
+
+		if ($('label[for="post_excerpt"] a img').attr('src') == dotclear.img_minus_src) {
 			$('#cke_post_excerpt').removeClass('hide');
 		} else {
 			$('#cke_post_excerpt').addClass('hide');
@@ -228,7 +267,7 @@ if (!empty($extraPlugins) && count($extraPlugins)>0) {
 
 	// @TODO: find a better way to retrieve active editor
 	for (var id in CKEDITOR.instances) {
-		CKEDITOR.instances[id].on('focus', function(e) {
+		CKEDITOR.instances[id].on('focus',function(e) {
 			$.active_editor = e.editor.name;
 		});
 	}
