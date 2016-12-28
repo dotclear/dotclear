f4b1f08bf5171c102b93968d03d75899a1d1195b
franck <carnet.franck.paul@gmail.com>
2016-12-28 17:15:37 +0100

    Add not (!) test on tpl:SysIf blog_lang attribute, closes #2230

--- inc/public/class.dc.template.php
+++ inc/public/class.dc.template.php
@@ -2929,7 +2929,8 @@ class dcTemplate extends template
 		}
 
 		if (isset($attr['blog_lang'])) {
-			$if[] = "\$core->blog->settings->system->lang == '".addslashes($attr['blog_lang'])."'";
+			$sign = (boolean) $attr['blog_lang'] ? '!' : '=';
+			$if[] = "\$core->blog->settings->system->lang ".$sign."= '".addslashes($attr['blog_lang'])."'";
 		}
 
 		if (isset($attr['current_tpl'])) {
