e11dac874e7c4668dea2ff52532b90a162f98814
franck <carnet.franck.paul@gmail.com>
2018-01-14 09:58:58 +0100

    And a little bit prettyfying code is fine ;-)

--- plugins/dcLegacyEditor/js/_post_editor.js
+++ plugins/dcLegacyEditor/js/_post_editor.js
@@ -1,17 +1,17 @@
 $(function() {
-	if ($('#edit-entry').length==0) {return;}
-	if (dotclear.legacy_editor_context===undefined
-	    || dotclear.legacy_editor_tags_context[dotclear.legacy_editor_context]===undefined) {
+	if ($('#edit-entry').length == 0) { return; }
+	if (dotclear.legacy_editor_context === undefined ||
+		dotclear.legacy_editor_tags_context[dotclear.legacy_editor_context] === undefined) {
 		return;
 	}
 
-	if ((dotclear.legacy_editor_tags_context[dotclear.legacy_editor_context].indexOf('#post_content')!==-1)
-	    && (dotclear.legacy_editor_tags_context[dotclear.legacy_editor_context].indexOf('#post_excerpt')!==-1)) {
+	if ((dotclear.legacy_editor_tags_context[dotclear.legacy_editor_context].indexOf('#post_content') !== -1) &&
+		(dotclear.legacy_editor_tags_context[dotclear.legacy_editor_context].indexOf('#post_excerpt') !== -1)) {
 		// Get document format and prepare toolbars
 		var formatField = $('#post_format').get(0);
 		var last_post_format = $(formatField).val();
 		$(formatField).change(function() {
-			if (this.value!='dcLegacyEditor') { return;}
+			if (this.value != 'dcLegacyEditor') { return; }
 
 			var post_format = this.value;
 
@@ -22,11 +22,11 @@ $(function() {
 				last_post_format = $(this).val();
 			} else {
 				// Restore last format if change cancelled
-			$(this).val(last_post_format);
+				$(this).val(last_post_format);
 			}
 
 			$('.format_control > *').addClass('hide');
-			$('.format_control:not(.control_no_'+post_format+') > *').removeClass('hide');
+			$('.format_control:not(.control_no_' + post_format + ') > *').removeClass('hide');
 		});
 
 		var excerptTb = new jsToolBar(document.getElementById('post_excerpt'));
@@ -34,11 +34,11 @@ $(function() {
 		excerptTb.context = contentTb.context = 'post';
 
 		$('.format_control > *').addClass('hide');
-		$('.format_control:not(.control_no_'+last_post_format+') > *').removeClass('hide');
+		$('.format_control:not(.control_no_' + last_post_format + ') > *').removeClass('hide');
 	}
 
-	if (dotclear.legacy_editor_tags_context[dotclear.legacy_editor_context].indexOf('#comment_content')!==-1) {
-		if ($('#comment_content').length>0) {
+	if (dotclear.legacy_editor_tags_context[dotclear.legacy_editor_context].indexOf('#comment_content') !== -1) {
+		if ($('#comment_content').length > 0) {
 			var commentTb = new jsToolBar(document.getElementById('comment_content'));
 			commentTb.draw('xhtml');
 		}
@@ -52,7 +52,7 @@ $(function() {
 		}
 
 		// Load toolbars
-		if (contentTb!==undefined && excerptTb!==undefined) {
+		if (contentTb !== undefined && excerptTb !== undefined) {
 			contentTb.switchMode(formatField.value);
 			excerptTb.switchMode(formatField.value);
 		}
