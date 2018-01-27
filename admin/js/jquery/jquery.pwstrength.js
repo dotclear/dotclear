865d4eb947398e6a792cf3636868bc2356917cb3
franck <carnet.franck.paul@gmail.com>
2018-01-27 13:22:08 +0100

    Better way (but not the best) to evaluate password strength

--- admin/js/jquery/jquery.pwstrength.js
+++ admin/js/jquery/jquery.pwstrength.js
@@ -1,66 +1,85 @@
-/**
- * jquery.pwstrength http://matoilic.github.com/jquery.pwstrength
- *
- * @version v0.1.1
- * @author Mato Ilic <info@matoilic.ch>
- * @copyright 2013 Mato Ilic
- *
- * Dual licensed under the MIT and GPL licenses:
- * http://www.opensource.org/licenses/mit-license.php
- * http://www.gnu.org/licenses/gpl.html
- */
-;(function($) {
-    $.pwstrength = function(password) {
-        var score = 0, length = password.length, upperCase, lowerCase, digits, nonAlpha;
-        
-        if(length < 5) score += 0;
-        else if(length < 8) score += 5;
-        else if(length < 16) score += 10;
-        else score += 15;
-        
-        lowerCase = password.match(/[a-z]/g);
-        if(lowerCase) score += 1;
-        
-        upperCase = password.match(/[A-Z]/g);
-        if(upperCase) score += 5;
-        
-        if(upperCase && lowerCase) score += 2;
-        
-        digits = password.match(/\d/g);
-        if(digits && digits.length > 1) score += 5;
-        
-        nonAlpha = password.match(/\W/g)
-        if(nonAlpha) score += (nonAlpha.length > 1) ? 15 : 10;
-        
-        if(upperCase && lowerCase && digits && nonAlpha) score += 15;
-
-        if(password.match(/\s/)) score += 10;
-
-        if(score < 15) return 0;
-        if(score < 20) return 1;
-        if(score < 35) return 2;
-        if(score < 50) return 3;
-        return 4;
-    };
-    
-    function updateIndicator(event) {
-        var strength = $.pwstrength($(this).val()), options = event.data, klass;
-        klass = options.classes[strength];
-        
-        options.indicator.removeClass(options.indicator.data('pwclass'));
-        options.indicator.data('pwclass', klass);
-        options.indicator.addClass(klass);
-        options.indicator.find(options.label).html(options.texts[strength]);
+(function($) {
+  $.pwstrength = function(password) {
+    var score = 0;
+    var symbols = '[!,@,#,$,%,^,&,*,?,_,~]';
+    // Regexp
+    var check = new RegExp('(' + symbols + ')');
+    var doublecheck = new RegExp('(' + '.*' + symbols + '.*' + symbols + ')');
+    // password length
+    score += password.length * 4;
+    score += checkRepetition(1, password).length - password.length;
+    score += checkRepetition(2, password).length - password.length;
+    score += checkRepetition(3, password).length - password.length;
+    score += checkRepetition(4, password).length - password.length;
+    // password has 3 numbers
+    if (password.match(/(.*[0-9].*[0-9].*[0-9])/)) {
+      score += 5;
     }
-    
-    $.fn.pwstrength = function(options) {
-        var options = $.extend({
-            label: '.label',
-            classes: ['pw-very-weak', 'pw-weak', 'pw-mediocre', 'pw-strong', 'pw-very-strong'],
-            texts: ['very weak', 'weak', 'mediocre', 'strong', 'very strong']
-        }, options || {});
-        options.indicator = $('#' + this.data('indicator'));
-        
-        return this.keyup(options, updateIndicator);
-    };
+    // password has at least 2 symbols
+    if (password.match(doublecheck)) {
+      score += 5;
+    }
+    // password has Upper and Lower chars
+    if (password.match(/([a-z].*[A-Z])|([A-Z].*[a-z])/)) {
+      score += 10;
+    }
+    // password has number and chars
+    if (password.match(/([a-zA-Z])/) && password.match(/([0-9])/)) {
+      score += 15;
+    }
+    // password has number and symbol
+    if (password.match(check) && password.match(/([0-9])/)) {
+      score += 15;
+    }
+    // password has char and symbol
+    if (password.match(check) && password.match(/([a-zA-Z])/)) {
+      score += 15;
+    }
+    // password is just numbers or chars
+    if (password.match(/^\w+$/) || password.match(/^\d+$/)) {
+      score -= 10;
+    }
+    return Math.floor(Math.max(Math.min(score, 99), 0) / 20);
+  };
+
+  function checkRepetition(rLen, str) {
+    var res = "",
+      repeated = false;
+    for (var i = 0; i < str.length; i++) {
+      repeated = true;
+      for (var j = 0; j < rLen && (j + i + rLen) < str.length; j++) {
+        repeated = repeated && (str.charAt(j + i) === str.charAt(j + i + rLen));
+      }
+      if (j < rLen) {
+        repeated = false;
+      }
+      if (repeated) {
+        i += rLen - 1;
+        repeated = false;
+      } else {
+        res += str.charAt(i);
+      }
+    }
+    return res;
+  }
+
+  function updateIndicator(event) {
+    var strength = $.pwstrength($(this).val()),
+      options = event.data,
+      klass;
+    klass = options.classes[strength];
+    options.indicator.removeClass(options.indicator.data('pwclass'));
+    options.indicator.data('pwclass', klass);
+    options.indicator.addClass(klass);
+    options.indicator.find(options.label).html(options.texts[strength]);
+  }
+  $.fn.pwstrength = function(options) {
+    var options = $.extend({
+      label: '.label',
+      classes: ['pw-very-weak', 'pw-weak', 'pw-mediocre', 'pw-strong', 'pw-very-strong'],
+      texts: ['very weak', 'weak', 'mediocre', 'strong', 'very strong']
+    }, options || {});
+    options.indicator = $('#' + this.data('indicator'));
+    return this.keyup(options, updateIndicator);
+  };
 })(jQuery);
