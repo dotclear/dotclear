c50686f451ad28c65107bc68dd12555e287be433
franck <carnet.franck.paul@gmail.com>
2018-02-15 13:06:24 +0100

    Code formatting

--- admin/js/_media_item.js
+++ admin/js/_media_item.js
@@ -1,81 +1,88 @@
 $(function() {
-	// Add datePicker if possible
-	var media_dt = document.getElementById('media_dt');
-	if (media_dt != undefined) {
-		var post_dtPick = new datePicker(media_dt);
-		post_dtPick.img_top = '1.5em';
-		post_dtPick.draw();
-	}
+  // Add datePicker if possible
+  var media_dt = document.getElementById('media_dt');
+  if (media_dt != undefined) {
+    var post_dtPick = new datePicker(media_dt);
+    post_dtPick.img_top = '1.5em';
+    post_dtPick.draw();
+  }
 
-	// Preview media
-	$('.modal-image').magnificPopup({ type:'image' });
+  // Preview media
+  $('.modal-image').magnificPopup({
+    type: 'image'
+  });
 
-	// Display zip file content
-	$('#file-unzip').each(function() {
-		var a = document.createElement('a');
-		var mediaId = $(this).find('input[name=id]').val();
-		var self = $(this);
+  // Display zip file content
+  $('#file-unzip').each(function() {
+    var a = document.createElement('a');
+    var mediaId = $(this).find('input[name=id]').val();
+    var self = $(this);
 
-		a.href = '#';
-		$(a).text(dotclear.msg.zip_file_content);
-		self.before(a);
-		$(a).wrap('<p></p>');
+    a.href = '#';
+    $(a).text(dotclear.msg.zip_file_content);
+    self.before(a);
+    $(a).wrap('<p></p>');
 
-		$(a).click(function() {
-			$.get('services.php',{f:'getZipMediaContent',id: mediaId},function(data) {
-				var rsp = $(data).children('rsp')[0];
+    $(a).click(function() {
+      $.get('services.php', {
+        f: 'getZipMediaContent',
+        id: mediaId
+      }, function(data) {
+        var rsp = $(data).children('rsp')[0];
 
-				if (rsp.attributes[0].value == 'ok') {
-					var div = document.createElement('div');
-					var list = document.createElement('ul');
-					var expanded = false;
+        if (rsp.attributes[0].value == 'ok') {
+          var div = document.createElement('div');
+          var list = document.createElement('ul');
+          var expanded = false;
 
-					$(div).css({
-						overflow: 'auto',
-						margin: '1em 0',
-						padding: '1px 0.5em'
-					});
-					$(div).addClass('color-div');
-					$(div).append(list);
-					self.before(div);
-					$(a).hide();
-					$(div).before('<h3>' + dotclear.msg.zip_file_content + '</h3>');
+          $(div).css({
+            overflow: 'auto',
+            margin: '1em 0',
+            padding: '1px 0.5em'
+          });
+          $(div).addClass('color-div');
+          $(div).append(list);
+          self.before(div);
+          $(a).hide();
+          $(div).before('<h3>' + dotclear.msg.zip_file_content + '</h3>');
 
-					$(rsp).find('file').each(function() {
-						$(list).append('<li>' + $(this).text() + '</li>');
-						if ($(div).height() > 200 && !expanded) {
-							$(div).css({height: '200px'});
-							expanded = true;
-						}
-					});
-				} else {
-					alert($(rsp).find('message').text());
-				}
-			});
-			return false;
-		});
-	});
+          $(rsp).find('file').each(function() {
+            $(list).append('<li>' + $(this).text() + '</li>');
+            if ($(div).height() > 200 && !expanded) {
+              $(div).css({
+                height: '200px'
+              });
+              expanded = true;
+            }
+          });
+        } else {
+          alert($(rsp).find('message').text());
+        }
+      });
+      return false;
+    });
+  });
 
-	// Confirm for inflating in current directory
-	$('#file-unzip').submit(function() {
-		if ($(this).find('#inflate_mode').val() == 'current') {
-			return window.confirm(dotclear.msg.confirm_extract_current);
-		}
-		return true;
-	});
+  // Confirm for inflating in current directory
+  $('#file-unzip').submit(function() {
+    if ($(this).find('#inflate_mode').val() == 'current') {
+      return window.confirm(dotclear.msg.confirm_extract_current);
+    }
+    return true;
+  });
 
-	// Confirm for deleting current medoa
-	$('#delete-form input[name="delete"]').click(function() {
-		return window.confirm(dotclear.msg.confirm_delete_media);
-	});
+  // Confirm for deleting current medoa
+  $('#delete-form input[name="delete"]').click(function() {
+    return window.confirm(dotclear.msg.confirm_delete_media);
+  });
 
-	// Get current insertion settings
-	$('#save_settings').submit(function() {
-		$('input[name="pref_src"]').val($('input[name="src"][type=radio]:checked').attr('value'));
-		$('input[name="pref_alignment"]').val($('input[name="alignment"][type=radio]:checked').attr('value'));
-		$('input[name="pref_insertion"]').val($('input[name="insertion"][type=radio]:checked').attr('value'));
-		$('input[name="pref_legend"]').val($('input[name="legend"][type=radio]:checked').attr('value'));
-	});
+  // Get current insertion settings
+  $('#save_settings').submit(function() {
+    $('input[name="pref_src"]').val($('input[name="src"][type=radio]:checked').attr('value'));
+    $('input[name="pref_alignment"]').val($('input[name="alignment"][type=radio]:checked').attr('value'));
+    $('input[name="pref_insertion"]').val($('input[name="insertion"][type=radio]:checked').attr('value'));
+    $('input[name="pref_legend"]').val($('input[name="legend"][type=radio]:checked').attr('value'));
+  });
 
   // Set focus if in popup mode
   $('#media-insert-form :input:visible:enabled:checked:first, #media-insert-form :input:visible:enabled:first').focus();
--- admin/popup_posts.php
+++ admin/popup_posts.php
@@ -10,71 +10,71 @@
 #
 # -- END LICENSE BLOCK -----------------------------------------
 
-require dirname(__FILE__).'/../inc/admin/prepend.php';
+require dirname(__FILE__) . '/../inc/admin/prepend.php';
 
 dcPage::check('usage,contentadmin');
 
-$q = !empty($_GET['q']) ? $_GET['q'] : null;
+$q         = !empty($_GET['q']) ? $_GET['q'] : null;
 $plugin_id = !empty($_GET['plugin_id']) ? html::sanitizeURL($_GET['plugin_id']) : '';
 
-$page = !empty($_GET['page']) ? max(1,(integer) $_GET['page']) : 1;
-$nb_per_page =  10;
+$page        = !empty($_GET['page']) ? max(1, (integer) $_GET['page']) : 1;
+$nb_per_page = 10;
 
 $type = !empty($_GET['type']) ? $_GET['type'] : null;
 
 $post_types = $core->getPostTypes();
 foreach ($post_types as $k => $v) {
- 	$type_combo[__($k)] = (string) $k;
+    $type_combo[__($k)] = (string) $k;
 }
 if (!in_array($type, $type_combo)) {
-	$type = null;
+    $type = null;
 }
 
-$params = array();
-$params['limit'] = array((($page-1)*$nb_per_page),$nb_per_page);
+$params               = array();
+$params['limit']      = array((($page - 1) * $nb_per_page), $nb_per_page);
 $params['no_content'] = true;
-$params['order'] = 'post_dt DESC';
+$params['order']      = 'post_dt DESC';
 
 if ($q) {
-	$params['search'] = $q;
+    $params['search'] = $q;
 }
 
 if ($type) {
-	$params['post_type'] = $type;
+    $params['post_type'] = $type;
 }
 
 dcPage::openPopup(__('Add a link to an entry'),
-	dcPage::jsLoad('js/_posts_list.js').
-    dcPage::jsLoad('js/_popup_posts.js').
-	$core->callBehavior('adminPopupPosts', $plugin_id));
+    dcPage::jsLoad('js/_posts_list.js') .
+    dcPage::jsLoad('js/_popup_posts.js') .
+    $core->callBehavior('adminPopupPosts', $plugin_id));
 
-echo '<h2 class="page-title">'.__('Add a link to an entry').'</h2>';
+echo '<h2 class="page-title">' . __('Add a link to an entry') . '</h2>';
 
-echo '<form action="'.$core->adminurl->get('admin.popup_posts').'" method="get">'.
-	'<p><label for="type" class="classic">'.__('Entry type:').'</label> '.form::combo('type',$type_combo,$type).''.
-	'<noscript><div><input type="submit" value="'.__('Ok').'" /></div></noscript>'.
-	form::hidden('plugin_id',html::escapeHTML($plugin_id)).'</p>'.
-	'</form>';
+echo '<form action="' . $core->adminurl->get('admin.popup_posts') . '" method="get">' .
+'<p><label for="type" class="classic">' . __('Entry type:') . '</label> ' . form::combo('type', $type_combo, $type) . '' .
+'<noscript><div><input type="submit" value="' . __('Ok') . '" /></div></noscript>' .
+form::hidden('plugin_id', html::escapeHTML($plugin_id)) . '</p>' .
+    '</form>';
 
-echo '<form action="'.$core->adminurl->get('admin.popup_posts').'" method="get">'.
-	'<p><label for="q" class="classic">'.__('Search entry:').'</label> '.form::field('q',30,255,html::escapeHTML($q)).
-	' <input type="submit" value="'.__('Search').'" />'.
-	form::hidden('plugin_id',html::escapeHTML($plugin_id)).
-	form::hidden('type',html::escapeHTML($type)).
-	'</p></form>';
+echo '<form action="' . $core->adminurl->get('admin.popup_posts') . '" method="get">' .
+'<p><label for="q" class="classic">' . __('Search entry:') . '</label> ' . form::field('q', 30, 255, html::escapeHTML($q)) .
+' <input type="submit" value="' . __('Search') . '" />' .
+form::hidden('plugin_id', html::escapeHTML($plugin_id)) .
+form::hidden('type', html::escapeHTML($type)) .
+    '</p></form>';
 
 try {
-	$posts = $core->blog->getPosts($params);
-	$counter = $core->blog->getPosts($params,true);
-	$post_list = new adminPostMiniList($core,$posts,$counter->f(0));
+    $posts     = $core->blog->getPosts($params);
+    $counter   = $core->blog->getPosts($params, true);
+    $post_list = new adminPostMiniList($core, $posts, $counter->f(0));
 } catch (Exception $e) {
-	$core->error->add($e->getMessage());
+    $core->error->add($e->getMessage());
 }
 
 echo '<div id="form-entries">'; # I know it's not a form but we just need the ID
-$post_list->display($page,$nb_per_page);
+$post_list->display($page, $nb_per_page);
 echo '</div>';
 
-echo '<p><button type="button" id="link-insert-cancel">'.__('cancel').'</button></p>';
+echo '<p><button type="button" id="link-insert-cancel">' . __('cancel') . '</button></p>';
 
 dcPage::closePopup();
