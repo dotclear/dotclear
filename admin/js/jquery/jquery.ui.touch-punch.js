6d98a434ff062500119fbfb4e98deed39d627fa2
franck <carnet.franck.paul@gmail.com>
2018-10-22 16:30:10 +0200

    Fix jQuery UI touch-punch plugin in order to give access to input/textarea fields on sortable elements.
    See this PR: https://github.com/furf/jquery-ui-touch-punch/pull/109

--- admin/js/jquery/jquery.ui.touch-punch.js
+++ admin/js/jquery/jquery.ui.touch-punch.js
@@ -8,7 +8,7 @@
  *  jquery.ui.widget.js
  *  jquery.ui.mouse.js
  */
-(function ($) {
+(function($) {
 
   // Detect touch support
   $.support.touch = 'ontouchend' in document;
@@ -19,44 +19,51 @@
   }
 
   var mouseProto = $.ui.mouse.prototype,
-      _mouseInit = mouseProto._mouseInit,
-      _mouseDestroy = mouseProto._mouseDestroy,
-      touchHandled;
+    _mouseInit = mouseProto._mouseInit,
+    _mouseDestroy = mouseProto._mouseDestroy,
+    touchHandled;
 
   /**
    * Simulate a mouse event based on a corresponding touch event
    * @param {Object} event A touch event
    * @param {String} simulatedType The corresponding mouse event
    */
-  function simulateMouseEvent (event, simulatedType) {
+  function simulateMouseEvent(event, simulatedType) {
 
     // Ignore multi-touch events
     if (event.originalEvent.touches.length > 1) {
       return;
     }
 
-    event.preventDefault();
+//    event.preventDefault();
 
     var touch = event.originalEvent.changedTouches[0],
-        simulatedEvent = document.createEvent('MouseEvents');
-    
+      simulatedEvent = document.createEvent('MouseEvents');
+
+    // Check if element is an input or a textarea
+    if ($(touch.target).is("input") || $(touch.target).is("textarea")) {
+      event.stopPropagation();
+    } else {
+      event.preventDefault();
+    }
+
     // Initialize the simulated mouse event using the touch event's coordinates
     simulatedEvent.initMouseEvent(
-      simulatedType,    // type
-      true,             // bubbles                    
-      true,             // cancelable                 
-      window,           // view                       
-      1,                // detail                     
-      touch.screenX,    // screenX                    
-      touch.screenY,    // screenY                    
-      touch.clientX,    // clientX                    
-      touch.clientY,    // clientY                    
-      false,            // ctrlKey                    
-      false,            // altKey                     
-      false,            // shiftKey                   
-      false,            // metaKey                    
-      0,                // button                     
-      null              // relatedTarget              
+      simulatedType, // type
+      true, // bubbles
+      true, // cancelable
+      window, // view
+      1, // detail
+      touch.screenX, // screenX
+      touch.screenY, // screenY
+      touch.clientX, // clientX
+      touch.clientY, // clientY
+      false, // ctrlKey
+      false, // altKey
+      false, // shiftKey
+      false, // metaKey
+      0, // button
+      null // relatedTarget
     );
 
     // Dispatch the simulated event to the target element
@@ -67,7 +74,7 @@
    * Handle the jQuery UI widget's touchstart events
    * @param {Object} event The widget element's touchstart event
    */
-  mouseProto._touchStart = function (event) {
+  mouseProto._touchStart = function(event) {
 
     var self = this;
 
@@ -96,7 +103,7 @@
    * Handle the jQuery UI widget's touchmove events
    * @param {Object} event The document's touchmove event
    */
-  mouseProto._touchMove = function (event) {
+  mouseProto._touchMove = function(event) {
 
     // Ignore event if not handled
     if (!touchHandled) {
@@ -114,7 +121,7 @@
    * Handle the jQuery UI widget's touchend events
    * @param {Object} event The document's touchend event
    */
-  mouseProto._touchEnd = function (event) {
+  mouseProto._touchEnd = function(event) {
 
     // Ignore event if not handled
     if (!touchHandled) {
@@ -144,8 +151,8 @@
    * translate touch events to mouse events and pass them to the widget's
    * original mouse event handling methods.
    */
-  mouseProto._mouseInit = function () {
-    
+  mouseProto._mouseInit = function() {
+
     var self = this;
 
     // Delegate the touch handlers to the widget's element
@@ -162,8 +169,8 @@
   /**
    * Remove the touch event handlers
    */
-  mouseProto._mouseDestroy = function () {
-    
+  mouseProto._mouseDestroy = function() {
+
     var self = this;
 
     // Delegate the touch handlers to the widget's element
