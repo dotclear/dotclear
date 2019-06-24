fdd2b5c0df1f63eec49d981b6e928b84d8ad73eb
franck <carnet.franck.paul@gmail.com>
2019-06-24 14:29:57 +0200

    Switching from inline JS variables to JSON script, theme editor plugin

---
+++ plugins/themeEditor/js/theme.js
@@ -0,0 +1,25 @@
+/*global $, CodeMirror, getData */
+'use strict';
+
+$(function() {
+  //const input = document.getElementById("colorsyntax_theme");
+  //var theme = input.options[input.selectedIndex].textContent;
+  const current = getData('theme_editor_current');
+  var editor = CodeMirror.fromTextArea(document.getElementById("codemirror"), {
+    mode: "javascript",
+    tabMode: "indent",
+    lineWrapping: 1,
+    lineNumbers: 1,
+    matchBrackets: 1,
+    autoCloseBrackets: 1,
+    theme: current.theme != '' ? current.theme : 'default'
+  });
+
+  $('#colorsyntax_theme').on('change', function() {
+    var input = document.getElementById("colorsyntax_theme");
+    var theme = input.options[input.selectedIndex].value;
+    if (theme == "") theme = "default";
+    editor.setOption("theme", theme);
+    editor.refresh();
+  });
+});
