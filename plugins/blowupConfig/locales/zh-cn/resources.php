0d4d3e4a66a358b0b31328962fbc21b81f5ec32f
franck <carnet.franck.paul@gmail.com>
2020-04-30 17:37:21 +0200

    Move localized plugins's help files where they belong (in plugins's locales/xx/help)

---
+++ plugins/blowupConfig/locales/zh-cn/resources.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @package Dotclear
+ * @subpackage Plugins
+ *
+ * @copyright Olivier Meunier & Association Dotclear
+ * @copyright GPL-2.0-only
+ */
+
+if (!isset($__resources['help']['blowupConfig'])) {
+    $__resources['help']['blowupConfig'] = dirname(__FILE__) . '/help/help.html';
+}
