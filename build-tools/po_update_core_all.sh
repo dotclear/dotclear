e10c5f8d98c7371061b461be80a382798f2bd9f0
franck <carnet.franck.paul@gmail.com>
2020-12-27 09:10:29 +0100

    Add tool to rebuild all locales

---
+++ build-tools/po_update_core_all.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# @package Dotclear
+#
+# @copyright Olivier Meunier & Association Dotclear
+# @copyright GPL-2.0-only
+#
+# Usage (from dotclear root)
+# po_update_core_all.sh
+
+if [ ! -d ./inc/core ]; then
+  echo "You are not on Dotclear root directory"
+  exit 1
+fi
+
+LANGS=$(cd locales && ls -d */ | sed -e "s/\///g" | grep -v "_pot")
+for l in $LANGS; do
+  ./build-tools/po_update.sh $l
+done
