ee9626203ac3bd3804d2fb5be78fa6c2e44fabd8
franck <carnet.franck.paul@gmail.com>
2021-03-06 18:12:23 +0100

    Update CKEditor from 4.15.1 to 4.16.0

--- plugins/dcCKEditor/js/ckeditor/config.js
+++ plugins/dcCKEditor/js/ckeditor/config.js
@@ -1,10 +1,11 @@
 /**
- * @license Copyright (c) 2003-2020, CKSource - Frederico Knabben. All rights reserved.
+ * @license Copyright (c) 2003-2021, CKSource - Frederico Knabben. All rights reserved.
  * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
  */
 
-CKEDITOR.editorConfig = function (config) {
-  // Define changes to default configuration here. For example:
-  // config.language = 'fr';
-  // config.uiColor = '#AADC6E';
+CKEDITOR.editorConfig = function( config ) {
+	// Define changes to default configuration here. For example:
+	// config.language = 'fr';
+	// config.uiColor = '#AADC6E';
 };
+
