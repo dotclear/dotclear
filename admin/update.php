a93dfd1298b5ee66c8f3aca711b29810d3198d61
franck <carnet.franck.paul@gmail.com>
2021-08-14 12:54:59 +0200

    Enforce visibility of last action « button » on update. Closes #104

--- admin/update.php
+++ admin/update.php
@@ -257,7 +257,7 @@ if (!$step) {
     echo
     '<p class="message">' .
     __("Congratulations, you're one click away from the end of the update.") .
-    ' <strong><a href="index.php?logout=1">' . __('Finish the update.') . '</a></strong>' .
+    ' <strong><a href="index.php?logout=1" class="button submit">' . __('Finish the update.') . '</a></strong>' .
         '</p>';
 }
 
