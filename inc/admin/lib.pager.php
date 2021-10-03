e81b8b002f4bddabe1320a101920cf67b27d1f4e
Jean-Christian Denis <contact@jcdenis.fr>
2021-10-03 14:35:40 +0200

    fix typo

--- inc/admin/lib.pager.php
+++ inc/admin/lib.pager.php
@@ -664,10 +664,10 @@ class adminCommentList extends adminGenericList
      */
     private function commentLine($checked = false, $spam = false, $filters = [])
     {
-        global $core, $author, $status, $sortby, $order, $nb_per_page;
+        global $author, $status, $sortby, $order, $nb;
 
         $author_url = $this->core->adminurl->get('admin.comments', [
-            'n'      => $nb_per_page,
+            'nb'     => $nb,
             'status' => $status,
             'sortby' => $sortby,
             'order'  => $order,
@@ -742,7 +742,7 @@ class adminCommentList extends adminGenericList
                 }
             }
             $cols['ip'] = '<td class="nowrap"><a href="' .
-            $core->adminurl->get('admin.comments', ['ip' => $this->rs->comment_ip]) . '">' .
+            $this->core->adminurl->get('admin.comments', ['ip' => $this->rs->comment_ip]) . '">' .
             $this->rs->comment_ip . '</a></td>';
             $cols['spam_filter'] = '<td class="nowrap">' . $filter_name . '</td>';
         }
