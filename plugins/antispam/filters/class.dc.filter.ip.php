415f3af9fb8f9d54fc04f47cdbc7fcde4b62177f
franck <carnet.franck.paul@gmail.com>
2021-10-18 16:06:37 +0200

    Fix IPv4 antispam filter for subnet mask > 32

--- plugins/antispam/filters/class.dc.filter.ip.php
+++ plugins/antispam/filters/class.dc.filter.ip.php
@@ -211,7 +211,7 @@ class dcFilterIP extends dcSpamFilter
                 $mask = -1;
             }
         } else {
-            $mask = ~((1 << (32 - (integer) $bits[1])) - 1);
+            $mask = ~((1 << (32 - min((integer) $bits[1], 32))) - 1);
         }
     }
 
