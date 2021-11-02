7b15c0d81b9420201a702cf6d8e6f3fb5fa9a97f
franck <carnet.franck.paul@gmail.com>
2021-11-02 15:21:25 +0100

    Javascript code review (using modern ES as far as possible)

--- admin/js/jquery/jquery.pageTabs.js
+++ admin/js/jquery/jquery.pageTabs.js
@@ -1,15 +1,14 @@
 /*global jQuery */
 'use strict';
 
-(function($) {
-
-  $.pageTabs = function(start_tab, opts) {
+(($) => {
+  $.pageTabs = function (start_tab, opts) {
     const defaults = {
       containerClass: 'part-tabs',
       partPrefix: 'part-',
       contentClass: 'multi-part',
       activeClass: 'part-tabs-active',
-      idTabPrefix: 'part-tabs-'
+      idTabPrefix: 'part-tabs-',
     };
 
     $.pageTabs.options = $.extend({}, defaults, opts);
@@ -20,22 +19,23 @@
     if (hash !== undefined && hash) {
       window.scrollTo(0, 0);
       active_tab = hash;
-    } else if (active_tab == '') { // open first part
-      active_tab = $('.' + $.pageTabs.options.contentClass + ':eq(0)').attr('id');
+    } else if (active_tab == '') {
+      // open first part
+      active_tab = $(`.${$.pageTabs.options.contentClass}:eq(0)`).attr('id');
     }
 
     createTabs();
 
-    $('ul li', '.' + $.pageTabs.options.containerClass).on('click', function() {
+    $('ul li', `.${$.pageTabs.options.containerClass}`).on('click', function () {
       if ($(this).hasClass($.pageTabs.options.activeClass)) {
         return;
       }
 
-      $(this).parent().find('li.' + $.pageTabs.options.activeClass).removeClass($.pageTabs.options.activeClass);
+      $(this).parent().find(`li.${$.pageTabs.options.activeClass}`).removeClass($.pageTabs.options.activeClass);
       $(this).addClass($.pageTabs.options.activeClass);
-      $('.' + $.pageTabs.options.contentClass + '.active').removeClass('active').hide();
+      $(`.${$.pageTabs.options.contentClass}.active`).removeClass('active').hide();
 
-      const part_to_activate = $('#' + $.pageTabs.options.partPrefix + getHash($(this).find('a').attr('href')));
+      const part_to_activate = $(`#${$.pageTabs.options.partPrefix}${getHash($(this).find('a').attr('href'))}`);
 
       part_to_activate.addClass('active').show();
       if (!part_to_activate.hasClass('loaded')) {
@@ -46,7 +46,7 @@
       part_to_activate.trigger('tabload');
     });
 
-    $(window).on('hashchange onhashchange', function() {
+    $(window).on('hashchange onhashchange', () => {
       $.pageTabs.clickTab($.pageTabs.getLocationHash());
     });
 
@@ -67,9 +67,11 @@
       // Tab displayed, now scroll to the sub-part if defined in original document.location (#tab.sub-part)
       elt.scrollIntoView();
       // Give focus to the sub-part if possible
-      $('#' + subhash).addClass('focus').on('focusout', function() {
-        $(this).removeClass('focus');
-      });
+      $(`#${subhash}`)
+        .addClass('focus')
+        .on('focusout', function () {
+          $(this).removeClass('focus');
+        });
       elt.focus();
     }
 
@@ -79,51 +81,60 @@
   const createTabs = function createTabs() {
     let lis = [];
 
-    $('.' + $.pageTabs.options.contentClass).each(function() {
+    $(`.${$.pageTabs.options.contentClass}`).each(function () {
       $(this).hide();
-      lis.push('<li id="' + $.pageTabs.options.idTabPrefix + $(this).attr('id') + '">' +
-        '<a href="#' + $(this).attr('id') + '">' + $(this).attr('title') + '</a></li>');
-      $(this).attr('id', $.pageTabs.options.partPrefix + $(this).attr('id')).prop('title', '');
+      lis.push(
+        `<li id="${$.pageTabs.options.idTabPrefix}${$(this).attr('id')}">` +
+          '<a href="#' +
+          $(this).attr('id') +
+          '">' +
+          $(this).attr('title') +
+          '</a></li>'
+      );
+      $(this)
+        .attr('id', $.pageTabs.options.partPrefix + $(this).attr('id'))
+        .prop('title', '');
     });
 
-    $('<div class="' + $.pageTabs.options.containerClass + '"><ul>' + lis.join('') + '</ul></div>')
-      .insertBefore($('.' + $.pageTabs.options.contentClass).get(0));
+    $(`<div class="${$.pageTabs.options.containerClass}"><ul>${lis.join('')}</ul></div>`).insertBefore(
+      $(`.${$.pageTabs.options.contentClass}`).get(0)
+    );
   };
 
-  const getHash = function getHash(href) {
-    href = href || '';
-
+  const getHash = function getHash(href = '') {
     return href.replace(/.*#/, '');
   };
 
-  $.pageTabs.clickTab = function(tab) {
+  $.pageTabs.clickTab = (tab) => {
     if (tab == '') {
-      tab = getHash($('ul li a', '.' + $.pageTabs.options.containerClass + ':eq(0)').attr('href'));
-    } else if ($('#' + $.pageTabs.options.idTabPrefix + tab, '.' + $.pageTabs.options.containerClass).length == 0) {
+      tab = getHash($('ul li a', `.${$.pageTabs.options.containerClass}:eq(0)`).attr('href'));
+    } else if ($(`#${$.pageTabs.options.idTabPrefix}${tab}`, `.${$.pageTabs.options.containerClass}`).length == 0) {
       // try to find anchor in a .multi-part div
-      if ($('#' + tab).length == 1) {
-        const div_content = $('#' + tab).parents('.' + $.pageTabs.options.contentClass);
-        if (div_content.length == 1) {
-          tab = div_content.attr('id').replace($.pageTabs.options.partPrefix, '');
-        } else {
-          tab = getHash($('ul li a', '.' + $.pageTabs.options.containerClass + ':eq(0)').attr('href'));
-        }
+      if ($(`#${tab}`).length == 1) {
+        const div_content = $(`#${tab}`).parents(`.${$.pageTabs.options.contentClass}`);
+        tab =
+          div_content.length == 1
+            ? div_content.attr('id').replace($.pageTabs.options.partPrefix, '')
+            : getHash($('ul li a', `.${$.pageTabs.options.containerClass}:eq(0)`).attr('href'));
       } else {
-        tab = getHash($('ul li a', '.' + $.pageTabs.options.containerClass + ':eq(0)').attr('href'));
+        tab = getHash($('ul li a', `.${$.pageTabs.options.containerClass}:eq(0)`).attr('href'));
       }
     }
 
-    $('ul li a', '.' + $.pageTabs.options.containerClass).filter(function() {
-      return getHash($(this).attr('href')) == tab;
-    }).parent().trigger('click');
+    $('ul li a', `.${$.pageTabs.options.containerClass}`)
+      .filter(function () {
+        return getHash($(this).attr('href')) == tab;
+      })
+      .parent()
+      .trigger('click');
   };
 
-  $.pageTabs.getLocationHash = function() {
+  $.pageTabs.getLocationHash = () => {
     // Return the URL hash (without subhash — #hash[.subhash])
     const h = getHash(document.location.hash).split('.');
     return h[0];
   };
-  $.pageTabs.getLocationSubhash = function() {
+  $.pageTabs.getLocationSubhash = () => {
     // Return the URL subhash if present (without hash — #hash[.subhash])
     const sh = getHash(document.location.hash).split('.');
     return sh[1];
