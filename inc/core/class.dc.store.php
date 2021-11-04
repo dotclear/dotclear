6505aaeaa30ed46f8a78a7a3a6f32d780f137c60
franck <carnet.franck.paul@gmail.com>
2021-11-04 16:00:27 +0100

    Avoid false positive with PHPStan

--- inc/core/class.dc.store.php
+++ inc/core/class.dc.store.php
@@ -63,6 +63,7 @@ class dcStore
         }
 
         try {
+            /* @phpstan-ignore-next-line */
             $parser = DC_DA_NOT_UPDATE ? false : dcStoreReader::quickParse($this->xml_url, DC_TPL_CACHE, $force);
         } catch (Exception $e) {
             return false;
