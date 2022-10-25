e6dae949d93a6c843379e0f7d468804fe746e054
franck <carnet.franck.paul@gmail.com>
2022-10-25 13:40:53 +0200

    Compass config reviewed, compact CSS output and remove compass framework from source

--- admin/style/config.rb
+++ admin/style/config.rb
@@ -6,13 +6,35 @@ css_dir = "./"
 sass_dir = "scss"
 images_dir = "./"
 
-# You can select your preferred output style here (can be overridden via the command line):
-# output_style = :expanded or :nested or :compact or :compressed
-# To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = true
-# To disable debugging comments that display the original location of your selectors. Uncomment:
-output_style = :nested
-environment = :development
-line_comments = false
+#environment = :development
+environment = :production
+
+if environment == :development
+  # You can select your preferred output style here (can be overridden via the command line):
+  # output_style = :expanded or :nested or :compact or :compressed
+  output_style = :nested
+
+  # To enable relative paths to assets via compass helper functions. Uncomment:
+  # relative_assets = true
+
+  # To disable debugging comments that display the original location of your selectors. Uncomment:
+  line_comments = true
+
+  sass_options = {:debug_info => false}
+end
+
+if environment == :production
+  # You can select your preferred output style here (can be overridden via the command line):
+  # output_style = :expanded or :nested or :compact or :compressed
+  output_style = :compact
+
+  # To enable relative paths to assets via compass helper functions. Uncomment:
+  # relative_assets = true
+
+  # To disable debugging comments that display the original location of your selectors. Uncomment:
+  line_comments = false
+
+  sass_options = {:debug_info => false}
+end
 
 Sass::Script::Number.precision = 7
