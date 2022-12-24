ac7a322a06782009f62341e286fb1bf80cf52d06
franck <carnet.franck.paul@gmail.com>
2022-12-24 16:07:20 +0100

    add aria-current="page" for current page (pages widget), addresses #222

--- plugins/pages/inc/public.tpl.php
+++ plugins/pages/inc/public.tpl.php
@@ -54,7 +54,7 @@ class tplPages
         while ($rs->fetch()) {
             $class = '';
             if (dcCore::app()->url->type === 'pages' && dcCore::app()->ctx->posts instanceof dcRecord && dcCore::app()->ctx->posts->post_id == $rs->post_id) {
-                $class = ' class="page-current"';
+                $class = ' class="page-current" aria-current="page"';
             }
             $res .= '<li' . $class . '><a href="' . $rs->getURL() . '">' .
             html::escapeHTML($rs->post_title) . '</a></li>';
