fd820a50674c72963cd06723677c5db7646dfffb
franck <carnet.franck.paul@gmail.com>
2023-01-21 12:02:56 +0100

    Move Autoloader

--- inc/helper/_common.php
+++ inc/helper/_common.php
@@ -97,79 +97,80 @@ class Clearbricks
         $this->autoloader = new Autoload();
 
         $this->autoloader->add([
+            'Autoloader'                => __DIR__ . '/Autoloader.php',
+
             // Common helpers
-            'Autoloader'       => __DIR__ . '/common/Autoloader.php',
-            'crypt'            => __DIR__ . '/common/lib.crypt.php',
-            'dt'               => __DIR__ . '/common/lib.date.php',
-            'files'            => __DIR__ . '/common/lib.files.php',
-            'path'             => __DIR__ . '/common/lib.files.php',
-            'form'             => __DIR__ . '/common/lib.form.php',
-            'formSelectOption' => __DIR__ . '/common/lib.form.php',
-            'html'             => __DIR__ . '/common/lib.html.php',
-            'http'             => __DIR__ . '/common/lib.http.php',
-            'l10n'             => __DIR__ . '/common/lib.l10n.php',
-            'text'             => __DIR__ . '/common/lib.text.php',
+            'crypt'                     => __DIR__ . '/common/lib.crypt.php',
+            'dt'                        => __DIR__ . '/common/lib.date.php',
+            'files'                     => __DIR__ . '/common/lib.files.php',
+            'path'                      => __DIR__ . '/common/lib.files.php',
+            'form'                      => __DIR__ . '/common/lib.form.php',
+            'formSelectOption'          => __DIR__ . '/common/lib.form.php',
+            'html'                      => __DIR__ . '/common/lib.html.php',
+            'http'                      => __DIR__ . '/common/lib.http.php',
+            'l10n'                      => __DIR__ . '/common/lib.l10n.php',
+            'text'                      => __DIR__ . '/common/lib.text.php',
 
             // Database Abstraction Layer
-            'dbLayer'  => __DIR__ . '/dblayer/dblayer.php',
-            'dbStruct' => __DIR__ . '/dbschema/class.dbstruct.php',
-            'dbSchema' => __DIR__ . '/dbschema/class.dbschema.php',
+            'dbLayer'                   => __DIR__ . '/dblayer/dblayer.php',
+            'dbStruct'                  => __DIR__ . '/dbschema/class.dbstruct.php',
+            'dbSchema'                  => __DIR__ . '/dbschema/class.dbschema.php',
 
             // Files Manager
-            'filemanager' => __DIR__ . '/filemanager/class.filemanager.php',
-            'fileItem'    => __DIR__ . '/filemanager/class.filemanager.php',
+            'filemanager'               => __DIR__ . '/filemanager/class.filemanager.php',
+            'fileItem'                  => __DIR__ . '/filemanager/class.filemanager.php',
 
             // Feed Reader
-            'feedParser' => __DIR__ . '/net.http.feed/class.feed.parser.php',
-            'feedReader' => __DIR__ . '/net.http.feed/class.feed.reader.php',
+            'feedParser'                => __DIR__ . '/net.http.feed/class.feed.parser.php',
+            'feedReader'                => __DIR__ . '/net.http.feed/class.feed.reader.php',
 
             // HTML Filter
-            'htmlFilter' => __DIR__ . '/html.filter/class.html.filter.php',
+            'htmlFilter'                => __DIR__ . '/html.filter/class.html.filter.php',
 
             // HTML Validator
-            'htmlValidator' => __DIR__ . '/html.validator/class.html.validator.php',
+            'htmlValidator'             => __DIR__ . '/html.validator/class.html.validator.php',
 
             // Image Manipulation Tools
-            'imageMeta'  => __DIR__ . '/image/class.image.meta.php',
-            'imageTools' => __DIR__ . '/image/class.image.tools.php',
+            'imageMeta'                 => __DIR__ . '/image/class.image.meta.php',
+            'imageTools'                => __DIR__ . '/image/class.image.tools.php',
 
             // Send Mail Utilities
-            'mail' => __DIR__ . '/mail/class.mail.php',
+            'mail'                      => __DIR__ . '/mail/class.mail.php',
 
             // Send Mail Through Sockets
-            'socketMail' => __DIR__ . '/mail/class.socket.mail.php',
+            'socketMail'                => __DIR__ . '/mail/class.socket.mail.php',
 
             // HTML Pager
-            'pager' => __DIR__ . '/pager/class.pager.php',
+            'pager'                     => __DIR__ . '/pager/class.pager.php',
 
             // REST Server
-            'restServer' => __DIR__ . '/rest/class.rest.php',
-            'xmlTag'     => __DIR__ . '/rest/class.rest.php',
+            'restServer'                => __DIR__ . '/rest/class.rest.php',
+            'xmlTag'                    => __DIR__ . '/rest/class.rest.php',
 
             // Database PHP Session
-            'sessionDB' => __DIR__ . '/session.db/class.session.db.php',
+            'sessionDB'                 => __DIR__ . '/session.db/class.session.db.php',
 
             // Simple Template Systeme
-            'template'               => __DIR__ . '/template/class.template.php',
-            'tplNode'                => __DIR__ . '/template/class.tplnode.php',
-            'tplNodeBlock'           => __DIR__ . '/template/class.tplnodeblock.php',
-            'tplNodeText'            => __DIR__ . '/template/class.tplnodetext.php',
-            'tplNodeValue'           => __DIR__ . '/template/class.tplnodevalue.php',
-            'tplNodeBlockDefinition' => __DIR__ . '/template/class.tplnodeblockdef.php',
-            'tplNodeValueParent'     => __DIR__ . '/template/class.tplnodevalueparent.php',
+            'template'                  => __DIR__ . '/template/class.template.php',
+            'tplNode'                   => __DIR__ . '/template/class.tplnode.php',
+            'tplNodeBlock'              => __DIR__ . '/template/class.tplnodeblock.php',
+            'tplNodeText'               => __DIR__ . '/template/class.tplnodetext.php',
+            'tplNodeValue'              => __DIR__ . '/template/class.tplnodevalue.php',
+            'tplNodeBlockDefinition'    => __DIR__ . '/template/class.tplnodeblockdef.php',
+            'tplNodeValueParent'        => __DIR__ . '/template/class.tplnodevalueparent.php',
 
             // URL Handler
-            'urlHandler' => __DIR__ . '/url.handler/class.url.handler.php',
+            'urlHandler'                => __DIR__ . '/url.handler/class.url.handler.php',
 
             // Wiki to XHTML Converter
-            'wiki2xhtml' => __DIR__ . '/text.wiki2xhtml/class.wiki2xhtml.php',
+            'wiki2xhtml'                => __DIR__ . '/text.wiki2xhtml/class.wiki2xhtml.php',
 
             // Common Socket Class
-            'netSocket' => __DIR__ . '/net/class.net.socket.php',
+            'netSocket'                 => __DIR__ . '/net/class.net.socket.php',
 
             // HTTP Client
-            'netHttp'    => __DIR__ . '/net.http/class.net.http.php',
-            'HttpClient' => __DIR__ . '/net.http/class.net.http.php',
+            'netHttp'                   => __DIR__ . '/net.http/class.net.http.php',
+            'HttpClient'                => __DIR__ . '/net.http/class.net.http.php',
 
             // XML-RPC Client and Server
             'xmlrpcValue'               => __DIR__ . '/net.xmlrpc/class.net.xmlrpc.php',
@@ -183,38 +184,38 @@ class Clearbricks
             'xmlrpcIntrospectionServer' => __DIR__ . '/net.xmlrpc/class.net.xmlrpc.php',
 
             // Zip tools
-            'fileUnzip' => __DIR__ . '/zip/class.unzip.php',
-            'fileZip'   => __DIR__ . '/zip/class.zip.php',
+            'fileUnzip'                 => __DIR__ . '/zip/class.unzip.php',
+            'fileZip'                   => __DIR__ . '/zip/class.zip.php',
 
             // Diff tools
-            'diff'     => __DIR__ . '/diff/lib.diff.php',
-            'tidyDiff' => __DIR__ . '/diff/lib.tidy.diff.php',
+            'diff'                      => __DIR__ . '/diff/lib.diff.php',
+            'tidyDiff'                  => __DIR__ . '/diff/lib.tidy.diff.php',
 
             // HTML Form helpers
-            'formComponent' => __DIR__ . '/html.form/class.form.component.php',
-            'formForm'      => __DIR__ . '/html.form/class.form.form.php',
-            'formTextarea'  => __DIR__ . '/html.form/class.form.textarea.php',
-            'formInput'     => __DIR__ . '/html.form/class.form.input.php',
-            'formButton'    => __DIR__ . '/html.form/class.form.button.php',
-            'formCheckbox'  => __DIR__ . '/html.form/class.form.checkbox.php',
-            'formColor'     => __DIR__ . '/html.form/class.form.color.php',
-            'formDate'      => __DIR__ . '/html.form/class.form.date.php',
-            'formDatetime'  => __DIR__ . '/html.form/class.form.datetime.php',
-            'formEmail'     => __DIR__ . '/html.form/class.form.email.php',
-            'formFile'      => __DIR__ . '/html.form/class.form.file.php',
-            'formHidden'    => __DIR__ . '/html.form/class.form.hidden.php',
-            'formNumber'    => __DIR__ . '/html.form/class.form.number.php',
-            'formPassword'  => __DIR__ . '/html.form/class.form.password.php',
-            'formRadio'     => __DIR__ . '/html.form/class.form.radio.php',
-            'formSubmit'    => __DIR__ . '/html.form/class.form.submit.php',
-            'formTime'      => __DIR__ . '/html.form/class.form.time.php',
-            'formUrl'       => __DIR__ . '/html.form/class.form.url.php',
-            'formLabel'     => __DIR__ . '/html.form/class.form.label.php',
-            'formFieldset'  => __DIR__ . '/html.form/class.form.fieldset.php',
-            'formLegend'    => __DIR__ . '/html.form/class.form.legend.php',
-            'formSelect'    => __DIR__ . '/html.form/class.form.select.php',
-            'formOptgroup'  => __DIR__ . '/html.form/class.form.optgroup.php',
-            'formOption'    => __DIR__ . '/html.form/class.form.option.php',
+            'formComponent'             => __DIR__ . '/html.form/class.form.component.php',
+            'formForm'                  => __DIR__ . '/html.form/class.form.form.php',
+            'formTextarea'              => __DIR__ . '/html.form/class.form.textarea.php',
+            'formInput'                 => __DIR__ . '/html.form/class.form.input.php',
+            'formButton'                => __DIR__ . '/html.form/class.form.button.php',
+            'formCheckbox'              => __DIR__ . '/html.form/class.form.checkbox.php',
+            'formColor'                 => __DIR__ . '/html.form/class.form.color.php',
+            'formDate'                  => __DIR__ . '/html.form/class.form.date.php',
+            'formDatetime'              => __DIR__ . '/html.form/class.form.datetime.php',
+            'formEmail'                 => __DIR__ . '/html.form/class.form.email.php',
+            'formFile'                  => __DIR__ . '/html.form/class.form.file.php',
+            'formHidden'                => __DIR__ . '/html.form/class.form.hidden.php',
+            'formNumber'                => __DIR__ . '/html.form/class.form.number.php',
+            'formPassword'              => __DIR__ . '/html.form/class.form.password.php',
+            'formRadio'                 => __DIR__ . '/html.form/class.form.radio.php',
+            'formSubmit'                => __DIR__ . '/html.form/class.form.submit.php',
+            'formTime'                  => __DIR__ . '/html.form/class.form.time.php',
+            'formUrl'                   => __DIR__ . '/html.form/class.form.url.php',
+            'formLabel'                 => __DIR__ . '/html.form/class.form.label.php',
+            'formFieldset'              => __DIR__ . '/html.form/class.form.fieldset.php',
+            'formLegend'                => __DIR__ . '/html.form/class.form.legend.php',
+            'formSelect'                => __DIR__ . '/html.form/class.form.select.php',
+            'formOptgroup'              => __DIR__ . '/html.form/class.form.optgroup.php',
+            'formOption'                => __DIR__ . '/html.form/class.form.option.php',
         ]);
 
         // We may need l10n __() function
