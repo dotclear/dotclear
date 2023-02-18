8a65a8a336b3faa66336b154e2c6529f53ed495f
franck <carnet.franck.paul@gmail.com>
2023-02-18 13:07:26 +0100

    Release 2.25.1

--- inc/prepend.php
+++ inc/prepend.php
@@ -196,7 +196,7 @@ if (!defined('DC_DEBUG')) {
 
 # Constants
 define('DC_ROOT', path::real(dcUtils::path([__DIR__, '..'])));
-define('DC_VERSION', '2.25.0');
+define('DC_VERSION', '2.25.1');
 define('DC_DIGESTS', dcUtils::path([__DIR__, 'digests']));
 define('DC_L10N_ROOT', dcUtils::path([__DIR__, '..', 'locales']));
 define('DC_L10N_UPDATE_URL', 'https://services.dotclear.net/dc2.l10n/?version=%s');
