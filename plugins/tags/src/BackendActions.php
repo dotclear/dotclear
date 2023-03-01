342e92b8f93ede5fc2edbabd7eb5b405ba66a478
Franck Paul <franck@noreply.git.dotclear.org>
2023-03-01 08:15:57 +0100

    Merge pull request 'fix posts tag actions render' (#296) from JcDenis/dotclear:master into master
    Reviewed-on: https://git.dotclear.org/dev/dotclear/pulls/296

---
+++ plugins/tags/src/BackendActions.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @brief tags, a plugin for Dotclear 2
+ *
+ * @package Dotclear
+ * @subpackage Plugins
+ *
+ * @copyright Olivier Meunier & Association Dotclear
+ * @copyright GPL-2.0-only
+ */
+declare(strict_types=1);
+
+namespace Dotclear\Plugin\tags;
+
+use dcPostsActions;
+
+class BackendActions extends dcPostsActions
+{
+    protected $use_render = true;
+}
