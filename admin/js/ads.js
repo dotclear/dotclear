6bdf4655bdaa9d7e5444867575e32da0f14cd24b
franck <carnet.franck.paul@gmail.com>
2023-03-04 14:59:57 +0100

    JS code review

--- admin/js/ads.js
+++ admin/js/ads.js
@@ -8,13 +8,10 @@ document.addEventListener('DOMContentLoaded', () => {
   // Check adblocker helper
   dotclear.adblockCheck = (msg) => {
     const ads = document.getElementById(ads_id);
-    let adblocker_on = false;
-    if (ads === null || window.getComputedStyle(ads).display === 'none') {
+    const adblocker_on = ads === null || window.getComputedStyle(ads).display === 'none';
+    if (msg && adblocker_on) {
       // An adblocker has remove the pseudo advertising block or
       // An adblocker has set display property of the pseudo advertising block to none
-      adblocker_on = true;
-    }
-    if (msg && adblocker_on) {
       window.alert(msg);
     }
     // Remove pseudo advertising block in page
