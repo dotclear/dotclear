99c1d295245c00b6eea5409e43b7a713ed52f1c2
Jean-Christian Denis <contact@jcdenis.fr>
2023-05-22 21:48:25 +0200

    use anonymous functions

--- inc/admin/lib.adminfilters.php
+++ inc/admin/lib.adminfilters.php
@@ -545,7 +545,7 @@ class adminPostFilter extends adminGenericFilterV2
         }
 
         return (new dcAdminFilter('format'))
-            ->param('where', ['adminPostFilter', 'getPostFormatParam'])
+            ->param('where', fn ($f) => " AND post_format = '" . $f[0] . "' ")
             ->title(__('Format:'))
             ->options(array_merge(
                 ['-' => ''],
@@ -554,18 +554,13 @@ class adminPostFilter extends adminGenericFilterV2
             ->prime(true);
     }
 
-    public static function getPostFormatParam($f)
-    {
-        return " AND post_format = '" . $f[0] . "' ";
-    }
-
     /**
      * Posts password state select
      */
     public function getPostPasswordFilter(): dcAdminFilter
     {
         return (new dcAdminFilter('password'))
-            ->param('where', ['adminPostFilter', 'getPostPasswordParam'])
+            ->param('where', fn ($f) => ' AND post_password IS ' . ($f[0] ? 'NOT ' : '') . 'NULL ')
             ->title(__('Password:'))
             ->options([
                 '-'                    => '',
@@ -573,11 +568,6 @@ class adminPostFilter extends adminGenericFilterV2
                 __('Without password') => '0',
             ])
             ->prime(true);
-    }
-
-    public static function getPostPasswordParam($f)
-    {
-        return ' AND post_password IS ' . ($f[0] ? 'NOT ' : '') . 'NULL ';
     }
 
     /**
@@ -633,23 +623,13 @@ class adminPostFilter extends adminGenericFilterV2
         }
 
         return (new dcAdminFilter('month'))
-            ->param('post_month', ['adminPostFilter', 'getPostMonthParam'])
-            ->param('post_year', ['adminPostFilter', 'getPostYearParam'])
+            ->param('post_month', fn ($f) => substr($f[0], 4, 2))
+            ->param('post_year', fn ($f) => substr($f[0], 0, 4))
             ->title(__('Month:'))
             ->options(array_merge(
                 ['-' => ''],
                 dcAdminCombos::getDatesCombo($dates)
             ));
-    }
-
-    public static function getPostMonthParam($f): string
-    {
-        return substr($f[0], 4, 2);
-    }
-
-    public static function getPostYearParam($f): string
-    {
-        return substr($f[0], 0, 4);
     }
 
     /**
@@ -685,7 +665,7 @@ class adminPostFilter extends adminGenericFilterV2
     public function getPostCommentFilter(): dcAdminFilter
     {
         return (new dcAdminFilter('comment'))
-            ->param('where', ['adminPostFilter', 'getPostCommentParam'])
+            ->param('where', fn ($f) => " AND post_open_comment = '" . $f[0] . "' ")
             ->title(__('Comments:'))
             ->options([
                 '-'          => '',
@@ -694,29 +674,19 @@ class adminPostFilter extends adminGenericFilterV2
             ]);
     }
 
-    public static function getPostCommentParam($f): string
-    {
-        return " AND post_open_comment = '" . $f[0] . "' ";
-    }
-
     /**
      * Posts trackbacks state select
      */
     public function getPostTrackbackFilter(): dcAdminFilter
     {
         return (new dcAdminFilter('trackback'))
-            ->param('where', ['adminPostFilter', 'getPostTrackbackParam'])
+            ->param('where', fn ($f) => " AND post_open_tb = '" . $f[0] . "' ")
             ->title(__('Trackbacks:'))
             ->options([
                 '-'          => '',
                 __('Opened') => '1',
                 __('Closed') => '0',
             ]);
-    }
-
-    public static function getPostTrackbackParam($f): string
-    {
-        return " AND post_open_tb = '" . $f[0] . "' ";
     }
 }
 
@@ -762,7 +732,7 @@ class adminCommentFilter extends adminGenericFilterV2
     public function getCommentTypeFilter(): dcAdminFilter
     {
         return (new dcAdminFilter('type'))
-            ->param('comment_trackback', ['adminCommentFilter', 'getCommentTypeParam'])
+            ->param('comment_trackback', fn ($f) => $f[0] == 'tb')
             ->title(__('Type:'))
             ->options([
                 '-'             => '',
@@ -770,11 +740,6 @@ class adminCommentFilter extends adminGenericFilterV2
                 __('Trackback') => 'tb',
             ])
             ->prime(true);
-    }
-
-    public static function getCommentTypeParam($f): bool
-    {
-        return $f[0] == 'tb';
     }
 
     /**
