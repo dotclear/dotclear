3e95e88d6aa7e48319e2e3cd29aaaf5b22eaa3aa
Jean-Christian Denis <contact@jcdenis.fr>
2023-06-17 18:23:34 +0200

    use App::process for admin index page

--- inc/admin/prepend.php
+++ inc/admin/prepend.php
@@ -12,6 +12,6 @@ use Dotclear\App;
 define('DC_CONTEXT_ADMIN', true);
 define('DC_ADMIN_CONTEXT', true); // For dyslexic devs ;-)
 
-require_once __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'prepend.php';
+require_once implode(DIRECTORY_SEPARATOR, [__DIR__, '..', 'prepend.php']);
 
 App::context(dcAdmin::class);
