8edd938bb283ad93c081dd363096437e609a6afc
franck <carnet.franck.paul@gmail.com>
2023-07-09 09:55:51 +0200

    Avoid defining twice the same function (window.CKEDITOR_GETURL)

--- plugins/dcCKEditor/js/_post_editor.js
+++ plugins/dcCKEditor/js/_post_editor.js
@@ -2,17 +2,19 @@
 /*exported CKEDITOR_GETURL */
 'use strict';
 
-// Get context
-Object.assign(dotclear, dotclear.getData('ck_editor_ctx'));
-// Get messages
-Object.assign(dotclear.msg, dotclear.getData('ck_editor_msg'));
-// Get CK Editor variables
-Object.assign(window, dotclear.getData('ck_editor_var'));
+if (!window.CKEDITOR_GETURL) {
+  // Get context
+  Object.assign(dotclear, dotclear.getData('ck_editor_ctx'));
+  // Get messages
+  Object.assign(dotclear.msg, dotclear.getData('ck_editor_msg'));
+  // Get CK Editor variables
+  Object.assign(window, dotclear.getData('ck_editor_var'));
 
-const CKEDITOR_GETURL = function (resource) {
-  // If this is not a full or absolute path.
-  if (resource.indexOf(':/') == -1 && resource.indexOf('/') !== 0) {
-    return this.basePath + resource;
-  }
-  return resource;
-};
+  window.CKEDITOR_GETURL = function (resource) {
+    // If this is not a full or absolute path.
+    if (resource.indexOf(':/') == -1 && resource.indexOf('/') !== 0) {
+      return this.basePath + resource;
+    }
+    return resource;
+  };
+}
