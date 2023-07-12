311854f901ff02938b0c183a6b7695953220b559
franck <carnet.franck.paul@gmail.com>
2023-07-12 09:07:50 +0200

    Reduce scrolling animation if necessary (a11y) for admin Go-Top button

--- admin/js/common.js
+++ admin/js/common.js
@@ -1048,7 +1048,14 @@ $(() => {
     }
   });
   $('#gototop').on('click', (e) => {
-    $('body,html').animate({ scrollTop: 0 }, 800);
+    const isReduced =
+      window.matchMedia(`(prefers-reduced-motion: reduce)`) === true ||
+      window.matchMedia(`(prefers-reduced-motion: reduce)`).matches === true;
+    if (!!isReduced) {
+      document.querySelector('html').scrollTop = 0;
+    } else {
+      $('body,html').animate({ scrollTop: 0 }, 800);
+    }
     e.preventDefault();
   });
 
