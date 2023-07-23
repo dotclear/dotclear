c9752319173b516657d9d9cbf56163707b2ecc61
franck <carnet.franck.paul@gmail.com>
2023-07-23 07:45:27 +0200

    Allow use of items in Link (a html element), add $format parameter for render() method

--- src/Helper/Html/Form/Link.php
+++ src/Helper/Html/Form/Link.php
@@ -33,16 +33,34 @@ class Link extends Component
     /**
      * Renders the HTML component.
      *
+     * @param   string  $format     sprintf() format applied for each items/fields ('%s' by default)
+     *
      * @return     string
      */
-    public function render(): string
+    public function render(?string $format = null): string
     {
         $buffer = '<' . ($this->getElement() ?? self::DEFAULT_ELEMENT) .
             (isset($this->href) ? ' href="' . $this->href . '"' : '') .
             $this->renderCommonAttributes() . '>';
+
+        $first = true;
+        $format ??= ($this->format ?? '%s');
+
+        // Cope with items
+        if (isset($this->items) && is_array($this->items)) {
+            foreach ($this->items as $item) {
+                if (!$first && $this->separator) {  // @phpstan-ignore-line
+                    $buffer .= (string) $this->separator;
+                }
+                $buffer .= sprintf($format, $item->render());   // @phpstan-ignore-line
+                $first = false;
+            }
+        }
+
         if ($this->text) {
             $buffer .= $this->text;
         }
+
         $buffer .= '</' . ($this->getElement() ?? self::DEFAULT_ELEMENT) . '>' . "\n";
 
         return $buffer;
