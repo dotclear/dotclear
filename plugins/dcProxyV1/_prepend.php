01c970f701af4a24bce91588984590c3267b958d
franck <carnet.franck.paul@gmail.com>
2023-09-05 08:54:11 +0200

    Oups :-p

--- plugins/dcProxyV1/_prepend.php
+++ plugins/dcProxyV1/_prepend.php
@@ -181,7 +181,7 @@ dcProxyV1::classAliases([
     'dcAuth'         => 'Dotclear\Core\Auth',
     'dcBlog'         => 'Dotclear\Core\Blog',
     'dcCategories'   => 'Dotclear\Core\Categories',
-    'dcDeprecated'   => 'Dotclear\Core\dcDeprecated',
+    'dcDeprecated'   => 'Dotclear\Core\Deprecated',
     'dcError'        => 'Dotclear\Core\Error',
     'dcLog'          => 'Dotclear\Core\Log',
     'dcMedia'        => 'Dotclear\Core\Media',
