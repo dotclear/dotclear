85dd38335328d80aa5a200780cb082d293500522
franck <carnet.franck.paul@gmail.com>
2023-12-17 17:53:35 +0100

    Improve a little bit the adblocker detection system

--- admin/js/ads.js
+++ admin/js/ads.js
@@ -2,14 +2,51 @@
 'use strict';
 
 dotclear.ready(() => {
-  // DOM ready and content loaded
+  // DOM content loaded
 
-  const ads_id = 'WJxYFNKPMRlS';
+  const bait_id = 'adTest';
+
+  // Create pseudo advertising block in page
+  const bait = document.createElement('div');
+  if (!bait) return;
+
+  const bait_ids = ['AdHeader', 'AdContainer', 'AD_Top', 'homead', 'ad-lead'];
+  const generatesBannersString = () => bait_ids.map((bannerId) => `<div id="${bannerId}"></div>`).join('');
+
+  bait.id = bait_id;
+  bait.classList.add(
+    'ad',
+    'adbadge',
+    'ads',
+    'adsbox',
+    'adsbygoogle',
+    'ad-box',
+    'ad-placement',
+    'ad-placeholder',
+    'ad-wrapper',
+    'BannerAd',
+    'doubleclick',
+    'pub_300x250',
+    'pub_300x250m',
+    'pub_728x90',
+    'textAd',
+    'text-ad',
+    'text-ad-links',
+    'text-ads',
+    'text_ad',
+    'text_ads',
+  );
+  bait.style.cssText +=
+    'width: 1px !important; height: 1px !important; position: absolute !important; left: -10000px !important; top: -1000px !important;';
+  bait.setAttribute('aria-hidden', 'true');
+  bait.innerHTML = `&nbsp;${generatesBannersString()}`;
+  document.body.appendChild(bait);
 
   // Check adblocker helper
   dotclear.adblockCheck = (msg) => {
-    const ads = document.getElementById(ads_id);
-    const adblocker_on = ads === null || window.getComputedStyle(ads).display === 'none';
+    const ads = document.getElementById(bait_id);
+    const adblocker_on =
+      ads === null || window.getComputedStyle(ads).display === 'none' || window.getComputedStyle(ads).display === 'hidden';
     if (msg && adblocker_on) {
       // An adblocker has remove the pseudo advertising block or
       // An adblocker has set display property of the pseudo advertising block to none
@@ -22,16 +59,6 @@ dotclear.ready(() => {
 
     return adblocker_on;
   };
-
-  // Create pseudo advertising block in page
-  const spot = document.createElement('div');
-  if (!spot) return;
-
-  spot.id = ads_id;
-  spot.classList.add('adsbygoogle');
-  spot.classList.add('adsbox');
-  spot.innerHTML = '&nbsp;';
-  document.body.appendChild(spot);
 
   new Promise((resolve) => setTimeout(resolve, 1000)).then(() => dotclear.adblockCheck(dotclear.msg.adblocker));
 });
