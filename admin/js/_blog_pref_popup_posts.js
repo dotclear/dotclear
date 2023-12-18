fb1cc3cffcfa989ccdca0a565589d7529e750244
franck <carnet.franck.paul@gmail.com>
2023-12-18 08:28:26 +0100

    WIP: JS Code review, switch from jQuery to Vanilla

--- admin/js/_blog_pref_popup_posts.js
+++ admin/js/_blog_pref_popup_posts.js
@@ -1,4 +1,4 @@
-/*global $, dotclear */
+/*global dotclear */
 'use strict';
 
 // Get context
@@ -7,30 +7,31 @@ Object.assign(dotclear, dotclear.getData('admin.blog_pref'));
 dotclear.ready(() => {
   // DOM ready and content loaded
 
-  $('#link-insert-cancel').on('click', () => {
+  const cancel = document.getElementById('link-insert-cancel');
+  cancel.addEventListener('click', () => {
     window.close();
   });
 
-  $('#form-entries tr>td.maximal>a').on('click', function () {
-    function stripBaseURL(url) {
-      if (dotclear.base_url != '' && url.indexOf(dotclear.base_url) == 0) {
-        return url.substr(dotclear.base_url.length);
+  const posts = document.querySelectorAll('#form-entries tr>td.maximal>a');
+  posts.forEach((elt) => {
+    elt.addEventListener('click', function () {
+      const stripBaseURL = (url) =>
+        dotclear.base_url !== '' && url.startsWith(dotclear.base_url) ? url.substr(dotclear.base_url.length) : url;
+
+      // Get entry URL
+      const main = window.opener;
+      if (main) {
+        const title = stripBaseURL(this.getAttribute('title'));
+
+        // Remove base scheme from beginning
+        const next = title.indexOf('/');
+        const href = next === -1 ? title : title.substring(next + 1);
+
+        // Set new URL
+        main.document.getElementById('static_home_url').setAttribute('value', href);
       }
 
-      return url;
-    }
-
-    // Get entry URL
-    const main = window.opener;
-    const title = stripBaseURL($(this).attr('title'));
-
-    // Remove base scheme from beginning
-    const next = title.indexOf('/');
-    const href = next === -1 ? title : title.substring(next + 1);
-
-    // Set new URL
-    main.$('#static_home_url').prop('value', href);
-
-    window.close();
+      window.close();
+    });
   });
 });
