3e7aba62c13f7564c028fcc0d6a1196f01950244
franck <carnet.franck.paul@gmail.com>
2023-12-20 09:57:04 +0100

    WIP: JS Code review, switch from jQuery to Vanilla

--- admin/js/_blogs.js
+++ admin/js/_blogs.js
@@ -1,4 +1,4 @@
-/*global $, dotclear */
+/*global dotclear */
 'use strict';
 
 dotclear.ready(() => {
@@ -10,13 +10,18 @@ dotclear.ready(() => {
   dotclear.enableShiftClick('#form-blogs td input[type=checkbox]');
   dotclear.condSubmit('#form-blogs td input[type=checkbox]', '#form-blogs #do-action');
   dotclear.responsiveCellHeaders(document.querySelector('#form-blogs table'), '#form-blogs table', 1);
-  $('#form-blogs').on('submit', function (event) {
-    if ($(this).find('select[name="action"]').val() == 'delete') {
-      if (!window.confirm(dotclear.msg.confirm_delete_blog.replace('%s', $('input[name="blogs[]"]:checked').length))) {
-        event.preventDefault();
-        return false;
+
+  // Ask confirmation before blog(s) deletion
+  document.getElementById('form-blogs')?.addEventListener('submit', (event) => {
+    if (document.querySelector('select[name="action"]')?.value === 'delete') {
+      const number = document.querySelectorAll('input[name="blogs[]"]:checked').length;
+      if (number) {
+        if (!window.confirm(dotclear.msg.confirm_delete_blog.replace('%s', number))) {
+          event.preventDefault();
+          return false;
+        }
+        return true;
       }
-      return true;
     }
   });
 });
