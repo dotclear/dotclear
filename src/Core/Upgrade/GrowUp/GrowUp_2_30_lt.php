c2463a5a209823ece1992c878c0ebcdb2a21907c
franck <carnet.franck.paul@gmail.com>
2024-03-03 10:08:55 +0100

    Switch from PNG to SVG for drag indicator image

---
+++ src/Core/Upgrade/GrowUp/GrowUp_2_30_lt.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * @package     Dotclear
+ *
+ * @copyright   Olivier Meunier & Association Dotclear
+ * @copyright   GPL-2.0-only
+ */
+declare(strict_types=1);
+
+namespace Dotclear\Core\Upgrade\GrowUp;
+
+use Dotclear\Core\Upgrade\Upgrade;
+
+/**
+ * @brief   Upgrade step.
+ */
+class GrowUp_2_30_lt
+{
+    public static function init(bool $cleanup_sessions): bool
+    {
+        // A bit of housecleaning for no longer needed folders
+        Upgrade::houseCleaning(
+            // Files
+            [
+                'admin/style/drag.png',
+            ],
+            // Folders
+            [
+            ]
+        );
+
+        return $cleanup_sessions;
+    }
+}
