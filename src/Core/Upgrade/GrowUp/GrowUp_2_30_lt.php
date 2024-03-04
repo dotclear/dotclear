0fe5253604c1ba29531178b24193284699a755b5
franck <carnet.franck.paul@gmail.com>
2024-03-04 17:40:04 +0100

    Switch from PNG to SVG for cancel icon (style)

--- src/Core/Upgrade/GrowUp/GrowUp_2_30_lt.php
+++ src/Core/Upgrade/GrowUp/GrowUp_2_30_lt.php
@@ -22,6 +22,7 @@ class GrowUp_2_30_lt
         Upgrade::houseCleaning(
             // Files
             [
+                'admin/style/cancel.png',
                 'admin/style/drag.png',
                 'admin/style/search.png',
                 'admin/style/settings.png',
