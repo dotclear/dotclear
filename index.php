b0d1c2eb1a82485767f4bb53bb137d883528abf5
franck <carnet.franck.paul@gmail.com>
2024-11-25 12:07:22 +0100

    Add doxygen conditional command to avoid misinterpreting global else variable in documentation

--- index.php
+++ index.php
@@ -10,14 +10,16 @@
  * @copyright   Olivier Meunier & Association Dotclear
  * @copyright   AGPL-3.0
  */
+
+/// @cond PRODUCTION
 if (isset($_SERVER['DC_BLOG_ID'])) {
     define('DC_BLOG_ID', $_SERVER['DC_BLOG_ID']);
 } elseif (isset($_SERVER['REDIRECT_DC_BLOG_ID'])) {
     define('DC_BLOG_ID', $_SERVER['REDIRECT_DC_BLOG_ID']);
 } else {
-    # Define your blog here
-    define('DC_BLOG_ID', 'default');
+    define('DC_BLOG_ID', 'default'); // <- define your blog here
 }
+/// @endcond
 
 require_once implode(DIRECTORY_SEPARATOR, [__DIR__, 'src', 'App.php']);
 
