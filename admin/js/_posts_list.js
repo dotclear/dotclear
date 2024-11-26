318245f6144b79cd0868e931fd9358ddaa9b0238
franck <carnet.franck.paul@gmail.com>
2024-11-26 10:24:42 +0100

    Switch from jQuery to Vanilla JS for posts list JS

--- admin/js/_posts_list.js
+++ admin/js/_posts_list.js
@@ -1,18 +1,17 @@
-/*global $, dotclear */
+/*global dotclear */
 'use strict';
 
 dotclear.viewPostContent = (line, _action = 'toggle', e = null) => {
-  if ($(line).attr('id') === undefined) {
-    return;
-  }
+  const target = dotclear.node(line);
+  if (!target || target.getAttribute('id') === null) return;
 
-  const postId = $(line).attr('id').substring(1);
+  const postId = target.getAttribute('id').substring(1);
   const lineId = `pe${postId}`;
   let tr = document.getElementById(lineId);
 
   if (tr) {
-    $(tr).toggle();
-    $(line).toggleClass('expand');
+    tr.style.display = tr.style.display === 'none' ? '' : 'none';
+    target.classList.toggleClass('expand');
   } else {
     // Get post content if possible
     dotclear.getEntryContent(
@@ -23,15 +22,16 @@ dotclear.viewPostContent = (line, _action = 'toggle', e = null) => {
           tr = document.createElement('tr');
           tr.id = lineId;
           const td = document.createElement('td');
-          td.colSpan = $(line).children('td').length;
+          // Set colspan of supplementary line to all cells of target line as we need only one cell in this line
+          td.colSpan = target.children.length;
           td.className = 'expand';
           tr.appendChild(td);
-          $(td).append(content);
-          $(line).addClass('expand');
+          td.append(...dotclear.htmlToNodes(content));
+          target.classList.add('expand');
           line.parentNode.insertBefore(tr, line.nextSibling);
           return;
         }
-        $(line).toggleClass('expand');
+        target.classList.remove('expand');
       },
       {
         clean: e.metaKey,
@@ -42,11 +42,6 @@ dotclear.viewPostContent = (line, _action = 'toggle', e = null) => {
 
 dotclear.ready(() => {
   // DOM ready and content loaded
-
-  // Entry type switcher
-  $('#type').on('change', function () {
-    this.form.submit();
-  });
 
   dotclear.expandContent({
     line: document.querySelector('#form-entries tr:not(.line)'),
@@ -60,4 +55,11 @@ dotclear.ready(() => {
   dotclear.condSubmit('#form-entries td input[type=checkbox]', '#form-entries #do-action');
   dotclear.postsActionsHelper();
   dotclear.responsiveCellHeaders(document.querySelector('#form-entries table'), '#form-entries table', 1, true);
+
+  // Type switcher (on entry list selector)
+  const switcher = document.getElementById('type');
+  if (switcher)
+    switcher.addEventListener('change', (event) => {
+      event.currentTarget.form.submit();
+    });
 });
