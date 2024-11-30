4d62e06d6932827fa8a626208133da6256f1d1f8
franck <carnet.franck.paul@gmail.com>
2024-11-30 23:54:15 +0100

    Switch from jQuery to Vanilla JS for categories management JS

--- admin/js/_categories.js
+++ admin/js/_categories.js
@@ -1,34 +1,46 @@
-/*global $, dotclear */
+/*global jQuery, dotclear */
 'use strict';
 
 dotclear.ready(() => {
   // DOM ready and content loaded
 
-  if ($.fn.nestedSortable !== undefined) {
-    $('#categories ul li').css('cursor', 'move');
-    $('#save-set-order').prop('disabled', true).addClass('disabled');
-    $('#categories ul').nestedSortable({
+  if (jQuery.fn.nestedSortable !== undefined) {
+    jQuery('#categories ul li').css('cursor', 'move');
+    jQuery('#save-set-order').prop('disabled', true).addClass('disabled');
+    jQuery('#categories ul').nestedSortable({
       listType: 'ul',
       items: 'li',
       placeholder: 'placeholder',
       update() {
-        $('#categories_order').attr('value', JSON.stringify($('#categories ul').nestedSortable('toArray')));
-        $('#save-set-order').prop('disabled', false).removeClass('disabled');
+        jQuery('#categories_order').attr('value', JSON.stringify(jQuery('#categories ul').nestedSortable('toArray')));
+        jQuery('#save-set-order').prop('disabled', false).removeClass('disabled');
       },
     });
   }
 
-  $('input[name^="delete"]').on('click', function (event) {
-    if (
-      !window.confirm(
-        dotclear.msg.confirm_delete_category.replace('%s', $(this).parents('li').first().find('.cat-title label a').text()),
-      )
-    ) {
-      event.preventDefault();
-      return false;
+  const deleteButtons = document.querySelectorAll('input[name^="delete"]');
+  const parents = (target, selector) => {
+    const parents = [];
+    let element = target.parentNode;
+    while (element && element !== document) {
+      if (!selector || element.matches(selector)) {
+        parents.push(element);
+      }
+      element = element.parentNode;
     }
-    return true;
-  });
+    return parents;
+  };
+  for (const deleteButton of deleteButtons) {
+    deleteButton.addEventListener('click', (event) => {
+      const lines = parents(event.currentTarget, 'li');
+      if (lines.length < 1) return false;
+      const category = lines[0].querySelector('.cat-title a').textContent;
 
-  $('input[name="reset"]').on('click', () => window.confirm(dotclear.msg.confirm_reorder_categories));
+      return dotclear.confirm(dotclear.msg.confirm_delete_category.replace('%s', category), event);
+    });
+  }
+
+  document
+    .querySelector('input[name="reset"]')
+    ?.addEventListener('click', (event) => dotclear.confirm(dotclear.msg.confirm_reorder_categories, event));
 });
