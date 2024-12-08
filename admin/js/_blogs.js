7ae2acbd3ac2e3139da554269b3faf4548506c4c
franck <carnet.franck.paul@gmail.com>
2024-12-08 11:02:26 +0100

    UX: Add a required attribute to password of the chosen action is delete on blogs page management

--- admin/js/_blogs.js
+++ admin/js/_blogs.js
@@ -11,6 +11,18 @@ dotclear.ready(() => {
   dotclear.condSubmit('#form-blogs td input[type=checkbox]', '#form-blogs #do-action');
   dotclear.responsiveCellHeaders(document.querySelector('#form-blogs table'), '#form-blogs table', 1);
 
+  // If password is mandatory for action, set the field as required
+  const password = document.getElementById('pwd');
+  const action = document.querySelector('select[name="action"]');
+  if (password && action) {
+    const setRequired = () => {
+      if (action.value === 'delete') password.setAttribute('required', 'required');
+      else password.removeAttribute('required');
+    };
+    setRequired();
+    action.addEventListener('change', setRequired);
+  }
+
   // Ask confirmation before blog(s) deletion
   document.getElementById('form-blogs')?.addEventListener('submit', (event) => {
     if (document.querySelector('select[name="action"]')?.value === 'delete') {
