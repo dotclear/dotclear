dotclear/dotclear

commit 998fd5a88f2dc8b9cf1cbec2dda4d75f41ffa572

Better and more readable this way

src/Core/Backend/ModulesList.php
@@ -337,10 +337,20 @@ class ModulesList
      */
     public function getURL($queries = '', bool $with_tab = true, ?string $force_tab = null): string
     {
-        return $this->page_url .
-            (!empty($queries) ? (str_contains($this->page_url, '?') ? '&amp;' : '?') : '') .
-            (is_array($queries) ? http_build_query($queries) : $queries) .
-            ($with_tab && !empty($this->page_tab) ? '#' . (!empty($force_tab) ? $force_tab : $this->page_tab) : '');
+        $query = '';
+        if (!empty($queries)) {
+            // Cope with queries
+            $query = str_contains($this->page_url, '?') ? '&amp;' : '?';
+            $query .= is_array($queries) ? http_build_query($queries) : $queries;
+        }
+
+        $tab = '';
+        if ($with_tab && (!empty($this->page) || !empty($force_tab))) {
+            // Cope with tab (as hash in URL)
+            $tab = '#' . $force_tab ?: $this->page_tab;
+        }
+
+        return $this->page_url . $query . $tab;
     }
 
     /**

commit 38810f6236e0cf341e55d264855f392fabf5e980

Fix: Back to correct tab on plugin search (typo introduced in commit 998fd5a88f2dc8b9cf1cbec2dda4d75f41ffa572)

src/Core/Backend/ModulesList.php
@@ -345,7 +345,7 @@ class ModulesList
         }
 
         $tab = '';
-        if ($with_tab && (!empty($this->page) || !empty($force_tab))) {
+        if ($with_tab && (!empty($this->page_tab) || !empty($force_tab))) {
             // Cope with tab (as hash in URL)
             $tab = '#' . ($force_tab ?: $this->page_tab);
         }
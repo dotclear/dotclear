38810f6236e0cf341e55d264855f392fabf5e980
franck <carnet.franck.paul@gmail.com>
2024-12-22 18:57:28 +0100

    Fix: Back to correct tab on plugin search (typo introduced in commit 998fd5a88f2dc8b9cf1cbec2dda4d75f41ffa572)

--- src/Core/Backend/ModulesList.php
+++ src/Core/Backend/ModulesList.php
@@ -345,7 +345,7 @@ class ModulesList
         }
 
         $tab = '';
-        if ($with_tab && (!empty($this->page) || !empty($force_tab))) {
+        if ($with_tab && (!empty($this->page_tab) || !empty($force_tab))) {
             // Cope with tab (as hash in URL)
             $tab = '#' . ($force_tab ?: $this->page_tab);
         }
