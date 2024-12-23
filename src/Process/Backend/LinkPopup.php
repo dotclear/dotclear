f789875e49f2ec165881fe52b83514d33dc54898
franck <carnet.franck.paul@gmail.com>
2024-12-23 21:14:01 +0100

    Switch from legacy form:: to Help/Html/Form/... for link popup

--- src/Process/Backend/LinkPopup.php
+++ src/Process/Backend/LinkPopup.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @package Dotclear
  * @subpackage Backend
@@ -10,12 +11,22 @@ declare(strict_types=1);
 
 namespace Dotclear\Process\Backend;
 
+use Dotclear\App;
 use Dotclear\Core\Backend\Combos;
 use Dotclear\Core\Backend\Page;
-use Dotclear\App;
 use Dotclear\Core\Process;
+use Dotclear\Helper\Html\Form\Button;
+use Dotclear\Helper\Html\Form\Form;
+use Dotclear\Helper\Html\Form\Input;
+use Dotclear\Helper\Html\Form\Label;
+use Dotclear\Helper\Html\Form\Note;
+use Dotclear\Helper\Html\Form\Para;
+use Dotclear\Helper\Html\Form\Select;
+use Dotclear\Helper\Html\Form\Set;
+use Dotclear\Helper\Html\Form\Submit;
+use Dotclear\Helper\Html\Form\Text;
+use Dotclear\Helper\Html\Form\Url;
 use Dotclear\Helper\Html\Html;
-use form;
 
 /**
  * @since 2.27 Before as admin/popup_link.php
@@ -35,7 +46,7 @@ class LinkPopup extends Process
         App::backend()->plugin_id = !empty($_GET['plugin_id']) ? Html::sanitizeURL($_GET['plugin_id']) : '';
 
         if (App::themes()->isEmpty()) {
-            # -- Loading themes, may be useful for some configurable theme --
+            // Loading themes, may be useful for some configurable theme --
             App::themes()->loadModules(App::blog()->themesPath(), 'admin', App::lang()->getLang());
         }
 
@@ -49,29 +60,55 @@ class LinkPopup extends Process
     public static function render(): void
     {
         # --BEHAVIOR-- adminPopupLink -- string
-        Page::openPopup(__('Add a link'), Page::jsLoad('js/_popup_link.js') . App::behavior()->callBehavior('adminPopupLink', App::backend()->plugin_id));
+        Page::openPopup(
+            __('Add a link'),
+            Page::jsLoad('js/_popup_link.js') . App::behavior()->callBehavior('adminPopupLink', App::backend()->plugin_id)
+        );
 
-        echo '<h2 class="page-title">' . __('Add a link') . '</h2>';
-
-        echo
-        '<form id="link-insert-form" action="#" method="get">' .
-        '<p class="form-note">' . sprintf(__('Fields preceded by %s are mandatory.'), '<span class="required">*</span>') . '</p>' .
-        '<p><label class="required" for="href"><span>*</span> ' . __('Link URL:') . '</label> ' .
-        form::field('href', 35, 512, [
-            'default'    => Html::escapeHTML(App::backend()->href),
-            'extra_html' => 'required placeholder="' . __('URL') . '"',
-        ]) .
-        '</p>' .
-        '<p><label for="title">' . __('Link title:') . '</label> ' .
-        form::field('title', 35, 512, Html::escapeHTML(App::backend()->title)) . '</p>' .
-        '<p><label for="hreflang">' . __('Link language:') . '</label> ' .
-        form::combo('hreflang', App::backend()->lang_combo, App::backend()->hreflang) .
-        '</p>' .
-
-        '</form>' .
-
-        '<p class="form-buttons"><button type="button" class="reset" id="link-insert-cancel">' . __('Cancel') . '</button> - ' .
-        '<button type="button" id="link-insert-ok"><strong>' . __('Insert') . '</strong></button></p>' . "\n";
+        echo (new Set())
+            ->items([
+                (new Text('h2', __('Add a link')))
+                    ->class('page-title'),
+                (new Form('link-insert-form'))
+                    ->method('get')
+                    ->fields([
+                        (new Note())
+                            ->class('form-note')
+                            ->text(sprintf(__('Fields preceded by %s are mandatory.'), (new Text('span', '*'))->class('required')->render())),
+                        (new Para())
+                            ->items([
+                                (new Url('href', Html::escapeHTML(App::backend()->href)))
+                                    ->size(35)
+                                    ->maxlength(512)
+                                    ->required(true)
+                                    ->placeholder(__('URL'))
+                                    ->label((new Label((new Text('span', '*'))->render() . __('Link URL:'), Label::OL_TF))->class('required')),
+                            ]),
+                        (new Para())
+                            ->items([
+                                (new Input('title', Html::escapeHTML(App::backend()->title)))
+                                    ->type('text')
+                                    ->size(35)
+                                    ->maxlength(512)
+                                    ->label((new Label(__('Link title:'), Label::OL_TF))),
+                            ]),
+                        (new Para())
+                            ->items([
+                                (new Select('hreflang'))
+                                    ->items(App::backend()->lang_combo)
+                                    ->value(App::backend()->hreflang)
+                                    ->label((new Label(__('Link language:'), Label::OL_TF))),
+                            ]),
+                        (new Para())
+                            ->class('form-buttons')
+                            ->items([
+                                (new Button('link-insert-cancel', __('Cancel')))
+                                    ->class('reset'),
+                                (new Submit('link-insert-ok', __('Insert'))),
+                            ]),
+                    ]),
+            ])
+        ->render();
 
         Page::closePopup();
     }
