68a9545751808f9e1aa80dbfbe75d37f10697858
Jean-Christian Denis <contact@jcdenis.fr>
2025-01-06 18:52:38 +0100

    split status types

--- src/Core/Status.php
+++ src/Core/Status.php
@@ -11,9 +11,12 @@ declare(strict_types=1);
 namespace Dotclear\Core;
 
 use Dotclear\App;
-use Dotclear\Helper\Stack\Status as Descriptor;
 use Dotclear\Helper\Stack\Statuses;
 use Dotclear\Interface\Core\StatusInterface;
+use Dotclear\Schema\Status\Blog;
+use Dotclear\Schema\Status\Comment;
+use Dotclear\Schema\Status\Post;
+use Dotclear\Schema\Status\User;
 
 /**
  * @brief   List statuses handler.
@@ -27,36 +30,12 @@ class Status implements StatusInterface
 	protected Statuses $post;
 	protected Statuses $user;
 
-	/**
-	 * Set default statuses.
-	 */
 	public function __construct()
 	{
-        $this->blog = new Statuses('blog_status', [
-            (new Descriptor(1 , 'online', __('online'), 'images/published.svg')),
-            (new Descriptor(0, 'offline', __('offline'), 'images/unpublished.svg')),
-            (new Descriptor(-1, 'removed', __('removed'), 'images/trash.svg')),
-            (new Descriptor(-2, 'undefined', __('undefined'), 'images/trash.svg')),
-        ]);
-
-        $this->post = new Statuses('post_status', [
-            (new Descriptor(1, 'published', __('Published'), 'images/published.svg')),
-            (new Descriptor(0, 'unpublished', __('Unpublished'), 'images/unpublished.svg')),
-            (new Descriptor(-1, 'scheduled', __('Scheduled'), 'images/scheduled.svg')),
-            (new Descriptor(-2, 'pending', __('Pending'), 'images/pending.svg')),
-        ]);
-
-        $this->comment = new Statuses('comment_status', [
-            (new Descriptor(1, 'published', __('Published'), 'images/published.svg')),
-            (new Descriptor(0, 'unpublished', __('Unpublished'), 'images/unpublished.svg')),
-            (new Descriptor(-1, 'pending', __('Pending'), 'images/pending.svg')),
-            (new Descriptor(-2, 'junk', __('Junk'), 'images/junk.svg')),
-        ]);
-
-        $this->user = new Statuses('user_status', [
-            (new Descriptor(1, 'enabled', __('enabled'), 'images/published.svg')),
-            (new Descriptor(0, 'disabled', __('disabled'), 'images/unpublished.svg')),
-        ]);
+        $this->blog    = new Blog();
+        $this->post    = new Post();
+        $this->comment = new Comment();
+        $this->user    = new User();
 	}
 
 	public function blog(): Statuses
