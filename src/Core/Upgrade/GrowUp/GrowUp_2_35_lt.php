dd148ceb4dc2b0d6db45798b161c84c81bb540c9
franck <carnet.franck.paul@gmail.com>
2025-05-13 11:01:01 +0200

    Add GrowUp for next 2.35

---
+++ src/Core/Upgrade/GrowUp/GrowUp_2_35_lt.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * @package     Dotclear
+ *
+ * @copyright   Olivier Meunier & Association Dotclear
+ * @copyright   AGPL-3.0
+ */
+declare(strict_types=1);
+
+namespace Dotclear\Core\Upgrade\GrowUp;
+
+use Dotclear\Core\Upgrade\Upgrade;
+
+/**
+ * @brief   Upgrade step.
+ */
+class GrowUp_2_35_lt
+{
+    public static function init(bool $cleanup_sessions): bool
+    {
+        // A bit of housecleaning for no longer needed folders
+        Upgrade::houseCleaning(
+            // Files
+            [
+                'src/Exception/UnautorizedException.php',
+            ],
+            // Folders
+            [
+            ]
+        );
+
+        return $cleanup_sessions;
+    }
+}
