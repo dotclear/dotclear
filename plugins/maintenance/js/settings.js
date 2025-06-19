eb807d30cc8504e3b0998a8d04607f585b0cba8a
franck <carnet.franck.paul@gmail.com>
2025-06-19 06:43:25 +0200

    Review dynamic activation/deactivation of delays in maintenance settings tab

--- plugins/maintenance/js/settings.js
+++ plugins/maintenance/js/settings.js
@@ -1,22 +1,33 @@
-/*global $, dotclear */
+/*global dotclear */
 'use strict';
 
 dotclear.ready(() => {
   // DOM ready and content loaded
 
-  $('.recall-for-all').attr('disabled', 'disabled');
-  $('#settings_recall_all').on('change', function () {
-    if ($(this).attr('selected') !== 'selected') {
-      $('.recall-per-task').attr('disabled', 'disabled');
-      $('.recall-for-all').removeAttr('disabled');
+  const globalRadio = document.getElementById('settings_recall_all');
+  const separateRadio = document.getElementById('settings_recall_separate');
+
+  const setStatus = (generic = true) => {
+    if (generic) document.getElementById('settings_recall_time').removeAttribute('disabled');
+    else document.getElementById('settings_recall_time').setAttribute('disabled', 'disabled');
+
+    for (const choice of document.querySelectorAll('.recall-per-task')) {
+      if (generic) choice.setAttribute('disabled', 'disabled');
+      else choice.removeAttribute('disabled');
     }
+  };
+
+  // 1st pass, set initial status
+  setStatus(globalRadio.getAttribute('checked') !== null);
+
+  // Listen change on radio choice
+  globalRadio.addEventListener('click', (event) => {
+    setStatus(event.target.getAttribute('checked') !== null);
   });
-  $('#settings_recall_separate').on('change', function () {
-    if ($(this).attr('selected') !== 'selected') {
-      $('.recall-per-task').removeAttr('disabled');
-      $('.recall-for-all').attr('disabled', 'disabled');
-    }
+  separateRadio.addEventListener('click', (event) => {
+    setStatus(event.target.getAttribute('checked') !== null);
   });
+
   dotclear.condSubmit('#part-maintenance input[type="radio"]', '#part-maintenance input[type="submit"]');
   dotclear.condSubmit('#part-backup input[type="radio"]', '#part-backup input[type="submit"]');
   dotclear.condSubmit('#part-dev input[type="radio"]', '#part-dev input[type="submit"]');
