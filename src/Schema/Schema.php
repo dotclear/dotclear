97bbdee55c7ea042b9bcc62fc96b36c7567e4efd
franck <carnet.franck.paul@gmail.com>
2025-07-04 17:38:31 +0200

    Add also media_private to idx_media_media_path index as this field is often used in queries with media_path and media_dir

--- src/Schema/Schema.php
+++ src/Schema/Schema.php
@@ -274,7 +274,7 @@ class Schema
         $struct->post->index('idx_blog_post_post_dt_post_id', 'btree', 'blog_id', 'post_dt', 'post_id');
         $struct->post->index('idx_blog_post_post_status', 'btree', 'blog_id', 'post_status');
         $struct->blog->index('idx_blog_blog_upddt', 'btree', 'blog_upddt');
-        $struct->media->index('idx_media_media_path', 'btree', 'media_path', 'media_dir');
+        $struct->media->index('idx_media_media_path', 'btree', 'media_path', 'media_dir', 'media_private');
         $struct->user->index('idx_user_user_super', 'btree', 'user_super');
 
         /* Foreign keys
