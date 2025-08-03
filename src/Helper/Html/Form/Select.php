a423fd1a762c0c9f80aea79343f035ad29295598
franck <carnet.franck.paul@gmail.com>
2025-08-03 18:45:42 +0200

    Int is a valid type for select option value

--- src/Helper/Html/Form/Select.php
+++ src/Helper/Html/Form/Select.php
@@ -16,9 +16,9 @@ use Dotclear\App;
  * @class Select
  * @brief HTML Forms select creation helpers
  *
- * @method      $this items(array<int|string, Component|string|array<int|string, Component|string>>|Iterable<int|string, Component|string|array<int|string, Component|string>> $items)
+ * @method      $this items(array<int|string, Component|string|int|array<int|string, Component|string|int>>|Iterable<int|string, Component|string|int|array<int|string, Component|string|int>> $items)
  *
- * @property    array<int|string, Component|string|array<int|string, Component|string>>|Iterable<int|string, Component|string|array<int|string, Component|string>> $items
+ * @property    array<int|string, Component|string|int|array<int|string, Component|string|int>>|Iterable<int|string, Component|string|int|array<int|string, Component|string|int>> $items
  */
 class Select extends Component
 {
