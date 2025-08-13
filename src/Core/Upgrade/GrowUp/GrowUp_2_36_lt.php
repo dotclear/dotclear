596f445eaf02b21701774ea284ddf7e24cff6c6c
franck <carnet.franck.paul@gmail.com>
2025-08-13 16:49:16 +0200

    Add GrowUp for next 2.36

---
+++ src/Core/Upgrade/GrowUp/GrowUp_2_36_lt.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * @package     Dotclear
+ * @subpackage  Upgrade
+ *
+ * @copyright   Olivier Meunier & Association Dotclear
+ * @copyright   AGPL-3.0
+ */
+declare(strict_types=1);
+
+namespace Dotclear\Core\Upgrade\GrowUp;
+
+use Dotclear\Core\Upgrade\Upgrade;
+
+/**
+ * @brief   Upgrade step.
+ */
+class GrowUp_2_36_lt
+{
+    public static function init(bool $cleanup_sessions): bool
+    {
+        // A bit of housecleaning for no longer needed folders
+        Upgrade::houseCleaning(
+            // Files
+            [
+            ],
+            // Folders
+            [
+                'plugins/antispam/locales/lb',
+            ]
+        );
+
+        return $cleanup_sessions;
+    }
+}
