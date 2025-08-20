545ddba51d0a85c1486ccc9b4eb7d3f64e5e1070
franck <carnet.franck.paul@gmail.com>
2025-08-20 10:07:11 +0200

    Switch from Atoum to PHPUnit for Helper/Network/Socket

---
+++ tests/unit/src/Helper/Network/Socket/IteratorTest.php
@@ -0,0 +1,88 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Dotclear\Tests\Helper;
+
+use Exception;
+use PHPUnit\Framework\TestCase;
+
+class IteratorTest extends TestCase
+{
+    public function testWithHandle()
+    {
+        $input = realpath(implode(DIRECTORY_SEPARATOR, [__DIR__, '..', '..', '..', '..', 'fixtures', 'src', 'Helper', 'Network', 'Socket', 'stream-input.txt']));
+
+        $handle   = fopen($input, 'rb');
+        $iterator = new \Dotclear\Helper\Network\Socket\Iterator($handle);
+
+        $this->assertIsResource(
+            $handle
+        );
+        $this->assertIsNotClosedResource(
+            $handle
+        );
+        $this->assertTrue(
+            $iterator->valid()
+        );
+        $this->assertEquals(
+            0,
+            $iterator->key()
+        );
+        $this->assertEquals(
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris pretium mauris erat, id pellentesque leo facilisis sed. Vestibulum sit amet maximus justo, ut vulputate turpis. Proin vestibulum gravida tellus a vulputate. Vivamus vestibulum metus cursus, ornare mauris ut, cursus mauris. Maecenas varius tincidunt gravida. Curabitur tristique et nunc id tempor. Duis commodo et neque a varius. Vestibulum ultricies fermentum erat quis aliquam. Maecenas at nunc sed purus faucibus rutrum. Donec tincidunt magna varius laoreet luctus. Etiam luctus dictum malesuada.' . "\n",
+            $iterator->current()
+        );
+        $this->assertEquals(
+            "\n",
+            $iterator->current()
+        );
+        $this->assertEquals(
+            0,
+            $iterator->key()
+        );
+        $this->assertEquals(
+            'Nam tempus augue sed risus placerat pharetra. Phasellus euismod varius pellentesque. Aenean eget pulvinar diam, sit amet scelerisque felis. In condimentum ultrices ex in sagittis. Integer tristique venenatis lacinia. Pellentesque vitae ex in risus ullamcorper luctus. Aenean tincidunt neque ex, tempus rhoncus neque iaculis in. Etiam lacinia sem at nulla rutrum imperdiet. Aliquam purus nunc, fringilla quis rhoncus ut, suscipit a nulla. Quisque euismod bibendum odio, non bibendum dolor. Nulla facilisi. Curabitur malesuada, nunc sit amet vehicula ultrices, magna lectus blandit augue, sed venenatis leo nibh ac nisl. Duis iaculis fermentum purus, eget egestas lorem accumsan non.' . "\n",
+            $iterator->current()
+        );
+        $this->assertFalse(
+            $iterator->current()
+        );
+        $this->assertFalse(
+            $iterator->valid()
+        );
+        $this->assertEquals(
+            0,
+            $iterator->key()
+        );
+
+        $iterator->rewind();
+
+        $this->assertFalse(
+            $iterator->valid()
+        );
+        $this->assertEquals(
+            0,
+            $iterator->key()
+        );
+
+        $iterator->next();
+        $this->assertFalse(
+            $iterator->valid()
+        );
+        $this->assertEquals(
+            1,
+            $iterator->key()
+        );
+
+        fclose($handle);
+    }
+
+    public function testWithoutHandle()
+    {
+        $handle = null;
+
+        $this->expectException(Exception::class);
+        $iterator = new \Dotclear\Helper\Network\Socket\Iterator($handle);
+    }
+}
---
+++ tests/unit/src/Helper/Network/Socket/SocketTest.php
@@ -0,0 +1,155 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Dotclear\Tests\Helper;
+
+use Exception;
+use PHPUnit\Framework\TestCase;
+
+class SocketTest extends TestCase
+{
+    public function test()
+    {
+        $socket = new \Dotclear\Helper\Network\Socket\Socket('example.org', 443);
+
+        $this->assertEquals(
+            'example.org',
+            $socket->host()
+        );
+        $this->assertTrue(
+            $socket->host('example.com')
+        );
+        $this->assertEquals(
+            'example.com',
+            $socket->host()
+        );
+        $this->assertEquals(
+            443,
+            $socket->port()
+        );
+        $this->assertTrue(
+            $socket->port(80)
+        );
+        $this->assertEquals(
+            80,
+            $socket->port()
+        );
+        $this->assertEquals(
+            10,
+            $socket->timeout()
+        );
+        $this->assertTrue(
+            $socket->timeout(20)
+        );
+        $this->assertEquals(
+            20,
+            $socket->timeout()
+        );
+        $this->assertEquals(
+            null,
+            $socket->streamTimeout()
+        );
+        $this->assertTrue(
+            $socket->streamTimeout(20)
+        );
+        $this->assertEquals(
+            20,
+            $socket->streamTimeout()
+        );
+        $this->assertEquals(
+            null,
+            $socket->verifyPeer()
+        );
+        $this->assertFalse(
+            $socket->setBlocking(true)
+        );
+        $this->assertFalse(
+            $socket->write('')
+        );
+        $this->assertFalse(
+            $socket->flush()
+        );
+        $this->assertFalse(
+            $socket->isOpen()
+        );
+
+        $socket->close();
+
+        // Next test with content
+
+        $socket = new \Dotclear\Helper\Network\Socket\Socket('example.org', 80);
+        $socket->open();
+
+        $this->assertTrue(
+            $socket->isOpen()
+        );
+        ;
+
+        $data = [
+            'GET / HTTP/1.0',
+        ];
+        $expected = [
+            'HTTP/1.0 400 Bad Request' . "\r\n",
+            'Server: AkamaiGHost' . "\r\n",
+            'Mime-Version: 1.0' . "\r\n",
+            'Content-Type: text/html' . "\r\n",
+            'Conten',
+            'Expire',
+            'Date: ',
+            'Connection: close' . "\r\n",
+            "\r\n",
+            '<HTML><HEAD>' . "\n",
+            '<TITLE>Invalid URL</TITLE>' . "\n",
+            '</HEAD><BODY>' . "\n",
+            '<H1>Invalid URL</H1>' . "\n",
+            '*',
+            '*',
+            '*',
+            '</BODY></HTML>' . "\n",
+        ];
+        $line = 0;
+        foreach ($socket->write($data) as $value) {
+            if ($line < count($expected)) {
+                //$this->dump($value);
+                if ($expected[$line] === '*') {
+                    continue;
+                }
+                $this->assertEquals(
+                    $expected[$line],
+                    mb_substr($value, 0, mb_strlen($expected[$line]))
+                );
+            }
+            $line++;
+        }
+
+        if (gettype($value) === 'boolean') {
+            $this->assertFalse(
+                $value
+            );
+        } else {
+            $this->assertStringStartsWith(
+                '</BODY></HTML>',
+                $value
+            );
+        }
+
+        $socket->close();
+
+        $this->assertFalse(
+            $socket->isOpen()
+        );
+    }
+
+    public function testOpenError()
+    {
+        $this->expectException(Exception::class);
+
+        $socket   = new \Dotclear\Helper\Network\Socket\Socket('unknowndomain.badextension', 80);
+        $iterator = $socket->open();
+
+        $this->assertFalse(
+            $socket->isOpen()
+        );
+    }
+}
