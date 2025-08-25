012669fb6b58c3211bcada38e7b76c1e7a924dd5
franck <carnet.franck.paul@gmail.com>
2025-08-25 10:34:38 +0200

    No need to require_once here, the autoloader will do the job

--- tests/unit/bootstrap.php
+++ tests/unit/bootstrap.php
@@ -13,6 +13,7 @@ declare(strict_types=1);
 use Dotclear\Config;
 use Dotclear\Core\Core;
 use Dotclear\Helper\Container\Factories;
+use Dotclear\Helper\L10n;
 
 // Composer Autoloader
 require_once __DIR__ . '/../../vendor/autoload.php';
@@ -30,7 +31,7 @@ $__autoload = [
 spl_autoload_register(function ($name) use ($__autoload) {if (isset($__autoload[$name])) { require_once $__autoload[$name]; }});
 
 // Ensure L10n functions exist
-require_once __DIR__ . '/../../src/Helper/L10n.php';
+L10n::bootstrap();
 
 // Instanciate Core
 new Core(new Config(implode(DIRECTORY_SEPARATOR, [__DIR__, '..', '..'])), Factories::getFactory(Core::CONTAINER_ID));
