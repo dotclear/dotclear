3e373e485684d2aa3b6588f7ec5748008d16a78d
Jean-Christian Denis <contact@jcdenis.fr>
2025-08-29 08:53:16 +0200

    Use Form Helper on install process

--- src/Process/Install/Wizard.php
+++ src/Process/Install/Wizard.php
@@ -13,12 +13,27 @@ use Dotclear\Core\Install\Utils;
 use Dotclear\Core\Process;
 use Dotclear\Helper\File\Files;
 use Dotclear\Helper\File\Path;
+use Dotclear\Helper\Html\Form\Div;
+use Dotclear\Helper\Html\Form\Email;
+use Dotclear\Helper\Html\Form\Form;
+use Dotclear\Helper\Html\Form\Input;
+use Dotclear\Helper\Html\Form\Label;
+use Dotclear\Helper\Html\Form\Li;
+use Dotclear\Helper\Html\Form\Note;
+use Dotclear\Helper\Html\Form\Para;
+use Dotclear\Helper\Html\Form\Password;
+use Dotclear\Helper\Html\Form\Select;
+use Dotclear\Helper\Html\Form\Set;
+use Dotclear\Helper\Html\Form\Span;
+use Dotclear\Helper\Html\Form\Strong;
+use Dotclear\Helper\Html\Form\Submit;
+use Dotclear\Helper\Html\Form\Text;
+use Dotclear\Helper\Html\Form\Ul;
 use Dotclear\Helper\Html\Html;
 use Dotclear\Helper\L10n;
 use Dotclear\Helper\Network\Http;
-use Dotclear\Helper\Text;
+use Dotclear\Helper\Text as Htext;
 use Exception;
-use form;
 
 /**
  * @brief   Installation wizard process.
@@ -81,11 +96,15 @@ class Wizard extends Process
         }
 
         if (!is_writable(dirname(App::config()->configPath()))) {
-            self::$err = '<p>' . sprintf(__('Path <strong>%s</strong> is not writable.'), Path::real(dirname(App::config()->configPath()))) . '</p>' .
-            '<p>' . sprintf(
-                __('Dotclear installation wizard could not create configuration file for you. You must change folder right or create the <strong>config.php</strong> file manually, please refer to <a href="%s">the documentation</a> to learn how to do this.'),
-                'https://dotclear.org/documentation/2.0/admin/install'
-            ) . '</p>';
+            self::$err = (new Set())
+                ->items([
+                    (new Text('p', sprintf(__('Path <strong>%s</strong> is not writable.'), Path::real(dirname(App::config()->configPath()))))),
+                    (new Text('p', sprintf(
+                        __('Dotclear installation wizard could not create configuration file for you. You must change folder right or create the <strong>config.php</strong> file manually, please refer to <a href="%s">the documentation</a> to learn how to do this.'),
+                        'https://dotclear.org/documentation/2.0/admin/install'
+                    ))),
+                ])
+                ->render();
         }
 
         return self::status();
@@ -114,7 +133,7 @@ class Wizard extends Process
 
                     # Can we write sqlite_db_directory ?
                     if (!is_writable($sqlite_db_directory)) {
-                        throw new Exception(sprintf(__('Cannot write "%s" directory.'), Path::real($sqlite_db_directory, false)));
+                        throw new Exception((new Text('p', sprintf(__('Cannot write "%s" directory.'), Path::real($sqlite_db_directory, false))))->render());
                     }
                     self::$DBNAME = (string) Path::real($sqlite_db_directory . self::$DBNAME, false);
                     if (!file_exists(self::$DBNAME)) {
@@ -126,33 +145,40 @@ class Wizard extends Process
                 try {
                     $con = App::db()->con(self::$DBDRIVER, self::$DBHOST, self::$DBNAME, self::$DBUSER, self::$DBPASSWORD);
                 } catch (Exception $e) {
-                    throw new Exception('<p>' . __($e->getMessage()) . '</p>', (int) $e->getCode(), $e);
+                    self::throwString(__($e->getMessage()), (int) $e->getCode(), $e);
                 }
 
                 # Checks system capabilites
                 $_e = [];
                 if (!Utils::check($con, $_e)) {
-                    throw new Exception('<p>' . __('Dotclear cannot be installed.') . '</p><ul><li>' . implode('</li><li>', $_e) . '</li></ul>');
+                    throw new Exception((new Set())
+                        ->items([
+                            new Text('p', __('Dotclear cannot be installed.')),
+                            (new Ul())
+                                ->items(array_map(fn ($e): Li => (new Li())->text($e), $_e)),
+                        ])
+                        ->render()
+                    );
                 }
 
                 # Check if dotclear is already installed
                 if (in_array(self::$DBPREFIX . 'version', $con->schema()->getTables())) {
-                    throw new Exception(__('Dotclear is already installed.'));
+                    self::throwString(__('Dotclear is already installed.'));
                 }
                 # Check master email
-                if (!Text::isEmail(self::$ADMINMAILFROM)) {
-                    throw new Exception(__('Master email is not valid.'));
+                if (!HText::isEmail(self::$ADMINMAILFROM)) {
+                    self::throwString(__('Master email is not valid.'));
                 }
 
                 # Does config.php.in exist?
                 $config_in = App::config()->dotclearRoot() . '/inc/config.php.in';
                 if (!is_file($config_in)) {
-                    throw new Exception(sprintf(__('File %s does not exist.'), $config_in));
+                    self::throwString(sprintf(__('File %s does not exist.'), $config_in));
                 }
 
                 # Can we write config.php
                 if (!is_writable(dirname(App::config()->configPath()))) {
-                    throw new Exception(sprintf(__('Cannot write %s file.'), App::config()->configPath()));
+                    self::throwString(sprintf(__('Cannot write %s file.'), App::config()->configPath()));
                 }
 
                 # Creates config.php file
@@ -187,7 +213,7 @@ class Wizard extends Process
 
                 $fp = @fopen(App::config()->configPath(), 'wb');
                 if ($fp === false) {
-                    throw new Exception(sprintf(__('Cannot write %s file.'), App::config()->configPath()));
+                    self::throwString(sprintf(__('Cannot write %s file.'), App::config()->configPath()));
                 }
                 fwrite($fp, $full_conf);
                 fclose($fp);
@@ -235,67 +261,128 @@ class Wizard extends Process
 </head>
 
 <body id="dotclear-admin" class="install">
-<div id="content">
-<?php
-        echo
-        '<h1>' . __('Dotclear installation wizard') . '</h1>' .
-            '<div id="main">';
-
-        if (self::$err !== '') {
-            echo '<div class="error" role="alert"><p><strong>' . __('Errors:') . '</strong></p>' . self::$err . '</div>';
-        } else {
-            echo '<h2>' . __('Welcome') . '</h2>' .
-            '<p>' . __('To complete your Dotclear installation and start writing on your blog, we just need to know how to access your database and who you are. Just fill this two steps wizard with this information and we will be done.') . '</p>' .
-            '<p class="message"><strong>' . __('Attention:') . '</strong> ' .
-            sprintf(
-                __('this wizard may not function on every host. If it does not work for you, please refer to <a href="%s">the documentation</a> to learn how to create the <strong>config.php</strong> file manually.'),
-                'https://dotclear.org/documentation/2.0/admin/install'
-            ) . '</p>';
-        }
-
-        echo
-        '<h2>' . __('System information') . '</h2>' .
-
-        '<p>' . __('Please provide the following information needed to create your configuration file.') . '</p>' .
-
-        '<form action="index.php" method="post">' .
-        '<p class="form-note">' . sprintf(__('Fields preceded by %s are mandatory.'), '<span class="required">*</span>') . '</p>' .
-        '<p><label class="required" for="DBDRIVER"><span>*</span> ' . __('Database type:') . '</label> ' .
-        form::combo(
-            'DBDRIVER',
-            App::db()->combo(),
-            ['default' => self::$DBDRIVER, 'extra_html' => 'required placeholder="' . __('Driver') . '"']
-        ) . '</p>' .
-        '<p><label for="DBHOST">' . __('Database Host Name:') . '</label> ' .
-        form::field('DBHOST', 30, 255, Html::escapeHTML(self::$DBHOST)) . '</p>' .
-        '<p><label for="DBNAME">' . __('Database Name:') . '</label> ' .
-        form::field('DBNAME', 30, 255, Html::escapeHTML(self::$DBNAME)) . '</p>' .
-        '<p><label for="DBUSER">' . __('Database User Name:') . '</label> ' .
-        form::field('DBUSER', 30, 255, Html::escapeHTML(self::$DBUSER)) . '</p>' .
-        '<p><label for="DBPASSWORD">' . __('Database Password:') . '</label> ' .
-        form::password('DBPASSWORD', 30, 255) . '</p>' .
-        '<p><label for="DBPREFIX" class="required"><span>*</span> ' . __('Database Tables Prefix:') . '</label> ' .
-        form::field('DBPREFIX', 30, 255, [
-            'default'    => Html::escapeHTML(self::$DBPREFIX),
-            'extra_html' => 'required placeholder="' . __('Prefix') . '"',
-        ]) .
-        '</p>' .
-        '<p><label for="ADMINMAILFROM">' . __('Master Email: (used as sender for password recovery)') . '</label> ' .
-        form::email('ADMINMAILFROM', [
-            'size'         => 30,
-            'default'      => Html::escapeHTML(self::$ADMINMAILFROM),
-            'autocomplete' => 'email',
-        ]) .
-        '</p>' .
-
-        '<p><input type="submit" value="' . __('Continue') . '"></p>' .
-            '</form>';
-        ?>
-</div>
-</div>
+        <?php self::renderContent(); ?>
 </body>
 </html>
-<?php
+        <?php
+    }
+
+    /**
+     * Render form content.
+     */
+    private static function renderContent(): void
+    {
+        $required = (new Span())->text('*')->render();
+
+        echo (new Div('content'))
+            ->items([
+                (new Text('h1', __('Dotclear installation wizard'))),
+                (new Div('main'))
+                    ->items([ ... 
+                        (self::$err !== '' ? [
+                            (new Div())
+                                ->class('error')
+                                ->role('main')
+                                ->items([
+                                    (new Para())
+                                        ->items([
+                                            (new Text('p'))
+                                                ->items([
+                                                    new Strong(__('Errors:')),
+                                                ]),
+                                            new Text('', self::$err),
+                                        ]),
+                                ])
+                        ] : [
+                            (new Text('h2', __('Welcome'))),
+                            (new Text('p', __('To complete your Dotclear installation and start writing on your blog, we just need to know how to access your database and who you are. Just fill this two steps wizard with this information and we will be done.'))),
+                            (new Set())
+                                ->items([
+                                    (new Text('p'))
+                                        ->class('message')
+                                        ->items([
+                                            new Strong(__('Attention:')),
+                                            (new Text('', sprintf(
+                                                __('this wizard may not function on every host. If it does not work for you, please refer to <a href="%s">the documentation</a> to learn how to create the <strong>config.php</strong> file manually.'),
+                                                'https://dotclear.org/documentation/2.0/admin/install'
+                                            ))),
+                                        ]),
+                                ]),
+                        ]),
+                        (new Text('h2', __('System information'))),
+                        (new Text('p', __('Please provide the following information needed to create your configuration file.'))),
+                        (new Form('install-form'))
+                            ->method('post')
+                            ->action('index.php')
+                            ->fields([
+                                (new Para())
+                                    ->class('form-note')
+                                    ->items([
+                                        (new Note())
+                                            ->text(sprintf(__('Fields preceded by %s are mandatory.'), '<span class="required">*</span>'))
+                                            ->class('form-note'),
+                                        (new Label($required . __('Database type:')))
+                                            ->class('required')
+                                            ->for('DBDRIVER'),
+                                        (new Select('DBDRIVER'))
+                                            ->items(App::db()->combo())
+                                            ->default(self::$DBDRIVER)
+                                            ->extra('required placeholder="' . __('Driver') . '"'),
+                                        (new Para())
+                                            ->items([
+                                                (new Input('DBHOST'))
+                                                    ->size(30)
+                                                    ->maxlength(255)
+                                                    ->value(Html::escapeHTML(self::$DBHOST))
+                                                    ->label(new Label(__('Database Host Name:'), Label::OUTSIDE_LABEL_BEFORE)),
+                                            ]),
+                                        (new Para())
+                                            ->items([
+                                                (new Input('DBNAME'))
+                                                    ->size(30)
+                                                    ->maxlength(255)
+                                                    ->value(Html::escapeHTML(self::$DBNAME))
+                                                    ->label(new Label(__('Database Name:'), Label::OUTSIDE_LABEL_BEFORE)),
+                                            ]),
+                                        (new Para())
+                                            ->items([
+                                                (new Input('DBUSER'))
+                                                    ->size(30)
+                                                    ->maxlength(255)
+                                                    ->value(Html::escapeHTML(self::$DBUSER))
+                                                    ->label(new Label(__('Database User Name:'), Label::OUTSIDE_LABEL_BEFORE)),
+                                            ]),
+                                        (new Para())
+                                            ->items([
+                                                (new Password('DBPASSWORD'))
+                                                    ->size(30)
+                                                    ->maxlength(255)
+                                                    ->value('')
+                                                    ->label(new Label(__('Database Password:'), Label::OUTSIDE_LABEL_BEFORE)),
+                                            ]),
+                                        (new Para())
+                                            ->items([
+                                                (new Input('DBPREFIX'))
+                                                    ->size(30)
+                                                    ->maxlength(255)
+                                                    ->value(Html::escapeHTML(self::$DBPREFIX))
+                                                    ->extra('required placeholder="' . __('Prefix') . '"')
+                                                    ->label((new Label($required . __('Database Tables Prefix:'), Label::OUTSIDE_LABEL_BEFORE))->class('required')),
+                                            ]),
+                                        (new Para())
+                                            ->items([
+                                                (new Email('ADMINMAILFROM'))
+                                                    ->size(30)
+                                                    ->autocomplete('email')
+                                                    ->value(Html::escapeHTML(self::$ADMINMAILFROM))
+                                                    ->label(new Label(__('Master Email: (used as sender for password recovery)'), Label::OUTSIDE_LABEL_BEFORE)),
+                                            ]),
+                                            (new Submit('submit', __('Continue')))
+                                    ]),
+                            ]),
+                    ]),
+            ])
+            ->render();
     }
 
     /**
@@ -312,5 +399,21 @@ class Wizard extends Process
         $value = str_replace("'", "\'", $value);
 
         return (string) preg_replace('/(\'' . $name . '\')(.*?)$/ms', '$1,\'' . $value . '\');', $config_content);
+    }
+
+    /**
+     * Parse error message.
+     *
+     * @param   string          $message    The message
+     * @param   int             $code       The code
+     * @param   null|Exception  $error      The error
+     *
+     * @throws  Exception
+     * 
+     * @return  never
+     */
+    private static function throwString(string $message, int $code = 0, ?Exception $error = null): void
+    {
+        throw new Exception((new Text('p', $message))->render(), (int) $code, $error);
     }
 }
