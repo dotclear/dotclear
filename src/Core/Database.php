8cab19daca24e5e5a46c853d8c566837a0bc0a73
franck <carnet.franck.paul@gmail.com>
2025-09-01 19:35:37 +0200

    If given driver is provided, use a load and forget service (handler)

--- src/Core/Database.php
+++ src/Core/Database.php
@@ -56,8 +56,8 @@ class Database extends Container implements DatabaseInterface
 
     public function con(string $driver = '', string $host = '', string $database = '', string $user = '', string $password = '', bool $persistent = false, string $prefix = ''): ConnectionInterface
     {
-        // Reload connection handler if driver is set
-        $reload = ($driver !== '');
+        // Reload connection handler (temporary) if driver is set
+        $reload = ($driver !== '' ? null : false);
 
         // If driver is not set, we use parameters from config
         if ($driver === '') {
