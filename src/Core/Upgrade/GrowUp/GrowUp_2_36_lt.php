e15176d6e3c7d25929e80eaf87ab001240f288bb
franck <carnet.franck.paul@gmail.com>
2025-10-31 17:14:54 +0100

    Remove dev-only growUp stuff

--- src/Core/Upgrade/GrowUp/GrowUp_2_36_lt.php
+++ src/Core/Upgrade/GrowUp/GrowUp_2_36_lt.php
@@ -11,7 +11,6 @@ declare(strict_types=1);
 
 namespace Dotclear\Core\Upgrade\GrowUp;
 
-use Dotclear\App;
 use Dotclear\Core\Upgrade\Upgrade;
 
 /**
@@ -49,33 +48,14 @@ class GrowUp_2_36_lt
                 'src/Interface/ConfigInterface.php',
                 'src/Interface/Core/ConnectionInterface.php',
                 'src/Interface/Core/SchemaInterface.php',
-
-                // Dev only -- TO BE REMOVED BEFORE 2.36 RELEASE - WERE NOT IN 2.35
-                'src/Interface/Database/DbSchemaInterface.php',
-                'src/Interface/Database/DbHandlerInterface.php',
-                'src/Interface/Database/InterfaceSchema.php',
-                'src/Database/ContainerHandler.php',
-                'src/Database/ContainerSchema.php',
-                'src/Database/DbSchemaInterface.php',
-                'src/Database/DbHandlerInterface.php',
             ],
             // Folders
             [
                 'plugins/antispam/locales/lb',
                 'src/Database/Driver',
                 'src/Interface/Exception',
-
-                // Dev only -- TO BE REMOVED BEFORE 2.36 RELEASE - WERE NOT IN 2.35
-                'src/Schema/Database/Sqlite',
             ]
         );
-
-        // Dev only -- TO BE REMOVED BEFORE 2.36 RELEASE - WERE NOT IN 2.35
-        // credential database table was wrong in 2.36-dev-r20250820...
-        $columns = App::db()->con()->schema()->getColumns(App::db()->con()->prefix() . 'credential');
-        if (array_key_exists('credential_id', $columns)) {
-            App::db()->con()->execute('DROP TABLE ' . App::db()->con()->prefix() . 'credential');
-        }
 
         return $cleanup_sessions;
     }
