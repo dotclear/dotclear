dotclear/dotclear

commit f95ab0061a81909013d58e7348f30ec3ef5c92b7

WIP: PHP code review (PHPStan max/Rector) Utilities (Backend, Frontend, Install, Upgrade)

src/Core/Backend/Utility.php
@@ -152,6 +152,7 @@ class Utility extends AbstractUtility
      */
     public function action(): Action
     {
+        // @phpstan-ignore return.type
         return $this->get(Action::class);
     }
 
@@ -160,6 +161,7 @@ class Utility extends AbstractUtility
      */
     public function auth(): Auth
     {
+        // @phpstan-ignore return.type
         return $this->get(Auth::class);
     }
 
@@ -168,6 +170,7 @@ class Utility extends AbstractUtility
      */
     public function combos(): Combos
     {
+        // @phpstan-ignore return.type
         return $this->get(Combos::class);
     }
 
@@ -176,6 +179,7 @@ class Utility extends AbstractUtility
      */
     public function helper(): Helper
     {
+        // @phpstan-ignore return.type
         return $this->get(Helper::class);
     }
 
@@ -184,6 +188,7 @@ class Utility extends AbstractUtility
      */
     public function filter(): Filter
     {
+        // @phpstan-ignore return.type
         return $this->get(Filter::class);
     }
 
@@ -192,6 +197,7 @@ class Utility extends AbstractUtility
      */
     public function favorites(): Favorites
     {
+        // @phpstan-ignore return.type
         return $this->get(Favorites::class);
     }
 
@@ -200,6 +206,7 @@ class Utility extends AbstractUtility
      */
     public function listing(): Listing
     {
+        // @phpstan-ignore return.type
         return $this->get(Listing::class);
     }
 
@@ -208,6 +215,7 @@ class Utility extends AbstractUtility
      */
     public function mediaPage(): MediaPage
     {
+        // @phpstan-ignore return.type
         return $this->get(MediaPage::class);
     }
 
@@ -216,6 +224,7 @@ class Utility extends AbstractUtility
      */
     public function menus(): Menus
     {
+        // @phpstan-ignore return.type
         return $this->get(Menus::class);
     }
 
@@ -224,6 +233,7 @@ class Utility extends AbstractUtility
      */
     public function modulesList(): ModulesList
     {
+        // @phpstan-ignore return.type
         return $this->get(
             ModulesList::class, // service
             false,              // reload
@@ -239,6 +249,7 @@ class Utility extends AbstractUtility
      */
     public function notices(): Notices
     {
+        // @phpstan-ignore return.type
         return $this->get(Notices::class);
     }
 
@@ -247,6 +258,7 @@ class Utility extends AbstractUtility
      */
     public function page(): Page
     {
+        // @phpstan-ignore return.type
         return $this->get(Page::class);
     }
 
@@ -255,6 +267,7 @@ class Utility extends AbstractUtility
      */
     public function resources(): Resources
     {
+        // @phpstan-ignore return.type
         return $this->get(Resources::class);
     }
 
@@ -263,6 +276,7 @@ class Utility extends AbstractUtility
      */
     public function themeConfig(): ThemeConfig
     {
+        // @phpstan-ignore return.type
         return $this->get(ThemeConfig::class);
     }
 
@@ -271,6 +285,7 @@ class Utility extends AbstractUtility
      */
     public function themesList(): ThemesList
     {
+        // @phpstan-ignore return.type
         return $this->get(
             ThemesList::class, // service
             false,              // reload
@@ -286,6 +301,7 @@ class Utility extends AbstractUtility
      */
     public function url(): Url
     {
+        // @phpstan-ignore return.type
         return $this->get(Url::class);
     }
 
@@ -294,6 +310,7 @@ class Utility extends AbstractUtility
      */
     public function userPref(): UserPref
     {
+        // @phpstan-ignore return.type
         return $this->get(UserPref::class);
     }
 
@@ -322,10 +339,21 @@ class Utility extends AbstractUtility
 
         // If we have a session we launch it now
         if (App::auth()->checkSession()) {
+            $process = isset($_REQUEST['process']) && is_string($process = $_REQUEST['process']) ? $process : '';
+
+            $user_status = App::status()->user()::DISABLED;
+            if ($process !== 'Logout') {
+                if (App::auth()->isSuperAdmin()) {
+                    $user_status = App::status()->user()::ENABLED;
+                } else {
+                    $user_status = is_numeric($user_status = App::auth()->getInfo('user_status'))
+                        ? (int) $user_status
+                        : App::status()->user()::DISABLED;
+                }
+            }
+
             // Fake process to logout (kill session) and return to auth page.
-            if (!empty($_REQUEST['process'])    && $_REQUEST['process'] == 'Logout'
-                || !App::auth()->isSuperAdmin() && App::status()->user()->isRestricted((int) App::auth()->getInfo('user_status'))
-            ) {
+            if ($process === 'Logout' || App::status()->user()->isRestricted($user_status)) {
                 // Enable REST service if disabled, for next requests
                 if (!App::rest()->serveRestRequests()) {
                     App::rest()->enableRestServer(true);
@@ -338,20 +366,27 @@ class Utility extends AbstractUtility
             }
 
             // Check nonce from POST requests
-            if ($_POST !== [] && (empty($_POST['xd_check']) || !App::nonce()->checkNonce($_POST['xd_check']))) {
-                throw new PreconditionException();
+            if ($_POST !== []) {
+                $xd_check = isset($_POST['xd_check']) && is_string($xd_check = $_POST['xd_check']) ? $xd_check : '';
+                if (!App::nonce()->checkNonce($xd_check)) {
+                    throw new PreconditionException();
+                }
             }
 
             // Switch blog
-            if (!empty($_REQUEST['switchblog']) && App::auth()->getPermissions($_REQUEST['switchblog']) !== false) {
-                App::session()->set('sess_blog_id', $_REQUEST['switchblog']);
+            $switch_blog = isset($_REQUEST['switchblog']) && is_string($switch_blog = $_REQUEST['switchblog']) ? $switch_blog : '';
+            if ($switch_blog !== '' && App::auth()->getPermissions($switch_blog) !== false) {
+                App::session()->set('sess_blog_id', $switch_blog);
 
-                if (!empty($_REQUEST['redir'])) {
+                if (!empty($_REQUEST['redir']) && is_string($_REQUEST['redir'])) {
                     // Keep context as far as possible
-                    $redir = (string) $_REQUEST['redir'];
+                    $redir = $_REQUEST['redir'];
                 } else {
                     // Removing switchblog from URL
-                    $redir = (string) $_SERVER['REQUEST_URI'];
+                    $redir = isset($_SERVER['REQUEST_URI']) && is_string($redir = $_SERVER['REQUEST_URI'])
+                        ? $redir
+                        : App::backend()->url()->get('admin.home');
+
                     $redir = (string) preg_replace('/switchblog=(.*?)(&|$)/', '', $redir);
                     $redir = (string) preg_replace('/\?$/', '', $redir);
                 }
@@ -371,7 +406,8 @@ class Utility extends AbstractUtility
             }
 
             // Check if requested blog is in URL query (blog=blog_id)
-            if (($url = parse_url((string) $_SERVER['REQUEST_URI'])) && isset($url['query'])) {
+            $request_uri = isset($_SERVER['REQUEST_URI']) && is_string($request_uri = $_SERVER['REQUEST_URI']) ? $request_uri : '';
+            if (($url = parse_url($request_uri)) && isset($url['query'])) {
                 $params = [];
                 parse_str($url['query'], $params);
                 if (isset($params['blog'])) {
@@ -380,13 +416,20 @@ class Utility extends AbstractUtility
             }
 
             // Check blog to use and log out if no result
-            if (App::session()->get('sess_blog_id') != '') {
-                if (App::auth()->getPermissions(App::session()->get('sess_blog_id')) === false) {
+            $sess_blog_id = is_string($sess_blog_id = App::session()->get('sess_blog_id')) ? $sess_blog_id : '';
+            if ($sess_blog_id !== '') {
+                if (App::auth()->getPermissions($sess_blog_id) === false) {
                     App::session()->unset('sess_blog_id');
                 }
-            } elseif (($b = App::auth()->findUserBlog(App::auth()->getInfo('user_default_blog'), false)) !== false) {
-                App::session()->set('sess_blog_id', $b);
-                unset($b);
+            } else {
+                $user_default_blog = is_string($user_default_blog = App::auth()->getInfo('user_default_blog')) ? $user_default_blog : '';
+                if ($user_default_blog !== '') {
+                    $user_blog = App::auth()->findUserBlog($user_default_blog, false);
+                    if ($user_blog !== false) {
+                        App::session()->set('sess_blog_id', $user_blog);
+                        unset($user_blog);
+                    }
+                }
             }
 
             // Load locales
@@ -397,9 +440,10 @@ class Utility extends AbstractUtility
                 $GLOBALS['_lang'] = App::lang()->getLang();
             }
 
-            // Load blog
-            if (App::session()->get('sess_blog_id') != '') {
-                App::blog()->loadFromBlog(App::session()->get('sess_blog_id'));
+            // Load blog - we don't use directly $sess_blog_id variable as this session may have been changed (see above)
+            $sess_blog_id = is_string($sess_blog_id = App::session()->get('sess_blog_id')) ? $sess_blog_id : '';
+            if ($sess_blog_id !== '') {
+                App::blog()->loadFromBlog($sess_blog_id);
             } else {
                 App::session()->destroy();
                 App::backend()->url()->redirect('admin.auth');

src/Core/Frontend/Utility.php
@@ -145,6 +145,7 @@ class Utility extends AbstractUtility
      */
     public function context(): Ctx
     {
+        // @phpstan-ignore return.type
         return $this->get(Ctx::class);
     }
 
@@ -156,6 +157,7 @@ class Utility extends AbstractUtility
     public function template(): Tpl
     {
         try {
+            // @phpstan-ignore return.type
             return $this->get(Tpl::class, false, App::config()->cacheRoot(), 'App::frontend()->template()');
         } catch (Throwable $e) {
             throw new TemplateException(__('Can\'t create template files.'), 571, $e);
@@ -171,6 +173,7 @@ class Utility extends AbstractUtility
      */
     public function xmlrpc(?string $blog_id): XmlRpc
     {
+        // @phpstan-ignore return.type
         return $this->get(XmlRpc::class, true, $blog_id);
     }
 
@@ -271,7 +274,8 @@ class Utility extends AbstractUtility
         }
 
         # Loading locales
-        App::lang()->setLang((string) App::blog()->settings()->system->lang);
+        $system_lang = is_string($system_lang = App::blog()->settings()->system->lang) ? $system_lang : 'en';
+        App::lang()->setLang($system_lang);
 
         if (App::lang()->set(App::config()->l10nRoot() . '/' . App::lang()->getLang() . '/date') === false && App::lang()->getLang() !== 'en') {
             App::lang()->set(App::config()->l10nRoot() . '/en/date');
@@ -301,40 +305,56 @@ class Utility extends AbstractUtility
 
         # Defining theme if not defined
         if (App::frontend()->theme === null) {
-            App::frontend()->theme = App::blog()->settings()->system->theme;
+            App::frontend()->theme = is_string($theme = App::blog()->settings()->system->theme) ? $theme : '';
         }
 
-        if (!App::themes()->moduleExists(App::frontend()->theme)) {
-            App::frontend()->theme = App::blog()->settings()->system->theme = App::config()->defaultTheme();
+        $theme = is_string($theme = App::frontend()->theme) ? $theme : '';
+        if ($theme === '' || !App::themes()->moduleExists($theme)) {
+            $theme = App::config()->defaultTheme();
+
+            App::frontend()->theme                 = $theme;
+            App::blog()->settings()->system->theme = $theme;
         }
 
-        App::frontend()->parent_theme = App::themes()->moduleInfo(App::frontend()->theme, 'parent');
-        if (is_string(App::frontend()->parent_theme) && (App::frontend()->parent_theme !== null && App::frontend()->parent_theme !== '') && !App::themes()->moduleExists(App::frontend()->parent_theme)) {
+        $parent_theme = is_string($parent_theme = App::themes()->moduleInfo($theme, 'parent')) ? $parent_theme : null;
+        $parent_theme = $parent_theme === '' ? null : $parent_theme;
+
+        App::frontend()->parent_theme = $parent_theme;
+
+        if (is_string($parent_theme) && !App::themes()->moduleExists($parent_theme)) {
             // Parent theme defined but not installed, fallback theme to default one
-            App::frontend()->theme        = App::blog()->settings()->system->theme = App::config()->defaultTheme();
-            App::frontend()->parent_theme = null;
+            $theme = App::config()->defaultTheme();
+
+            App::blog()->settings()->system->theme = $theme;
+            App::frontend()->theme                 = $theme;
+            App::frontend()->parent_theme          = null;
         }
 
         # If theme doesn't exist, stop everything
-        if (!App::themes()->moduleExists(App::frontend()->theme)) {
+        if (!App::themes()->moduleExists($theme)) {
             throw new TemplateException(App::config()->debugMode() ?
                 __('This either means you removed your default theme or set a wrong theme ' .
                 'path in your blog configuration. Please check theme_path value in ' .
-                'about:config module or reinstall default theme. (' . App::frontend()->theme . ')') :
+                'about:config module or reinstall default theme. (' . $theme . ')') :
                 __('Default theme not found.'));
         }
 
         # Loading _public.php file for selected theme
-        App::themes()->loadNsFile(App::frontend()->theme, 'public');
+        App::themes()->loadNsFile($theme, 'public');
 
         # Loading translations for selected theme
-        if (is_string(App::frontend()->parent_theme) && (App::frontend()->parent_theme !== null && App::frontend()->parent_theme !== '')) {
-            App::themes()->loadModuleL10N(App::frontend()->parent_theme, App::lang()->getLang(), 'main');
+        if ($parent_theme !== null) {
+            App::themes()->loadModuleL10N($parent_theme, App::lang()->getLang(), 'main');
         }
-        App::themes()->loadModuleL10N(App::frontend()->theme, App::lang()->getLang(), 'main');
+        App::themes()->loadModuleL10N($theme, App::lang()->getLang(), 'main');
 
         # --BEHAVIOR-- publicPrepend --
         App::behavior()->callBehavior('publicPrependV2');
+
+        // Check again theme and parent_theme as they might have been changed via previous behavior
+        // We assume that the corresponding parent_theme is correct if it is not equal to null
+        $theme        = is_string($theme = App::frontend()->theme) ? $theme : '';
+        $parent_theme = is_string($parent_theme = App::frontend()->parent_theme) ? $parent_theme : null;
 
         # Prepare the HTTP cache thing
         App::cache()->addFiles(get_included_files());
@@ -351,29 +371,31 @@ class Utility extends AbstractUtility
         }
 
         $tpl_path = [
-            App::config()->varRoot() . '/themes/' . App::config()->blogId() . '/' . App::frontend()->theme . '/tpl',
-            App::blog()->themesPath() . '/' . App::frontend()->theme . '/tpl',
+            App::config()->varRoot() . '/themes/' . App::config()->blogId() . '/' . $theme . '/tpl',
+            App::blog()->themesPath() . '/' . $theme . '/tpl',
         ];
-        if (App::frontend()->parent_theme) {
-            $tpl_path[] = App::blog()->themesPath() . '/' . App::frontend()->parent_theme . '/tpl';
-        }
-        $tplset = App::themes()->moduleInfo(App::frontend()->theme, 'tplset');
-        $dir    = implode(DIRECTORY_SEPARATOR, [App::config()->dotclearRoot(), 'inc', 'public', self::TPL_ROOT, $tplset]);
-        if (!empty($tplset) && is_dir($dir)) {
-            App::frontend()->template()->setPath(
-                $tpl_path,
-                $dir,
-                App::frontend()->template()->getPath()
-            );
-        } else {
-            App::frontend()->template()->setPath(
-                $tpl_path,
-                App::frontend()->template()->getPath()
-            );
+        if ($parent_theme !== null) {
+            $tpl_path[] = App::blog()->themesPath() . '/' . $parent_theme . '/tpl';
         }
 
+        $tplset = is_string($tplset = App::themes()->moduleInfo($theme, 'tplset')) ? $tplset : '';
+        if ($tplset !== '') {
+            $dir = implode(DIRECTORY_SEPARATOR, [App::config()->dotclearRoot(), 'inc', 'public', self::TPL_ROOT, $tplset]);
+            if (is_dir($dir)) {
+                $tpl_path[] = $dir;
+            }
+        }
+
+        App::frontend()->template()->setPath(
+            $tpl_path,
+            App::frontend()->template()->getPath()
+        );
+
         // Set URL scan mode
-        App::url()->setMode(App::blog()->settings()->system->url_scan);
+        $url_scan = is_string($url_scan = App::blog()->settings()->system->url_scan) ? $url_scan : '';
+        if ($url_scan !== '') {
+            App::url()->setMode($url_scan);
+        }
 
         try {
             # --BEHAVIOR-- publicBeforeDocument --

src/Core/Install/Utility.php
@@ -52,16 +52,19 @@ class Utility extends AbstractUtility
 
     public function favorites(): Favorites
     {
+        // @phpstan-ignore return.type
         return $this->get(Favorites::class);
     }
 
     public function page(): Page
     {
+        // @phpstan-ignore return.type
         return $this->get(Page::class);
     }
 
     public function utils(): Utils
     {
+        // @phpstan-ignore return.type
         return $this->get(Utils::class);
     }
 
@@ -87,7 +90,7 @@ class Utility extends AbstractUtility
     public static function init(): bool
     {
         // We need to pass CLI argument to App::task()->run()
-        if (isset($_SERVER['argv'][1])) {
+        if (isset($_SERVER['argv']) && is_array($_SERVER['argv']) && isset($_SERVER['argv'][1])) {
             $_SERVER['DC_RC_PATH'] = $_SERVER['argv'][1];
         }
 

src/Core/Upgrade/Utility.php
@@ -109,6 +109,7 @@ class Utility extends AbstractUtility
      */
     public function menus(): Menus
     {
+        // @phpstan-ignore return.type
         return $this->get(Menus::class);
     }
 
@@ -117,11 +118,14 @@ class Utility extends AbstractUtility
      */
     public function nextStore(): NextStore
     {
+        $xml_url = is_string($xml_url = App::blog()->settings()->get('system')->get('store_plugin_url')) ? $xml_url : '';
+
+        // @phpstan-ignore return.type
         return $this->get(
             NextStore::class,   // service
             false,              // reload
             modules: App::plugins(),
-            xml_url: (string) App::blog()->settings()->get('system')->get('store_plugin_url'),
+            xml_url: $xml_url,
             force: true,
             use_host_cache: true
         );
@@ -132,6 +136,7 @@ class Utility extends AbstractUtility
      */
     public function notices(): Notices
     {
+        // @phpstan-ignore return.type
         return $this->get(Notices::class);
     }
 
@@ -141,6 +146,7 @@ class Utility extends AbstractUtility
     public function otp(): false|Otp
     {
         try {
+            // @phpstan-ignore return.type
             return App::config()->authPasswordOnly() ? false : $this->get(Otp::class);
         } catch (Throwable) { // silently fail
             return false;
@@ -152,6 +158,7 @@ class Utility extends AbstractUtility
      */
     public function page(): Page
     {
+        // @phpstan-ignore return.type
         return $this->get(Page::class);
     }
 
@@ -160,6 +167,7 @@ class Utility extends AbstractUtility
      */
     public function pluginsList(): PluginsList
     {
+        // @phpstan-ignore return.type
         return $this->get(
             PluginsList::class, // service
             false,              // reload
@@ -175,6 +183,7 @@ class Utility extends AbstractUtility
      */
     public function resources(): Resources
     {
+        // @phpstan-ignore return.type
         return $this->get(Resources::class);
     }
 
@@ -183,6 +192,7 @@ class Utility extends AbstractUtility
      */
     public function update(): Update
     {
+        // @phpstan-ignore return.type
         return $this->get(
             Update::class,  // service
             false,          // reload
@@ -198,6 +208,7 @@ class Utility extends AbstractUtility
      */
     public function updateAttic(): UpdateAttic
     {
+        // @phpstan-ignore return.type
         return $this->get(
             UpdateAttic::class, // service
             false,              // reload
@@ -211,6 +222,7 @@ class Utility extends AbstractUtility
      */
     public function upgrade(): Upgrade
     {
+        // @phpstan-ignore return.type
         return $this->get(Upgrade::class);
     }
 
@@ -219,13 +231,14 @@ class Utility extends AbstractUtility
      */
     public function url(): Url
     {
+        // @phpstan-ignore return.type
         return $this->get(Url::class);
     }
 
     public static function init(): bool
     {
         // We need to pass CLI argument to App::task()->run()
-        if (isset($_SERVER['argv'][1])) {
+        if (isset($_SERVER['argv']) && is_array($_SERVER['argv']) && isset($_SERVER['argv'][1])) {
             $_SERVER['DC_RC_PATH'] = $_SERVER['argv'][1];
         }
 
@@ -265,8 +278,11 @@ class Utility extends AbstractUtility
             }
 
             // Check nonce from POST requests
-            if ($_POST !== [] && (empty($_POST['xd_check']) || !App::nonce()->checkNonce($_POST['xd_check']))) {
-                throw new PreconditionException();
+            if ($_POST !== []) {
+                $xd_check = isset($_POST['xd_check']) && is_string($xd_check = $_POST['xd_check']) ? $xd_check : '';
+                if (!App::nonce()->checkNonce($xd_check)) {
+                    throw new PreconditionException();
+                }
             }
 
             // Load locales
@@ -284,7 +300,9 @@ class Utility extends AbstractUtility
      */
     public static function loadLocales(): void
     {
-        App::lang()->setLang((string) App::auth()->getInfo('user_lang'));
+        $user_lang = is_string($user_lang = App::auth()->getInfo('user_lang')) ? $user_lang : 'en';
+
+        App::lang()->setLang($user_lang);
 
         if (App::lang()->set(App::config()->l10nRoot() . '/' . App::lang()->getLang() . '/date') === false && App::lang()->getLang() !== 'en') {
             App::lang()->set(App::config()->l10nRoot() . '/en/date');

src/Helper/Stack/Statuses.php
@@ -102,8 +102,10 @@ class Statuses
      * Search by (int) level or (string) id.
      *
      * Levels are compared like that:
-     * - needle <= defaul level = restricted
-     * - needle > defaut level = not restricted
+     *
+     * - needle <= defaul level then needle is restricted
+     *
+     * - needle > defaut level then needle is not restricted
      */
     public function isRestricted(int|string $needle): bool
     {

commit 2e03a2ee5f2df74ec2e096b3aebb9ef8c5fb187a

Fix authentication when user has no default blog defined (bug introduced by me in commit f95ab0061a81909013d58e7348f30ec3ef5c92b7)

src/Core/Backend/Utility.php
@@ -422,8 +422,9 @@ class Utility extends AbstractUtility
                     App::session()->unset('sess_blog_id');
                 }
             } else {
-                $user_default_blog = is_string($user_default_blog = App::auth()->getInfo('user_default_blog')) ? $user_default_blog : '';
-                if ($user_default_blog !== '') {
+                $user_default_blog = is_string($user_default_blog = App::auth()->getInfo('user_default_blog')) ? $user_default_blog : null;
+                if ($user_default_blog === null || $user_default_blog !== '') {
+                    // If no default blog (null) or a default one
                     $user_blog = App::auth()->findUserBlog($user_default_blog, false);
                     if ($user_blog !== false) {
                         App::session()->set('sess_blog_id', $user_blog);