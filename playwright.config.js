6dc97b090894d0f7a7fd0092a4feb7b32a9c6e19
franck <contact@open-time.net>
2026-09-21 11:13:14 +0200

    Rename tests/functional to tests/e2e (end to end)

--- playwright.config.js
+++ playwright.config.js
@@ -13,7 +13,7 @@ dotenv.config({ path: path.resolve(__dirname, 'playwright/.auth/config.env') });
  * @see https://playwright.dev/docs/test-configuration
  */
 export default defineConfig({
-  testDir: './tests/functional',
+  testDir: './tests/e2e',
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
